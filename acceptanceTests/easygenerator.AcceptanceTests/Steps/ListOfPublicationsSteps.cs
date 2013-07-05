a4eb6b9efa7755d117186982c2ee84f2895af5f9
vr-dan <vr.danylchuk@ism-ukraine.com>
2013-07-05 12:10:49 +0000

    Publication list tests

--- acceptanceTests/easygenerator.AcceptanceTests/Steps/ListOfPublicationsSteps.cs
+++ acceptanceTests/easygenerator.AcceptanceTests/Steps/ListOfPublicationsSteps.cs
@@ -20,11 +20,6 @@ namespace easygenerator.AcceptanceTests.Steps
     public class ListOfPublicationsSteps
     {
         PublicationsListPage publicationsPage;
-        Dictionary<string, Order> orderWay = new Dictionary<string, Order>()
-        {
-            {"ascending", Order.Ascending},
-            {"descending", Order.Descending}
-        };
         public ListOfPublicationsSteps(PublicationsListPage publicationsPage)
         {
             this.publicationsPage = publicationsPage;
@@ -44,67 +39,99 @@ namespace easygenerator.AcceptanceTests.Steps
         {
             var expectedPublications = table.CreateSet<PublicationData>().ToArray();
             var realPublications = publicationsPage.Items;
-            Assert.IsTrue(expectedPublications.All(obj => realPublications.Any(item => item.Title == obj.Title)));
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                expectedPublications.All(obj => realPublications.Any(item => item.Title == obj.Title)),
+                "Not all expected objectives on page");
         }
 
         [Then(@"publications tiles list consists of ordered items")]
         public void ThenPublicationsTilesListConsistsOfOrderedItems(Table table)
         {
-            ScenarioContext.Current.Pending();
+            var expectedPublications = table.CreateSet<PublicationData>().Select(obj => obj.Title).ToArray();
+            var realPublications = publicationsPage.Items.Select(obj => obj.Title).ToArray();
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                TestUtils.AreCollectionsEqual(expectedPublications, realPublications),
+                "Order of objectives should be the same");
         }
 
         [Then(@"publications list order switch is set to '(.*)'")]
-        public void ThenPublicationsListOrderSwitchIsSetTo(string p0)
+        public void ThenPublicationsListOrderSwitchIsSetTo(string orderString)
         {
-            ScenarioContext.Current.Pending();
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                GherkinConstants.OrderWay[orderString] == publicationsPage.Order,
+                "Incorrect publications order switch state");
         }
 
         [When(@"I switch publications list order to '(.*)'")]
-        public void WhenISwitchPublicationsListOrderTo(string p0)
+        public void WhenISwitchPublicationsListOrderTo(string orderString)
         {
-            ScenarioContext.Current.Pending();
+            var expectedOrder = GherkinConstants.OrderWay[orderString];
+            if (publicationsPage.Order != expectedOrder)
+                publicationsPage.Order = expectedOrder;
         }
 
         [When(@"click on publication list item with title '(.*)'")]
-        public void WhenClickOnPublicationListItemWithTitle(string p0)
+        public void WhenClickOnPublicationListItemWithTitle(string title)
         {
-            ScenarioContext.Current.Pending();
+            foreach (var item in publicationsPage.Items)
+            {
+                if (item.Title == title)
+                {
+                    item.Click();
+                    return;
+                }
+            }
+            throw new InvalidOperationException("Cannot find publication with given title");
         }
 
         [Then(@"publication list item with title '(.*)' is selected")]
-        public void ThenPublicationListItemWithTitleIsSelected(string p0)
+        public void ThenPublicationListItemWithTitleIsSelected(string title)
         {
-            ScenarioContext.Current.Pending();
+            var item = publicationsPage.Items.First(it => it.Title == title);
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                item.IsSelected,
+                "Publication should be selected");
         }
 
         [Then(@"publication list item with title '(.*)' is not selected")]
-        public void ThenPublicationListItemWithTitleIsNotSelected(string p0)
+        public void ThenPublicationListItemWithTitleIsNotSelected(string title)
         {
-            ScenarioContext.Current.Pending();
+            var item = publicationsPage.Items.First(it => it.Title == title);
+            TestUtils.Assert_IsFalse_WithWait(() =>
+                item.IsSelected,
+                "Publication should not be selected");
         }
                 
         [Then(@"publications list is displayed in (.*) columns")]
-        public void ThenPublicationsListIsDisplayedInColumns(int p0)
+        public void ThenPublicationsListIsDisplayedInColumns(int columnsCount)
         {
-            ScenarioContext.Current.Pending();
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                columnsCount == publicationsPage.GetColumnsCount(),
+                "Incorrect columns count");
         }
 
         [Then(@"last element of publications list is visible")]
         public void ThenLastElementOfPublicationsListIsVisible()
         {
-            ScenarioContext.Current.Pending();
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                publicationsPage.Items.Last().IsVisisble(),
+                "Element should be visible");
         }
 
         [When(@"mouse hover element of publications list with title '(.*)'")]
-        public void WhenMouseHoverElementOfPublicationsListWithTitle(string p0)
+        public void WhenMouseHoverElementOfPublicationsListWithTitle(string title)
         {
-            ScenarioContext.Current.Pending();
+            var item = publicationsPage.Items.First(it => it.Title == title);
+            item.Hover();
         }
 
-        [Then(@"Action open is enabled true for publications list item with title '(.*)'")]
-        public void ThenActionOpenIsEnabledTrueForPublicationsListItemWithTitle(string p0)
+        [Then(@"Action open is enabled (.*) for publications list item with title '(.*)'")]
+        public void ThenActionOpenIsEnabledTrueForPublicationsListItemWithTitle(bool isEnabled, string title)
         {
-            ScenarioContext.Current.Pending();
+            var item = publicationsPage.Items.First(it => it.Title == title);
+            TestUtils.Assert_IsTrue_WithWait(() =>
+                isEnabled = item.IsOpenEnabled,
+                "Open should be enabled");
         }
 
         [Then(@"Action select is enabled true for publications list item with title '(.*)'")]
