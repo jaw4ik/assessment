45bd7492421b09cb9172fac376562ddb122b72c1
BOBO4KA <kuchuk.v@gmail.com>
2013-07-07 14:42:10 +0000

    Experiences GA events moved to viewmodel

--- sources/easygenerator.Web/App/viewmodels/experiences/experiences.js
+++ sources/easygenerator.Web/App/viewmodels/experiences/experiences.js
@@ -1,51 +1,62 @@
 ﻿define(['dataContext', 'constants', 'eventTracker', 'durandal/plugins/router'],
     function (dataContext, constants, eventTracker, router) {
         "use strict";
-        
+
+        var
+            events = {
+                category: 'Experiences',
+                navigateToObjectives: "Navigate to objectives",
+                sortByTitleAsc: "Sort by title ascending",
+                sortByTitleDesc: "Sort by title descending"
+            },
+
+            sendEvent = function (eventName) {
+                eventTracker.publish(eventName, events.category);
+            };
+
         var
             experiences = ko.observableArray([]),
-            
             currentSortingOption = ko.observable(),
 
-            sortByTitleAsc = function () {
-                if (currentSortingOption() == constants.sortingOptions.byTitleAsc)
-                    return;
-                
-                currentSortingOption(constants.sortingOptions.byTitleAsc);
-                experiences(_.sortBy(experiences(), function (experience) { return experience.title.toLowerCase(); }));
-                eventTracker.publish(constants.events.publicationsSortedByTitleAsc);
-            },
-            
-            sortByTitleDesc = function () {
-                if (currentSortingOption() == constants.sortingOptions.byTitleDesc)
-                    return;
-
-                currentSortingOption(constants.sortingOptions.byTitleDesc);
-                experiences(_.sortBy(experiences(), function (experience) { return experience.title.toLowerCase(); }).reverse());
-                eventTracker.publish(constants.events.publicationsSortedByTitleDesc);
-            },
-            
             activate = function () {
-                return Q.fcall(function () {                  
+                return Q.fcall(function () {
                     experiences(ko.utils.arrayMap(dataContext.experiences, function (item) {
                         return { id: item.id, title: item.title, objectives: item.objectives };
                     }));
                     sortByTitleAsc();
                 });
             },
-            
+
+            sortByTitleAsc = function () {
+                if (currentSortingOption() == constants.sortingOptions.byTitleAsc)
+                    return;
+
+                currentSortingOption(constants.sortingOptions.byTitleAsc);
+                experiences(_.sortBy(experiences(), function (experience) { return experience.title.toLowerCase(); }));
+                sendEvent(events.sortByTitleAsc);
+            },
+
+            sortByTitleDesc = function () {
+                if (currentSortingOption() == constants.sortingOptions.byTitleDesc)
+                    return;
+
+                currentSortingOption(constants.sortingOptions.byTitleDesc);
+                experiences(_.sortBy(experiences(), function (experience) { return experience.title.toLowerCase(); }).reverse());
+                sendEvent(events.sortByTitleDesc);
+            },
+
             goToObjectives = function () {
                 router.navigateTo('#/objectives');
-                eventTracker.publish(constants.events.navigateToObjectives);
+                sendEvent(events.navigateToObjectives);
             };
 
         return {
-            activate: activate,
             experiences: experiences,
-            sortByTitleAsc: sortByTitleAsc,
-            sortByTitleDesc: sortByTitleDesc,
             currentSortingOption: currentSortingOption,
             sortingOptions: constants.sortingOptions,
+            activate: activate,
+            sortByTitleAsc: sortByTitleAsc,
+            sortByTitleDesc: sortByTitleDesc,
             goToObjectives: goToObjectives
         };
     }
