6522660962462866321d9a1ac796d3cad795452b
BOBO4KA <kuchuk.v@gmail.com>
2013-07-15 06:22:06 +0000

    translations added

--- sources/easygenerator.Web/App/localization/resources.js
+++ sources/easygenerator.Web/App/localization/resources.js
@@ -1,62 +1,134 @@
 ﻿define({
+    
+    /* Resource section for model General */
+    save: {
+        en: 'Save',
+        nl: 'Opslaan',
+        de: 'Speichern'
+    },
+    cancel: {
+        en: 'Cancel',
+        nl: 'Annuleren',
+        de: 'Abbrechen'
+    },
+    back: {
+        en: 'Back',
+        nl: 'Terug',
+        de: 'Zurück'
+    },
+    sortBy: {
+        en: 'Sort by',
+        nl: 'Sorteren op',
+        de: 'Sortieren nach'
+    },
+    firstLetter: {
+        en: 'A',
+        nl: 'A',
+        de: 'A'
+    },
+    lastLetter: {
+        en: 'Z',
+        nl: 'Z',
+        de: 'Z'
+    },
+    open: {
+        en: 'Open',
+        nl: 'Openen',
+        de: 'Öffnen'
+    },
+    select: {
+        en: 'Select',
+        nl: 'Selecteer',
+        de: 'Auswählen'
+    },
+    unselect: {
+        en: 'Unselect',
+        nl: 'Deselecteren',
+        de: 'Deaktivieren'
+    },
+    backTo: {
+        en: 'Back to',
+        nl: 'Terug naar',
+        de: 'Zurück zu'
+    },
+    language: {
+        en: 'Language',
+        nl: 'Language NL',
+        de: 'Language DE'
+    },
+    /* End resource section for model General */
+    
+    /* Resource section for model Objectives */
     learningObjectives: {
         en: 'Learning objectives',
-        nl: 'Learning objectives NL',
-        de: 'Learning objectives DE'
+        nl: 'Leerdoelen',
+        de: 'Lernziele'
     },
+    /* End resource section for model Objectives */
+    
+    /* Resource section for model Objective */
+    objectiveTitleEmptyErrorMessage: {
+        en: 'Please, provide title for objective',
+        nl: 'Please, provide title for objective NL',
+        de: 'Please, provide title for objective DE'
+    },
+    /* End resource section for model Objective */
+    
+    /* Resource section for model Experiences */
     experiences: {
         en: 'Experiences',
-        nl: 'Experiences NL',
-        de: 'Experiences DE'
+        nl: 'Ervaringen',
+        de: 'Erfahrungen'
     },
-
-    /* createObjective.html */
+    /* End resource section for model Experiences */
+    
+    /* Resource section for model CreateObjective */
     newObjective: {
-        en: 'New Objective',
-        nl: 'New Objective NL',
-        de: 'New Objective DE'
+        en: 'New Objective',
+        nl: 'New Objective NL',
+        de: 'New Objective DE'
     },
     objectiveTitle: {
-        en: 'Objective title',
-        nl: 'Objective title NL',
-        de: 'Objective title DE'
+        en: 'Objective title',
+        nl: 'Objective title NL',
+        de: 'Objective title DE'
     },
     objectiveTitlePlaceholder: {
-        en: 'Objective title...',
-        nl: 'Objective title... NL',
-        de: 'Objective title... DE'
+        en: 'Objective title...',
+        nl: 'Objective title… NL',
+        de: 'Objective title… DE'
     },
     objectiveThumbnail: {
         en: 'Thumbnail',
         nl: 'Thumbnail NL',
         de: 'Thumbnail DE'
     },
-    /* endof createObjective.html */
-
-    /* createExperience.html */
+    /* End resource section for model CreateObjective */
+    
+    /* Resource section for model CreateExperience */
     newExperience: {
-        en: 'New Experience',
-        nl: 'New Experience NL',
-        de: 'New Experience DE'
+        en: 'New Experience',
+        nl: 'New Experience NL',
+        de: 'New Experience DE'
     },
     experienceTitle: {
-        en: 'Experience title',
-        nl: 'Experience title NL',
-        de: 'Experience title DE'
+        en: 'Experience title',
+        nl: 'Experience title NL',
+        de: 'Experience title DE'
     },
     experienceTitlePlaceholder: {
-        en: 'Experience title...',
-        nl: 'Experience title... NL',
-        de: 'Experience title... DE'
+        en: 'Experience title...',
+        nl: 'Experience title… NL',
+        de: 'Experience title… DE'
     },
     objectivesInExperience: {
-        en: 'Objectives in experience',
-        nl: 'Objectives in experience NL',
-        de: 'Objectives in experience DE'
+        en: 'Objectives in experience',
+        nl: 'Objectives in experience NL',
+        de: 'Objectives in experience DE'
     },
-    /* endof createExperience.html */
-
-    /* question.html */
+    /* End resource section for model CreateExperience */
+    
+    /* Resource section for model Question */
     previous: {
         en: 'Previous',
         nl: 'Previous NL',
@@ -68,100 +140,38 @@
         de: 'Next DE'
     },
     emptyAnswerOptions: {
-        en: 'The answer options list is empty',
-        nl: 'The answer options list is empty NL',
-        de: 'The answer options list is empty DE'
+        en: 'The answer options list is empty',
+        nl: 'The answer options list is empty NL',
+        de: 'The answer options list is empty DE'
     },
     answerOptions: {
-        en: 'Answer options',
-        nl: 'Answer options NL',
-        de: 'Answer options DE'
+        en: 'Answer options',
+        nl: 'Answer options NL',
+        de: 'Answer options DE'
     },
     emptyExplanations: {
-        en: 'The explanations list is empty',
-        nl: 'The explanations list is empty NL',
-        de: 'The explanations list is empty DE'
+        en: 'The explanations list is empty',
+        nl: 'The explanations list is empty NL',
+        de: 'The explanations list is empty DE'
     },
     explanations: {
         en: 'Explanations',
         nl: 'Explanations NL',
         de: 'Explanations DE'
     },
-    /* endof question.html */
-
-    objectiveTitleEmptyErrorMessage: {
-        en: 'Please, provide title for objective',
-        nl: 'Please, provide title for objective NL',
-        de: 'Please, provide title for objective DE'
-    },
-    /* common */
-    language: {
-        en: 'Language',
-        nl: 'Language NL',
-        de: 'Language DE'
-    },
-    save: {
-        en: 'Save',
-        nl: 'Save NL',
-        de: 'Save DE'
-    },
-    cancel: {
-        en: 'Cancel',
-        nl: 'Cancel NL',
-        de: 'Cancel DE'
-    },
-    back: {
-        en: 'Back',
-        nl: 'Back NL',
-        de: 'Back DE'
-    },
-    sortBy: {
-        en: 'Sort by',
-        nl: 'Sort by NL',
-        de: 'Sort by DE'
-    },
-    firstLetter: {
-        en: 'A',
-        nl: 'A NL',
-        de: 'A DE'
-    },
-    lastLetter: {
-        en: 'Z',
-        nl: 'Z NL',
-        de: 'Z DE'
-    },
-    open: {
-        en: 'Open',
-        nl: 'Open NL',
-        de: 'Open DE'
-    },
-    select: {
-        en: 'Select',
-        nl: 'Select NL',
-        de: 'Select DE'
-    },
-    unselect: {
-        en: 'Unselect',
-        nl: 'Unselect NL',
-        de: 'Unselect DE'
-    },
-    backTo: {
-        en: 'Back to',
-        nl: 'NL Back to',
-        de: 'DE Back to'
-    },
-    /* endof common */
-
-    /*list of questions*/
+    /* End resource section for model Question */
+    
+    /* Resource section for model Questions */
     edit: {
         en: 'Edit',
         nl: 'Edit NL',
         de: 'Edit DE'
     },
     relatedQuestions: {
-        en: 'Related questions',
-        nl: 'Related questions NL',
-        de: 'Related questions NL'
+        en: 'Related questions',
+        nl: 'Gerelateerde vragen',
+        de: 'Ähnliche Fragen'
     }
-    /*end of questions*/
+    /* End resource section for model Questions */
+           
 });
