36c593e4b7343f73d783c744db0cf2d046fb9c5b
LeonidPelyhivskyi <plo.91ua@gmail.com>
2013-07-26 08:44:34 +0000

    EWD-22256 - Semantic tags plugin for html editor

--- sources/easygenerator.Web/Scripts/ckeditor/config.js
+++ sources/easygenerator.Web/Scripts/ckeditor/config.js
@@ -16,6 +16,8 @@ CKEDITOR.editorConfig = function (config) {
 
     config.startupFocus = true;
 
+    CKEDITOR.plugins.addExternal('semanticTagsPlugin', CKEDITOR.basePath + 'customPlugins/semanticTagsPlugin/', 'plugin.js');
+
     config.extraPlugins = 'semanticTagsPlugin';
 
     config.removeFormatTags = 'b,big,code,del,dfn,em,font,i,ins,kbd,q,s,samp,small,span,strike,strong,sub,sup,tt,u,var,abbr,acronym,blockquote,cite,mark,figcaption,time';
