f8f1dc4f00235472e6f7098aa40c50ae63a3dd5a
NadiaGamula <nadia.zakharova@gmail.com>
2013-08-02 11:45:10 +0000

    EWD-22220 - Build Experience: Question feedback
    Former-commit-id: 0d6f0bd991e761de1f26878bf5318a417835fc0e

--- sources/easygenerator.Web/Templates/Default/App/viewmodels/feedback.js
+++ sources/easygenerator.Web/Templates/Default/App/viewmodels/feedback.js
@@ -23,6 +23,40 @@
             });
 
             this.score = question.score;
+        },
+        triggerRedraw = ko.observable(false),
+        redrawCanvas = function(cnxt) {
+            var progress = this.score / 100;
+
+            cnxt.beginPath();
+            cnxt.arc(107, 107, 97, 0, 2 * Math.PI);
+            cnxt.strokeStyle = 'rgb(211,212,216)';
+            cnxt.lineWidth = 20;
+            cnxt.closePath();
+            cnxt.stroke();
+
+
+            if (progress > 0) {
+                cnxt.beginPath();
+                cnxt.strokeStyle = 'rgb(87,157,193)';
+                cnxt.lineWidth = 20;
+
+                if (progress == 1) {
+                    cnxt.arc(107, 107, 97, 0, 2 * Math.PI);
+                }
+                else {
+                    cnxt.arc(107, 107, 97, 1.5 * Math.PI, (progress * 2 - 0.5) * Math.PI);
+                }
+
+                cnxt.stroke();
+            }
+            
+            cnxt.beginPath();
+            cnxt.font = '24pt Arial';
+            cnxt.textAlign = 'center';
+            cnxt.textBaseline = "middle";
+            cnxt.fillStyle = '#666666';
+            cnxt.fillText(this.score + "%", 107, 107);
         };
 
     return {
@@ -31,6 +65,8 @@
         backToObjectives: backToObjectives,
         showExplanations: showExplanations,
         chooseNextQuestion: chooseNextQuestion,
-        activate: activate
+        activate: activate,
+        triggerRedraw: triggerRedraw,
+        redrawCanvas: redrawCanvas
     };
 });
