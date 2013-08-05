c3191d90c3bef7e050fa50230053758f7cb5469f
ivanmuzika <ivan.muzika@gmail.com>
2013-08-05 12:49:29 +0000

    Change creation of zip package for text on MacOS
    Former-commit-id: 126b085c7bb6d78543caf97666c3b03c695ae04a

--- sources/easygenerator.Web/BuildExperience/BuildPackageCreator.cs
+++ sources/easygenerator.Web/BuildExperience/BuildPackageCreator.cs
@@ -1,4 +1,9 @@
-﻿using System.IO.Compression;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 using easygenerator.Infrastructure;
 
 namespace easygenerator.Web.BuildExperience
@@ -15,7 +20,49 @@ namespace easygenerator.Web.BuildExperience
         public virtual void CreatePackageFromFolder(string packageFolderPath, string destinationFileName)
         {
             _fileManager.DeleteFile(destinationFileName);
-            ZipFile.CreateFromDirectory(packageFolderPath, destinationFileName);
+            DoCreateFromDirectory(packageFolderPath, destinationFileName);
+        }
+
+        private void DoCreateFromDirectory(string sourceDirectoryName, string destinationArchiveFileName)
+        {
+            sourceDirectoryName = Path.GetFullPath(sourceDirectoryName);
+            destinationArchiveFileName = Path.GetFullPath(destinationArchiveFileName);
+            using (ZipArchive archive = ZipFile.Open(destinationArchiveFileName, ZipArchiveMode.Create))
+            {
+                DirectoryInfo info = new DirectoryInfo(sourceDirectoryName);
+                string fullName = info.FullName;
+
+                foreach (FileSystemInfo info2 in info.EnumerateFileSystemInfos("*", SearchOption.AllDirectories))
+                {
+                    int length = info2.FullName.Length - fullName.Length;
+                    string entryName = info2.FullName.Substring(fullName.Length, length).TrimStart(new[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar });
+                    if (info2 is FileInfo)
+                    {
+                        archive.CreateEntryFromFile(info2.FullName, entryName);
+                    }
+                    else
+                    {
+                        DirectoryInfo possiblyEmptyDir = info2 as DirectoryInfo;
+                        if ((possiblyEmptyDir != null) && IsDirEmpty(possiblyEmptyDir))
+                        {
+                            archive.CreateEntry(entryName + Path.AltDirectorySeparatorChar);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool IsDirEmpty(DirectoryInfo possiblyEmptyDir)
+        {
+            using (IEnumerator<FileSystemInfo> enumerator = possiblyEmptyDir.EnumerateFileSystemInfos("*", SearchOption.AllDirectories).GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    FileSystemInfo current = enumerator.Current;
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
