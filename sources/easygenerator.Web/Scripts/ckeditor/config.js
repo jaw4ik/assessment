0eb69738fb48c6a472cf1b86e8ad7d6dbd6a565a
BOBO4KA <kuchuk.v@gmail.com>
2013-08-07 12:23:09 +0000

    allow iframe element
    Former-commit-id: f999c4ff4e8f66c65c68c459688fb192809ee052

--- sources/easygenerator.Web/Scripts/ckeditor/config.js
+++ sources/easygenerator.Web/Scripts/ckeditor/config.js
@@ -14,6 +14,7 @@ CKEDITOR.editorConfig = function (config) {
     CKEDITOR.plugins.addExternal('semanticTagsPlugin', CKEDITOR.basePath + 'customPlugins/semanticTagsPlugin/', 'plugin.js');
 
     config.extraPlugins = 'semanticTagsPlugin';
+    config.extraAllowedContent = 'iframe';
 
     config.removeFormatTags = 'b,big,code,del,dfn,em,font,i,ins,kbd,q,s,samp,small,span,strike,strong,sub,sup,tt,u,var,abbr,acronym,blockquote,cite,mark,figcaption,time';
 
