10277fc981eacf256b79e1e25e77720205385c97
NadiaGamula <nadia.zakharova@gmail.com>
2013-08-09 06:43:37 +0000

    EWD-22142 - Build Experience - add long names of answer option, question title
    Former-commit-id: c760185783cf193b718f59616bfa601bffc17637

--- sources/easygenerator.Web/Templates/Default/content/data.js
+++ sources/easygenerator.Web/Templates/Default/content/data.js
@@ -8,9 +8,9 @@
             "questions": [
                 {
                     "id": "1",
-                    "title": "Question 1",
+                    "title": "Question1 is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
                     "answers": [
-                        { "id": "1", "isCorrect": false, "text": "Answer option 1" },
+                        { "id": "1", "isCorrect": false, "text": "Answer option 1 dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." },
                         { "id": "1", "isCorrect": true, "text": "Answer option 2" },
                         { "id": "1", "isCorrect": false, "text": "Answer option 3" }
                     ],
