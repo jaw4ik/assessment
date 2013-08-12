c622409ad5bb4729e2477af2872ecb680dc3b0a6
NadiaGamula <nadia.zakharova@gmail.com>
2013-08-12 08:42:18 +0000

    EWD-22303 - Not Supported Browser page
    Former-commit-id: 2866f8b8d31810796fb57f4a275b7cfa346d104d

--- sources/easygenerator.Web/Templates/Default/App/browserSupport.js
+++ sources/easygenerator.Web/Templates/Default/App/browserSupport.js
@@ -1,18 +1,22 @@
 ﻿define([],
     function () {
 
-        // check iOS 6+
+        // iOS 6+ is supported 
         if (/iP(hone|od|ad)/.test(navigator.platform)) {
             var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-            if (parseInt(v[1], 10) < 6)
-                return false;
+            if (parseInt(v[1], 10) >= 6)
+                return true;
         }
+        
+        //Android is supported
+        if (navigator.userAgent.toLowerCase().indexOf("android") != -1)
+            return true;
 
-        // check Opera 
+        // Opera is not supported
         if (navigator.appName.toLowerCase() == "opera" || navigator.userAgent.indexOf("OPR") != -1)
             return false;
 
-        //check IE 9+, Chrome 28+, Firefox 22+, Safari 5+
+        //IE 9+, Chrome 28+, Firefox 22+, Safari 5+ are supported
         var ua = navigator.userAgent,
             N = navigator.appName, tem,
             M = ua.match(/(chrome|safari|firefox|msie)\/?\s*([\d\.]+)/i) || [];
@@ -23,12 +27,13 @@
         var browser = M[0].toLowerCase();
         var version = parseInt(M[1], 10);
 
-        if (browser == "chrome" && version < 28 ||
-            browser == "msie" && version < 9 ||
-            browser == "firefox" && version < 22 ||
-            browser == "safari" && version < 533)
-            return false;
+        if (browser == "chrome" && version >= 28 ||
+            browser == "msie" && version >= 9 ||
+            browser == "firefox" && version >= 22 ||
+            browser == "safari" && version >= 533)
+            return true;
 
-        return true;
+        //all other are not supported
+        return false;
 
     });
