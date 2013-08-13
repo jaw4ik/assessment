50dadd0401d807c962d46781a7cee3746731c453
jaw4ik <urec.savchuk@gmail.com>
2013-08-13 11:38:59 +0000

    add createExperience.spec.js
    Former-commit-id: ca7b372e1fec2b29a67e4a1a7100651e086f2aef

---
+++ sources/easygenerator.Web/App/viewmodels/experiences/createExperience.spec.js
@@ -0,0 +1,17 @@
+﻿define(['viewmodels/experiences/createExperience'],
+    function (viewModel) {
+        "use strict";
+
+        describe('viewModel [createExperience]', function() {
+
+            it('should be a object', function() {
+                expect(viewModel).toBeObject();
+            });
+
+            describe('navigate to experiences', function() {
+
+            });
+
+        });
+    }
+);
