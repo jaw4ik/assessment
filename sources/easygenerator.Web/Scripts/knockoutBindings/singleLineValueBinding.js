5a5bccc2a0f1404feb56f399123b39b11ec541f5
YuliaSavchuk <yulia.savchuk3@gmail.com>
2013-08-21 14:19:18 +0000

    EWD-22149 - Create/Update/Delete Question
    Former-commit-id: c51601c1806ea32d5b736972f668050ad2e6fef9

---
+++ sources/easygenerator.Web/Scripts/knockoutBindings/singleLineValueBinding.js
@@ -0,0 +1,39 @@
+﻿ko.bindingHandlers.singleLineValue = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = ko.unwrap(valueAccessor());
+        valueAccessor()(filterValue(value));
+        
+        ko.bindingHandlers.value.init(element, valueAccessor, allBindingsAccessor, viewModel);
+
+        var onKeyPress = function (e) {
+            if (e.keyCode != 13)
+                return;
+
+            try {
+                $elem.blur();
+            } finally {
+                event.preventDefault();
+                event.returnValue = false;
+            }
+        };
+
+        var $elem = $(element);
+        $elem.bind('keypress', onKeyPress);
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            $elem.unbind('keypress', onKeyPress);
+        });
+    },
+
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var value = ko.unwrap(valueAccessor());
+        valueAccessor()(filterValue(value));
+        
+        ko.bindingHandlers.value.update(element, valueAccessor, allBindingsAccessor, viewModel);
+    }
+};
+
+function filterValue(str) {
+    return str.replace(/(\r\n|\n|\r)/gm, " ");
+}
+
