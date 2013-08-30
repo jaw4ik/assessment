857445b653968ac42117dc802d1dd690516adcf1
vr-dan <vr.danylchuk@ism-ukraine.com>
2013-08-30 10:47:06 +0000

    Former-commit-id: 06f03746db6bc92200078a5cccc087cd432b2957

--- acceptanceTests/StartServer.bat
+++ acceptanceTests/StartServer.bat
@@ -2,4 +2,4 @@ set testSite=TestSite
 FOR /F %%i IN ("%~dp0easygenerator.Web") DO set path=%%~fi
 "%programfiles(x86)%\IIS Express\APPCMD.exe" delete site %testSite%
 "%programfiles(x86)%\IIS Express\APPCMD.exe" add site /name:%testSite% /bindings:"http/*:5656:" /physicalPath:"%path%"
-"%programfiles(x86)%\IIS Express\iisexpress.exe" /site:%testSite%
+"%programfiles(x86)%\IIS Express\iisexpress.exe" /systray:false /site:%testSite%
