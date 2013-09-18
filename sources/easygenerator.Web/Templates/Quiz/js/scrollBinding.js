17a12675b2b921ef454b07837645c1963899ddab
andriydemchuk <andrewdemchuk@gmail.com>
2013-09-18 06:24:23 +0000

    Former-commit-id: 15706867cd9bee429a6190af593e02ce1cc3cdf3

--- sources/easygenerator.Web/Templates/Quiz/js/scrollBinding.js
+++ sources/easygenerator.Web/Templates/Quiz/js/scrollBinding.js
@@ -20,9 +20,7 @@
             element.style.display = "";
             $(window).on("scroll.ko.scrollHandler", function() {
                 if (($(document).height() - offset <= $(window).height() + $(window).scrollTop())) {
-                    alert('scrollEnd');
                     if (self.updating) {
-                        alert('scrollReset');
                         loadFunc();
                         self.updating = false;
                     }
