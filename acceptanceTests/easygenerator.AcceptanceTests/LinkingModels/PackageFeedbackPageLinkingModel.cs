01d9237fc66db0e7b5d0b036e656bbd2884dde52
vr-dan <vr.danylchuk@ism-ukraine.com>
2013-09-26 08:59:48 +0000

    acceptance tests: default template
    Former-commit-id: 0040bd74497e15f1ff3c69fa67dbb1db7628dad3

---
+++ acceptanceTests/easygenerator.AcceptanceTests/LinkingModels/PackageFeedbackPageLinkingModel.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easygenerator.AcceptanceTests.LinkingModels
+{
+    public class PackageFeedbackPageLinkingModel : ILinkingModel
+    {
+        public string QuestionProgressScore = ".//div[contains(@class,'question-progress-score')]";
+
+        public string ShowExplanationsButton = ".//div[contains(@class,'navigation-btn-container')]//a[contains(@class,'navigation-btn-left')]";
+        public string TryAgainButton = ".//div[contains(@class,'navigation-btn-container')]//a[contains(@class,'navigation-btn-right')]";
+
+        public string BackToObjectivesLink = ".//div[contains(@class,'navigation-link-container')]//div[contains(@class,'pull-left')]//a";
+        public string ProgressSummaryLink = ".//ul[contains(@class,'navigation-menu')]//li[contains(@class,'pull-right')]//a";
+        public string HomeLink = ".//ul[contains(@class,'navigation-menu')]//li[contains(@class,'pull-left')]//a";
+
+
+    }
+}
---
+++ acceptanceTests/easygenerator.AcceptanceTests/Steps/PackageFeedbackPageSteps.cs
@@ -0,0 +1,63 @@
+﻿using easygenerator.AcceptanceTests.ElementObjects;
+using easygenerator.AcceptanceTests.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace easygenerator.AcceptanceTests.Steps
+{
+    [Binding]
+    public class PackageFeedbackPageSteps
+    {
+        PackageFeedbackPage PackageFeedback;
+        public PackageFeedbackPageSteps(PackageFeedbackPage PackageFeedback)
+        {
+            this.PackageFeedback = PackageFeedback;
+        }
+
+        [Then(@"question progress score '(.*)' is shown on package feedback page")]
+        public void ThenQuestionProgressScoreIsShownOnPackageFeedbackPage(string questionScore)
+        {
+            var expectedScore = questionScore;
+            var realScore = PackageFeedback.QuestionProgressScore;
+            TestUtils.Assert_IsTrue_WithWait(() => (realScore == expectedScore), "Incorrect question score is shown: " + realScore);
+        }
+
+        [When(@"click on show explanations button on package feedback page")]
+        public void WhenClickOnShowExplanationsButtonOnPackageFeedbackPage()
+        {
+            PackageFeedback.ShowExplanationsButtonClick();
+        }
+
+        [When(@"click on try again button on package feedback page")]
+        public void WhenClickOnTryAgainButtonOnPackageFeedbackPage()
+        {
+            PackageFeedback.TryAgainButtonClick();
+        }
+
+        [When(@"click on back to objectives link on package feedback page")]
+        public void WhenClickOnBackToObjectivesLinkOnPackageFeedbackPage()
+        {
+            PackageFeedback.BackToObjectivesLinkClick();
+        }
+
+        [When(@"click on progress summary link on package feedback page")]
+        public void WhenClickOnProgressSummaryLinkOnPackageFeedbackPage()
+        {
+            PackageFeedback.ProgressSummaryLinkClick();
+        }
+
+        [When(@"click on home link on package feedback page")]
+        public void WhenClickOnHomeLinkOnPackageFeedbackPage()
+        {
+            PackageFeedback.HomeLinkClick();
+        }
+
+
+    }
+}
