0cc2bdb054e631a80c1767c3366b4d2526c15e0b
BOBO4KA <kuchuk.v@gmail.com>
2013-09-28 12:14:04 +0000

    EWD-22551 - Update CKEditor to version 4.2.1
    Former-commit-id: ef9280adc537af6e17b3e0571d71b2e6ef6e9840

--- sources/easygenerator.Web/Scripts/knockoutBindings/ckeditorBinding.js
+++ sources/easygenerator.Web/Scripts/knockoutBindings/ckeditorBinding.js
@@ -31,7 +31,6 @@
                 if (!isEditing())
                     isEditing(true);
 
-                updateToolbarPosition();
                 saveIntervalId = setInterval(saveData, autosaveInterval);
             });
 
@@ -179,13 +178,6 @@
             var range = editor.createRange();
             range.moveToPosition(range.root, CKEDITOR.POSITION_BEFORE_END);
             editor.getSelection().selectRanges([range]);
-        }
-
-        function updateToolbarPosition() {
-            var screenTop = document.documentElement.scrollTop || document.body.scrollTop;
-            var editorTop = editor.container.getDocumentPosition().y - $toolbarElement.height();
-            var toolbarTopPosition = screenTop > editorTop ? 0 : editorTop;
-            $toolbarElement.css('top', toolbarTopPosition);
         }
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
