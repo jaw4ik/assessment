bf87ecfd0ccfafcb177601bfb915d892e7d332ff
LeonidPelyhivskyi <plo.91ua@gmail.com>
2013-10-09 16:07:33 +0000

    Former-commit-id: ef5e51bb9964a2789605b1f06f35ffaa487de521

--- sources/easygenerator.Web/Components/AuthenticationProvider.cs
+++ sources/easygenerator.Web/Components/AuthenticationProvider.cs
@@ -19,13 +19,7 @@ namespace easygenerator.Web.Components
 
         public void SignOut()
         {
-            if (System.Web.HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName] != null)
-            {
-                var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName);
-                authCookie.Expires = DateTime.Now.AddDays(-1d);
-                System.Web.HttpContext.Current.Response.Cookies.Add(authCookie);
-            }
-            //FormsAuthentication.SignOut();
+            FormsAuthentication.SignOut();
         }
     }
 }
