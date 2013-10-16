95f28f330e571ee7ef2c12804a5b56e95902c159
andriydemchuk <andrewdemchuk@gmail.com>
2013-10-16 12:12:08 +0000

    EWD-22567 - Help hint area and button
    Former-commit-id: e0bd475b0cfbe8b4f1f12ca7da48e824f4969c4e

---
+++ sources/easygenerator.Web/Scripts/knockoutBindings/cssBinding.js
@@ -0,0 +1,24 @@
+﻿ko.bindingHandlers.fixedBlocksPosition = {
+    init: function (elem, va) {
+
+    },
+
+    update: function (elem, va) {
+        var 
+            helpHintHeight,
+            contentContainer = '#content',
+            helpHint = '.help-hint',
+            helpHintText = '.help-hint .hh-text',
+            notFixedContainer = '.view-module',
+            fixedContainer = '.view-module > .fixed-container:first-child';
+
+        var headerHeight = $('.header').height(),
+            topNavHeight = $('.top-navigation').height();
+
+        ($(helpHintText).is(':empty')) ? helpHintHeight = 0 : helpHintHeight = $(helpHint).height(); 
+        
+        $(fixedContainer).css('top', headerHeight + topNavHeight + helpHintHeight + 'px');
+        $(notFixedContainer).css('padding-top', 40 + helpHintHeight + 'px');
+        $(contentContainer).height(600 + helpHintHeight);
+    }
+};
