d592b9ec9acd2dccde9ea8a9dbaf074b57de5846
BOBO4KA <kuchuk.v@gmail.com>
2013-10-21 06:32:37 +0000

    EWD-22560 - Forgot password
    Former-commit-id: d966b3903c09b6301205abca296d07d954e9e302

---
+++ sources/easygenerator.Web/Scripts/account/passwordrecovery.spec.js
@@ -0,0 +1,125 @@
+﻿define([], function() {
+    "use strict";
+
+    var viewModel = app.passwordRecoveryViewModel();
+
+    describe('viewModel [passwordRecoveryViewModel]', function () {
+
+        describe('password:', function () {
+
+            it('should be observable', function() {
+                expect(viewModel.password).toBeObservable();
+            });
+
+        });
+
+        describe('isPasswordVisible:', function () {
+
+            it('should be observable', function() {
+                expect(viewModel.isPasswordVisible).toBeObservable();
+            });
+
+        });
+        
+        describe('isPasswordEditing:', function () {
+
+            it('should be observable', function () {
+                expect(viewModel.isPasswordEditing).toBeObservable();
+            });
+
+        });
+
+        describe('showHidePassword:', function () {
+
+            it('should be function', function() {
+                expect(viewModel.showHidePassword).toBeFunction();
+            });
+
+            describe('when password visible', function() {
+
+                it('should hide password', function() {
+                    viewModel.isPasswordVisible(true);
+                    
+                    viewModel.showHidePassword();
+
+                    expect(viewModel.isPasswordVisible()).toBeFalsy();
+                });
+
+            });
+            
+            describe('when password hidden', function () {
+
+                it('should show password', function () {
+                    viewModel.isPasswordVisible(false);
+
+                    viewModel.showHidePassword();
+
+                    expect(viewModel.isPasswordVisible()).toBeTruthy();
+                });
+
+            });
+            
+        });
+
+        describe('errorMessage:', function () {
+
+            it('should be observable', function() {
+                expect(viewModel.errorMessage).toBeObservable();
+            });
+
+        });
+
+        describe('hasError:', function () {
+
+            it('should be computed', function() {
+                expect(viewModel.hasError).toBeComputed();
+            });
+
+            describe('when errorMessage is empty', function () {
+
+                it('should return false', function() {
+                    viewModel.errorMessage('');
+
+                    var result = viewModel.hasError();
+
+                    expect(result).toBeFalsy();
+                });
+
+            });
+            
+            describe('when errorMessage is not empty', function () {
+
+                it('should return true', function () {
+                    viewModel.errorMessage('asdasdasdasd');
+
+                    var result = viewModel.hasError();
+
+                    expect(result).toBeTruthy();
+                });
+
+            });
+            
+        });
+
+        describe('canSubmit:', function () {
+
+            it('should be computed', function() {
+                expect(viewModel.canSubmit).toBeComputed();
+            });
+
+            describe('when password is valid', function() {
+
+                it('should return true', function() {
+                    viewModel.password('Easy123');
+
+                    var result = viewModel.canSubmit();
+
+                    expect(result).toBeTruthy();
+                });
+
+            });
+        });
+        
+    });
+    
+});
