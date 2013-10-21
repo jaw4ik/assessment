c0bd6c0943b469de8433d9cefe5a22d82d86caa0
andriydemchuk <andrewdemchuk@gmail.com>
2013-10-21 06:09:52 +0000

    EWD-22645 - Incorrect markup of fields in IE
    Former-commit-id: da4226db7675824a22813dad9bf4403737b16bf7

--- sources/easygenerator.Web/Scripts/account/focusHandlerBinding.js
+++ sources/easygenerator.Web/Scripts/account/focusHandlerBinding.js
@@ -3,13 +3,31 @@
     },
 
     update: function () {
-        var inputElem = '.user-form-input';
 
-        $(inputElem).focusin(function () {
-            $(this).parent(inputElem + '-wrapper').addClass('focus');
+        var elem = '.user-form-input',
+            wrapper = elem + '-wrapper';
+
+        $(elem).focusin(function () {
+        }).blur(function () {
+        }).focusin();
+
+        $(wrapper).click(function () {
+            if ($(this).find(elem).is(':focus')) {
+                $(this).bind('click mousedown mouseup', function () {
+                    return false;
+                });
+            } else {
+                $(this).bind('click mousedown mouseup', function () {
+                    return false;
+                }).find(elem).focus();
+            }
         });
-        $(inputElem).focusout(function () {
-            $(this).parent(inputElem + '-wrapper').removeClass('focus');
+
+        $(elem).focusin(function () {
+            $(this).parent(wrapper).addClass('focus');
+        });
+        $(elem).blur(function () {
+            $(this).parent(wrapper).removeClass('focus');
         });
     }
 };
