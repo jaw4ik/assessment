48bdf3e6ee559b992f02676904d3e02026290687
BOBO4KA <kuchuk.v@gmail.com>
2013-10-25 12:48:03 +0000

    EWD-22693 - Exception while clicking several times on signup
    Former-commit-id: 549f7d895c3b4f9f780c47cc85ec6614eb4519ab

--- sources/easygenerator.Web/Scripts/account/signupsecondstep.js
+++ sources/easygenerator.Web/Scripts/account/signupsecondstep.js
@@ -2,16 +2,25 @@
     var peopleBusyWithCourseDevelopmentAmount = ko.observable(null),
         needAuthoringTool = ko.observable(null),
         usedAuthoringTool = ko.observable(null),
+        isSignupRequestPending = ko.observable(false),
+        
         signUp = function () {
+            if (isSignupRequestPending()) {
+                return;
+            }
+
+            isSignupRequestPending(true);
+
             $.ajax({
                 url: '/api/user/signup',
                 data: { PeopleBusyWithCourseDevelopmentAmount: peopleBusyWithCourseDevelopmentAmount(), NeedAuthoringTool: needAuthoringTool(), UsedAuthoringTool: usedAuthoringTool() },
                 type: 'POST'
-            })
-            .done(function (response) {
+            }).done(function (response) {
                 app.trackEvent(app.constants.events.signup, { username: response.data }).done(function () {
                     app.openHomePage();
                 });
+            }).fail(function() {
+                isSignupRequestPending(false);
             });
         };
     
@@ -19,6 +28,7 @@
         peopleBusyWithCourseDevelopmentAmount: peopleBusyWithCourseDevelopmentAmount,
         needAuthoringTool: needAuthoringTool,
         usedAuthoringTool: usedAuthoringTool,
-        signUp: signUp
+        signUp: signUp,
+        isSignupRequestPending: isSignupRequestPending
     };
 }
--- sources/easygenerator.Web/Scripts/account/signupsecondstep.spec.js
+++ sources/easygenerator.Web/Scripts/account/signupsecondstep.spec.js
@@ -1,123 +1,178 @@
-﻿define([], function() {
+﻿define([], function () {
     "use strinct";
 
     var viewModel;
 
     describe('viewModel [signupsecondstep]', function () {
-        beforeEach(function() {
+        beforeEach(function () {
             viewModel = signUpSecondStepModel();
         });
 
-        it('should be object', function() {
+        it('should be object', function () {
             expect(viewModel).toBeObject();
         });
 
         describe('peopleBusyWithCourseDevelopmentAmount:', function () {
-            
-            it('should be defined', function() {
+
+            it('should be defined', function () {
                 expect(viewModel.peopleBusyWithCourseDevelopmentAmount).toBeDefined();
             });
-            
-            it('should be observable', function() {
+
+            it('should be observable', function () {
                 expect(viewModel.peopleBusyWithCourseDevelopmentAmount).toBeObservable();
             });
-            
+
         });
 
         describe('needAuthoringTool', function () {
-            
+
             it('should be defined', function () {
                 expect(viewModel.needAuthoringTool).toBeDefined();
             });
-            
-            it('should be observable', function() {
+
+            it('should be observable', function () {
                 expect(viewModel.needAuthoringTool).toBeObservable();
             });
-            
+
         });
 
         describe('usedAuthoringTool', function () {
-            
+
             it('should be defined', function () {
                 expect(viewModel.usedAuthoringTool).toBeDefined();
             });
-            
-            it('should be observable', function() {
+
+            it('should be observable', function () {
                 expect(viewModel.usedAuthoringTool).toBeObservable();
             });
-            
+
         });
 
+        describe('isSignupRequestPending:', function () {
+
+            it('should be observable', function () {
+                expect(viewModel.isSignupRequestPending).toBeObservable();
+            });
+
+        });
 
         describe('signup', function () {
-            
-            it('should be function', function() {
+
+            it('should be function', function () {
                 expect(viewModel.signUp).toBeFunction();
             });
-            
+
             var ajax;
             beforeEach(function () {
                 ajax = $.Deferred();
                 spyOn($, 'ajax').andReturn(ajax.promise());
             });
-            
-            it('should call \"/api/user/signup"', function () {
-                viewModel.peopleBusyWithCourseDevelopmentAmount('2');
-                viewModel.needAuthoringTool('now');
-                viewModel.usedAuthoringTool('PowerPoint');
-                viewModel.signUp();
 
-                expect($.ajax).toHaveBeenCalledWith({
-                    url: '/api/user/signup',
-                    data: { PeopleBusyWithCourseDevelopmentAmount: '2', NeedAuthoringTool: 'now', UsedAuthoringTool: 'PowerPoint' },
-                    type: 'POST'
-                });
-            });
-            
-            describe('and request succeded', function () {
-                var trackEvent;
-                var username;
-                
-                beforeEach(function () {
-                    trackEvent = $.Deferred();
-                    spyOn(app, 'trackEvent').andReturn(trackEvent.promise());
-                    username = 'username@easygenerator.com';
-                    ajax.resolve({ data: 'username@easygenerator.com' });
-                });
+            describe('when signup request already pending', function () {
 
-                it('should track event \'Sign up\'', function () {
+                it('should not call \"/api/user/signup"', function () {
+                    viewModel.isSignupRequestPending(true);
+                    viewModel.peopleBusyWithCourseDevelopmentAmount('2');
+                    viewModel.needAuthoringTool('now');
+                    viewModel.usedAuthoringTool('PowerPoint');
                     viewModel.signUp();
-                    
-                    waitsFor(function () {
-                        return ajax.state() === "resolved";
-                    });
-                    runs(function () {
-                        expect(app.trackEvent).toHaveBeenCalledWith('Sign up', { username: username });
+
+                    expect($.ajax).not.toHaveBeenCalled();
+                });
+
+            });
+
+            describe('when signup request is not pending', function () {
+
+                it('should call \"/api/user/signup"', function () {
+                    viewModel.peopleBusyWithCourseDevelopmentAmount('2');
+                    viewModel.needAuthoringTool('now');
+                    viewModel.usedAuthoringTool('PowerPoint');
+                    viewModel.signUp();
+
+                    expect($.ajax).toHaveBeenCalledWith({
+                        url: '/api/user/signup',
+                        data: { PeopleBusyWithCourseDevelopmentAmount: '2', NeedAuthoringTool: 'now', UsedAuthoringTool: 'PowerPoint' },
+                        type: 'POST'
                     });
                 });
 
-                describe('and event is tracked', function () {
+                it('should set isSignupRequestPending', function () {
+                    viewModel.isSignupRequestPending(false);
+                    viewModel.peopleBusyWithCourseDevelopmentAmount('2');
+                    viewModel.needAuthoringTool('now');
+                    viewModel.usedAuthoringTool('PowerPoint');
+                    viewModel.signUp();
+
+                    expect(viewModel.isSignupRequestPending()).toBeTruthy();
+                });
+
+                describe('and request succeded', function () {
+                    var trackEvent;
+                    var username;
 
                     beforeEach(function () {
-                        trackEvent.resolve();
+                        trackEvent = $.Deferred();
+                        spyOn(app, 'trackEvent').andReturn(trackEvent.promise());
+                        username = 'username@easygenerator.com';
+                        ajax.resolve({ data: 'username@easygenerator.com' });
                     });
 
-                    it('should redirect to home page', function () {
-                        spyOn(app, 'openHomePage');
+                    it('should track event \'Sign up\'', function () {
+                        viewModel.signUp();
 
+                        waitsFor(function () {
+                            return ajax.state() === "resolved";
+                        });
+                        runs(function () {
+                            expect(app.trackEvent).toHaveBeenCalledWith('Sign up', { username: username });
+                        });
+                    });
+
+                    describe('and event is tracked', function () {
+
+                        beforeEach(function () {
+                            trackEvent.resolve();
+                        });
+
+                        it('should redirect to home page', function () {
+                            spyOn(app, 'openHomePage');
+
+                            viewModel.signUp();
+
+                            waitsFor(function () {
+                                return ajax.state() !== "pending";
+                            });
+                            runs(function () {
+                                expect(app.openHomePage).toHaveBeenCalled();
+                            });
+                        });
+
+                    });
+
+                });
+
+                describe('and request fails', function () {
+
+                    beforeEach(function () {
+                        ajax.reject();
+                    });
+                    
+                    it('should reset isSignupRequestPending to false', function () {
                         viewModel.signUp();
 
                         waitsFor(function () {
                             return ajax.state() !== "pending";
                         });
                         runs(function () {
-                            expect(app.openHomePage).toHaveBeenCalled();
+                            expect(viewModel.isSignupRequestPending()).toBeFalsy();
                         });
                     });
 
                 });
 
             });
+
         });
 
     });
