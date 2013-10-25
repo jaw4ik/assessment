b3e9a066a475b38c4cad21d91c33bb4034b95bde
BOBO4KA <kuchuk.v@gmail.com>
2013-10-25 08:52:47 +0000

    EWD-22690 - main-built.js not caching
    Former-commit-id: 35545eaf6ef947dd20e85aa4902cb7db1c531c1f

--- sources/easygenerator.Infrastructure/DateTimeWrapper.cs
+++ sources/easygenerator.Infrastructure/DateTimeWrapper.cs
@@ -4,6 +4,12 @@ namespace easygenerator.Infrastructure
 {
     public static class DateTimeWrapper
     {
+        static DateTimeWrapper()
+        {
+            InstanceStartTime = DateTime.UtcNow;
+        }
+
         public static Func<DateTime> Now = () => DateTime.UtcNow;
+        public static DateTime InstanceStartTime { get; private set; }
     }
 }
