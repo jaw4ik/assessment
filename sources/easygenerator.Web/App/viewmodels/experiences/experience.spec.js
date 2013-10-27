4489bfc1c67627e26d53cc73e8cc449363e1a5c5
andreydrebot <a.drebot@gmail.com>
2013-10-27 13:37:04 +0000

    Former-commit-id: badfdb8d5cef8dfbbcd8573cbac8f4cea1e11be5

---
+++ sources/easygenerator.Web/App/viewmodels/experiences/experience.spec.js
@@ -0,0 +1,9 @@
+﻿define(['viewmodels/experiences/define', 'viewmodels/experiences/design', 'viewmodels/experiences/deliver'], function (define, design, deliver) {
+
+
+
+    return {
+       activeModule: activeModule
+    };
+
+});
