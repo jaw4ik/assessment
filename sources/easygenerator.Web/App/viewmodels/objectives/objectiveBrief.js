0479312f218724a2c508b7539c06a8f9d8a8a042
andriydemchuk <andrewdemchuk@gmail.com>
2013-11-13 09:11:00 +0000

    EWD-22785 - Redesign of learning experience editor
    Former-commit-id: 9c1f2da54caf4931279efffbd68f7756fb0a3bac

--- sources/easygenerator.Web/App/viewmodels/objectives/objectiveBrief.js
+++ sources/easygenerator.Web/App/viewmodels/objectives/objectiveBrief.js
@@ -7,7 +7,7 @@
             title: item.title,
             image: item.image,
             questionsCount: item.questionsCount || (item.questions ? item.questions.length : null),
-
+            modifiedOn: item.modifiedOn,
             isSelected: ko.observable(false)
         };
 
