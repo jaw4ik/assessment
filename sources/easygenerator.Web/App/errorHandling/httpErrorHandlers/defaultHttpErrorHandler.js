05a91402ceba71f90d1af6dd791acc6d136a9657
YuliaSavchuk <yulia.savchuk3@gmail.com>
2013-11-13 12:58:33 +0000

    EWD-22783 - Details page can be opened instead of maintenance page - added missing files 2
    Former-commit-id: b6e9f00ee0e49c4b468bd62071f694c560efcf67

---
+++ sources/easygenerator.Web/App/errorHandling/httpErrorHandlers/defaultHttpErrorHandler.js
@@ -0,0 +1,15 @@
+﻿define(['localization/localizationManager', 'notify'], function (localizationManager, notify) {
+    var handleError = function (response) {
+
+        if (!_.isNullOrUndefined(response) && typeof response == "string") {
+            notify.error(response);
+        } else {
+            notify.error(localizationManager.localize("responseFailed"));
+        }
+
+    };
+
+    return {
+        handleError: handleError
+    };
+});
---
+++ sources/easygenerator.Web/App/errorHandling/httpErrorHandlers/defaultHttpErrorHandler.spec.js
@@ -0,0 +1,40 @@
+﻿define(['errorHandling/httpErrorHandlers/defaultHttpErrorHandler', 'localization/localizationManager', 'notify'], function (errorHandler, localizationManager, notify) {
+
+    describe('[defaultHttpErrorHandler]', function () {
+
+        describe('handleError:', function () {
+            var defaultErrorNotification = 'failed';
+            beforeEach(function () {
+                spyOn(notify, 'error');
+                spyOn(localizationManager, 'localize').andReturn(defaultErrorNotification);
+            });
+
+            describe('and response is undefined', function () {
+
+                it('should show default error notification', function () {
+                    errorHandler.handleError();
+                    expect(notify.error).toHaveBeenCalledWith(defaultErrorNotification);
+                });
+            });
+
+            describe('and response is string', function () {
+
+                it('should show error notification with response', function () {
+                    var response = 'message';
+                    errorHandler.handleError(response);
+                    expect(notify.error).toHaveBeenCalledWith(response);
+                });
+            });
+
+            describe('and response is not-string object', function () {
+
+                it('should show default error notification', function () {
+                    errorHandler.handleError({ value: '100500' });
+                    expect(notify.error).toHaveBeenCalledWith(defaultErrorNotification);
+                });
+                
+            });
+        });
+
+    });
+});
