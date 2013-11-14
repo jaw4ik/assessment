e67e8d897d3175cca2c2acd9a9313e89b4285e34
YuliaSavchuk <yulia.savchuk3@gmail.com>
2013-11-14 07:23:20 +0000

    EWD-22705 - Redirect to Login page if session was lost. - added spec case
    Former-commit-id: bedaefbe86fe2a4207ed4d1eb04db72e32db2395

--- sources/easygenerator.Web/App/errorHandling/errorHandlingConfiguration.spec.js
+++ sources/easygenerator.Web/App/errorHandling/errorHandlingConfiguration.spec.js
@@ -1,5 +1,5 @@
-﻿define(['errorHandling/errorHandlingConfiguration', 'errorHandling/httpErrorHandlerRegistrator', 'errorHandling/httpErrorHandlers/serviceUnavailableHttpErrorHandler'],
-    function (configuration, errorHandlerRegistrator, serviceUnavailableHttpErrorHandler) {
+﻿define(['errorHandling/errorHandlingConfiguration', 'errorHandling/httpErrorHandlerRegistrator', 'errorHandling/httpErrorHandlers/serviceUnavailableHttpErrorHandler', 'errorHandling/httpErrorHandlers/unauthorizedHttpErrorHandler'],
+    function (configuration, errorHandlerRegistrator, serviceUnavailableHttpErrorHandler, unauthorizedHttpErrorHandler) {
 
         describe('[errorHandlingConfiguration]', function () {
 
@@ -12,12 +12,16 @@
                 it('should be function', function () {
                     expect(configuration.configure).toBeFunction();
                 });
+                
+                it('should register unauthorizedHttpErrorHandler for 401 error', function () {
+                    configuration.configure();
+                    expect(errorHandlerRegistrator.registerHandler).toHaveBeenCalledWith(401, unauthorizedHttpErrorHandler);
+                });
 
                 it('should register serviceUnavailableHttpErrorHandler for 503 error', function () {
                     configuration.configure();
                     expect(errorHandlerRegistrator.registerHandler).toHaveBeenCalledWith(503, serviceUnavailableHttpErrorHandler);
                 });
-
             });
 
         });
