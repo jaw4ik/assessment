7ef9bc875be1a3709c4db8f18a20e8f115456ed4
jaw4ik <urec.savchuk@gmail.com>
2013-11-18 14:04:26 +0000

    add routingManager
    Former-commit-id: 19ba3548396045be8c34c446475061f1956a26db

---
+++ sources/easygenerator.Web/Templates/Quiz/App/xApi/routingManager.spec.js
@@ -0,0 +1,47 @@
+﻿define(['xApi/routingManager', 'plugins/router'],
+    function (routingManager, router) {
+
+        describe('viewModel [routingManager]', function () {
+
+            it('should be defined', function () {
+                expect(routingManager).toBeDefined();
+            });
+
+            describe('mapRoutes:', function () {
+
+                it('should be function', function () {
+                    expect(routingManager.mapRoutes).toBeFunction();
+                });
+
+                it('should map routes', function () {
+                    spyOn(router, 'map');
+                    routingManager.mapRoutes();
+                    expect(router.map).toHaveBeenCalled();
+                });
+
+                it('should change routeGuard', function () {
+                    var guard = router.guardRoute;
+                    routingManager.mapRoutes();
+                    expect(router.guardRoute).not.toBe(guard);
+                });
+
+            });
+
+            describe('removeRoutes:', function() {
+
+                it('should be function', function() {
+                    expect(routingManager.removeRoutes).toBeFunction();
+                });
+
+                it('should change routeGuard', function () {
+                    var guard = router.guardRoute;
+                    routingManager.removeRoutes();
+                    expect(router.guardRoute).not.toBe(guard);
+                });
+
+            });
+
+        });
+
+    }
+);
