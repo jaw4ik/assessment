2529ba7b1bda166ff38831b65db4e69ea46d4c8f
andriydemchuk <andrewdemchuk@gmail.com>
2013-11-22 14:50:45 +0000

    remove app-sprite.png
    Former-commit-id: 2ba5b19847e26ed05e7ccaa6e9de4abf6a27e33b

--- sources/easygenerator.Web/Scripts/knockoutBindings/dropDownBinding.js
+++ sources/easygenerator.Web/Scripts/knockoutBindings/dropDownBinding.js
@@ -3,15 +3,18 @@
     init: function (element) {
         var elem = $(element),
             html = $('html'),
+            dropDownHolder = 'header-menu-list-item',
             dropDownList = 'dropdown-list';
 
         elem.click(function (e) {
             $('.' + dropDownList).toggle();
+            $('.' + dropDownHolder).toggleClass('active');
             e.stopPropagation();
         });
 
         html.click(function () {
             $('.' + dropDownList).hide();
+            $('.' + dropDownHolder).removeClass('active');
         });
 
     },
