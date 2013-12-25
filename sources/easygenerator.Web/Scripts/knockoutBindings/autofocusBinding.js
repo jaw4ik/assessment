e84a19e40693256d7ac00a524f84e0408f76bbcc
YuliaSavchuk <yulia.savchuk3@gmail.com>
2013-12-25 12:44:21 +0000

    EWD-22962 - Title is selected after losing the focus
    Former-commit-id: 4a9984fc78f3e3996632bd1a618ea06bfa73ae7b

--- sources/easygenerator.Web/Scripts/knockoutBindings/autofocusBinding.js
+++ sources/easygenerator.Web/Scripts/knockoutBindings/autofocusBinding.js
@@ -7,27 +7,47 @@
             autoselect = ko.unwrap(valueAccessor().autoselect) || false;
 
         setTimeout(function () {
-            
             if (focus) {
                 $(element).focus();
             }
 
             if (autoselect) {
-                var doc = window.document, sel, range;
-                if (window.getSelection && doc.createRange) {
-                    sel = window.getSelection();
-                    range = doc.createRange();
-                    range.selectNodeContents(element);
-                    sel.removeAllRanges();
-                    sel.addRange(range);
-                } else if (doc.body.createTextRange) {
-                    range = doc.body.createTextRange();
-                    range.moveToElementText(element);
-                    range.select();
-                }
+                selectElement(element);
+                $(element).blur(onBlur);
             }
-
         }, 0);
+
+        function onBlur() {
+            clearSelection();
+            $(element).off('blur', onBlur);
+        }
+
+        function clearSelection() {
+            if (window.getSelection) {
+                if (window.getSelection().empty) {
+                    window.getSelection().empty();
+                } else if (window.getSelection().removeAllRanges) {
+                    window.getSelection().removeAllRanges();
+                }
+            } else if (document.selection) {
+                document.selection.empty();
+            }
+        }
+
+        function selectElement(elem) {
+            var doc = window.document, sel, range;
+            if (window.getSelection && doc.createRange) {
+                sel = window.getSelection();
+                range = doc.createRange();
+                range.selectNodeContents(elem);
+                sel.removeAllRanges();
+                sel.addRange(range);
+            } else if (doc.body.createTextRange) {
+                range = doc.body.createTextRange();
+                range.moveToElementText(elem);
+                range.select();
+            }
+        }
     },
     update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
         // This will be called once when the binding is first applied to an element,
