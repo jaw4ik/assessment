5f931fb796930679956983e5db8c1e68d9729fbf
YuliaSavchuk <yulia.savchuk3@gmail.com>
2014-01-06 13:20:33 +0000

    EWD-22399 - SCORM 1.2: Create minimum SCORM conformant package - added missing files
    Former-commit-id: 2d21f3975ab8c40cb2cfe607beb69c383a256eb0

---
+++ sources/easygenerator.Web/App/viewmodels/courses/deliveringActions/publish.js
@@ -0,0 +1,11 @@
+﻿define(['constants'], function (constants) {
+    var viewModel = {
+        state: ko.observable()
+    };
+
+    viewModel.isDelivering = ko.computed(function () {
+        return this.state() === constants.deliveringStates.building || this.state() === constants.deliveringStates.publishing;
+    }, viewModel);
+
+    return viewModel;
+});
