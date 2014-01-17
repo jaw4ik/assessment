a38fb794620e70436ad2334c37d876b81f735695
BOBO4KA <kuchuk.v@gmail.com>
2014-01-17 09:11:37 +0000

    EWD-23042 - Track two steps of sign up (Google Analytics)
    Former-commit-id: fae5305d4c3789a0d9c2680fc4925ec8b74f8550

--- sources/easygenerator.Web/Scripts/account/analytics.js
+++ sources/easygenerator.Web/Scripts/account/analytics.js
@@ -6,7 +6,7 @@
         return {
             trackEvent: function (eventName, eventProperties) {
                 var deferred = jQuery.Deferred();
-                
+
                 var mixpanel = window.mixpanel;
                 if (mixpanel) {
                     var username = eventProperties.username;
@@ -14,7 +14,7 @@
 
                     if (username) {
                         mixpanel.identify(username);
-                        if (eventName == application.constants.events.signup) {
+                        if (eventName == application.constants.events.signupSecondStep) {
                             mixpanel.alias(username);
                             mixpanel.people.set({
                                 "$email": username,
@@ -44,10 +44,34 @@
         };
     }
 
+    function googleAnalyticsProvider() {
+        return {
+            trackEvent: function (eventName, eventProperties) {
+                var deferred = jQuery.Deferred();
+                var gaq = window._gaq;
+
+                if (gaq) {
+                    gaq.push(['_set', 'hitCallback', function () {
+                        deferred.resolve();
+                    }]);
+                    gaq.push(['_trackEvent', 'Account', eventName]);
+                } else {
+                    deferred.resolve();
+                }
+
+                return deferred.promise();
+            }
+        };
+    }
+
     var mixpanelAnalyticesProvider = mixpanelAnalyticsProvider();
+    var googleAnalyticesProvider = googleAnalyticsProvider();
 
     application.trackEvent = function (eventName, eventProperties) {
-        return mixpanelAnalyticesProvider.trackEvent(eventName, eventProperties);
+        return jQuery.when(
+            mixpanelAnalyticesProvider.trackEvent(eventName, eventProperties),
+            googleAnalyticesProvider.trackEvent(eventName, eventProperties)
+        );
     };
 
 }(app))
