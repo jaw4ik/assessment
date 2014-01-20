863beb7aab009221cd9e168585fcdbd0b21607fc
andreydrebot <a.drebot@gmail.com>
2014-01-20 13:39:34 +0000

    Former-commit-id: 63e01acc5a7b916643518f9ed6c570ce0f781c1d

--- sources/easygenerator.Web/Components/ActionFilters/Authorization/RequireAccessAttribute.cs
+++ sources/easygenerator.Web/Components/ActionFilters/Authorization/RequireAccessAttribute.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using easygenerator.DomainModel.Entities;
 using easygenerator.DomainModel.Repositories;
-using easygenerator.Web.Components.ActionResults;
 
 namespace easygenerator.Web.Components.ActionFilters.Authorization
 {
@@ -40,7 +39,7 @@ namespace easygenerator.Web.Components.ActionFilters.Authorization
             var user = UserRepository.GetUserByEmail(httpContext.User.Identity.Name);
             if (!user.HasAccess(AccessType))
             {
-                authorizationContext.Result = new ForbiddenResult(ErrorMessageResourceKey);
+                authorizationContext.Result = String.IsNullOrWhiteSpace(ErrorMessageResourceKey) ? new HttpStatusCodeResult(403) : new HttpStatusCodeResult(403, ErrorMessageResourceKey);
             }
         }
     }
