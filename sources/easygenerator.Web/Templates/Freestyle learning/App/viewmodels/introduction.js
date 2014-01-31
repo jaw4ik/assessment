acc594f1f52c316be8e8d448519fd0524c8a31f3
NadiaGamula <nadia.zakharova@gmail.com>
2014-01-31 14:12:30 +0000

    EWD-23061 - Add introduction content to Freestyle learning
    Former-commit-id: 4ce2cfa7a4d404943e7cf90a48b4df6e5cfc5f97

--- sources/easygenerator.Web/Templates/Freestyle learning/App/viewmodels/introduction.js
+++ sources/easygenerator.Web/Templates/Freestyle learning/App/viewmodels/introduction.js
@@ -3,16 +3,18 @@
     var title = null,
         content = null,
 
+        canActivate = function() {
+            if (context.course.hasIntroductionContent == false) {
+                return { redirect: '#objectives' };
+            }
+            return true;
+        },
+
         activate = function () {
             this.title = "\"" + context.course.title + "\"";
 
             var that = this;
             return Q.fcall(function() {
-                if (context.course.hasIntroductionContent == false) {
-                    router.navigate('objectives');
-                    return;
-                }
-
                 return http.get('content/content.html').then(function (response) {
                     that.content = response;
                 }).fail(function () {
@@ -31,6 +33,7 @@
         content: content,
 
         startCourse: startCourse,
+        canActivate: canActivate,
         activate: activate
     };
 });
--- sources/easygenerator.Web/Templates/Freestyle learning/App/viewmodels/introduction.spec.js
+++ sources/easygenerator.Web/Templates/Freestyle learning/App/viewmodels/introduction.spec.js
@@ -22,8 +22,38 @@
             });
         });
 
+        describe('canActivate:', function() {
+            it('should be function', function () {
+                expect(viewModel.canActivate).toBeFunction();
+            });
+
+            describe('when course has introduction page', function () {
+                beforeEach(function() {
+                    context.course.hasIntroductionContent = true;
+                });
+
+                it('should return true', function () {
+                    var result = viewModel.canActivate();
+                    expect(result).toBeTruthy();
+                });
+            });
+
+            describe('when course hasn\'t introduction page', function() {
+                beforeEach(function () {
+                    context.course.hasIntroductionContent = false;
+                });
+
+                it('should redirect to objectives', function () {
+                    var result = viewModel.canActivate();
+                    expect(result.redirect).toBe('#objectives');
+                });
+            });
+        });
+
         describe('activate:', function () {
-            it('should be defined', function () {
+            var getDeferred;
+
+            it('should be function', function () {
                 expect(viewModel.activate).toBeFunction();
             });
 
@@ -39,85 +69,59 @@
                 expect(viewModel.title).toBe('"some title"');
             });
 
-            describe('when course hasn\'t introduction', function () {
+            it('should get content from file \'content/content.html\'', function () {
+                getDeferred = Q.defer();
+                spyOn(http, 'get').andReturn(getDeferred.promise);
+                getDeferred.resolve();
+
+                var promise = viewModel.activate();
+                waitsFor(function () {
+                    return !promise.isPending();
+                });
+                runs(function () {
+                    expect(http.get).toHaveBeenCalledWith('content/content.html');
+                });
+            });
+
+            describe('when it is possible to get content', function () {
                 beforeEach(function () {
-                    context.course.hasIntroductionContent = false;
+                    getDeferred = Q.defer();
+                    spyOn(http, 'get').andReturn(getDeferred.promise);
+                    getDeferred.resolve('some content');
                 });
 
-                it('should navigate to objectives', function () {
-                    spyOn(router, 'navigate');
-
+                it('should set viewModel content property', function () {
                     var promise = viewModel.activate();
                     waitsFor(function () {
                         return !promise.isPending();
                     });
                     runs(function () {
-                        expect(router.navigate).toHaveBeenCalledWith('objectives');
+                        expect(viewModel.content).toBe('some content');
                     });
                 });
             });
 
-            describe('when course has introduction', function () {
-                var getDeferred;
-
+            describe('it is not possible to get content', function () {
                 beforeEach(function () {
-                    context.course.hasIntroductionContent = true;
-                });
-
-                it('should get content from file \'content/content.html\'', function () {
                     getDeferred = Q.defer();
                     spyOn(http, 'get').andReturn(getDeferred.promise);
-                    getDeferred.resolve();
+                    getDeferred.reject();
+                });
 
+                it('should set viewModel content property to empty string', function () {
                     var promise = viewModel.activate();
                     waitsFor(function () {
                         return !promise.isPending();
                     });
                     runs(function () {
-                        expect(http.get).toHaveBeenCalledWith('content/content.html');
-                    });
-                });
-
-                describe('when it is possible to get content', function () {
-                    beforeEach(function () {
-                        getDeferred = Q.defer();
-                        spyOn(http, 'get').andReturn(getDeferred.promise);
-                        getDeferred.resolve('some content');
-                    });
-
-                    it('should set viewModel content property', function () {
-                        var promise = viewModel.activate();
-                        waitsFor(function () {
-                            return !promise.isPending();
-                        });
-                        runs(function () {
-                            expect(viewModel.content).toBe('some content');
-                        });
-                    });
-                });
-
-                describe('it is not possible to get content', function () {
-                    beforeEach(function () {
-                        getDeferred = Q.defer();
-                        spyOn(http, 'get').andReturn(getDeferred.promise);
-                        getDeferred.reject();
-                    });
-
-                    it('should set viewModel content property to empty string', function () {
-                        var promise = viewModel.activate();
-                        waitsFor(function () {
-                            return !promise.isPending();
-                        });
-                        runs(function () {
-                            expect(viewModel.content).toBe('');
-                        });
+                        expect(viewModel.content).toBe('');
                     });
                 });
             });
         });
 
         describe('startCourse:', function () {
-            it('should be defined', function () {
+            it('should be function', function () {
                 expect(viewModel.startCourse).toBeFunction();
             });
 
