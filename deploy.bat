1fa7b813c8f20f011c262873dee4d3a00bfd5d4b
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2014-03-06 00:49:12 +0000

    oO завтикав, що на CI msTest - не обновляли =) Вертаю старий батнік =) Все одно цей комент ніхто не прочитає) Але якщо все таки: гарного тобі дня, друг ;)
    Former-commit-id: 93b1968ee62364029dd32183979cf958eefeef93

--- deploy.bat
+++ deploy.bat
@@ -24,9 +24,9 @@ IF NOT %ERRORLEVEL% == 0 GOTO ERROR
 
 ECHO Running .Net unit tests...
 
-"%VS120COMNTOOLS%..\IDE\mstest.exe" /testcontainer:sources\easygenerator.DomainModel.Tests\bin\Debug\easygenerator.DomainModel.Tests.dll
+"%VS110COMNTOOLS%..\IDE\mstest.exe" /testcontainer:sources\easygenerator.DomainModel.Tests\bin\Debug\easygenerator.DomainModel.Tests.dll
 IF NOT %ERRORLEVEL% == 0 GOTO ERROR
-"%VS120COMNTOOLS%..\IDE\mstest.exe" /testcontainer:sources\easygenerator.Web.Tests\bin\Debug\easygenerator.Web.Tests.dll 
+"%VS110COMNTOOLS%..\IDE\mstest.exe" /testcontainer:sources\easygenerator.Web.Tests\bin\Debug\easygenerator.Web.Tests.dll 
 IF NOT %ERRORLEVEL% == 0 GOTO ERROR
 
 :: echo Running Jasmine tests...
