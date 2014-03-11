0bdcacc602fde4f8f5d921685ebd22d47f42631a
LeonidPelyhivskyi <plo.91ua@gmail.com>
2014-03-11 07:33:23 +0000

    EWD-23306 - AutomaticMigrationsDisabledException
    Former-commit-id: b58c063560366d6e0c500ca2b2ea8158bafd38ee

--- sources/easygenerator.DataAccess/Migrations/201402251153129_ClearDefaultUserNameInUsersTable.cs
+++ sources/easygenerator.DataAccess/Migrations/201402251153129_ClearDefaultUserNameInUsersTable.cs
@@ -3,7 +3,7 @@ namespace easygenerator.DataAccess.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class ClearDefaultUserNameInUsersTable : DbMigration
     {
         public override void Up()
         {
