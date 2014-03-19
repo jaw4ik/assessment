9772f150d5e71a95bc52ab6a6ceca6e6327e50f9
dmitry-zaets <dmitry.zaets@gmail.com>
2014-03-19 09:38:48 +0000

    EWD-23313 - xAPI and IE9: Simple course Template
    Former-commit-id: f284f5387ad81fdd3c11c48c296885be21020edc

--- sources/easygenerator.Web/Templates/Simple course/App/xApi/viewmodels/login.js
+++ sources/easygenerator.Web/Templates/Simple course/App/xApi/viewmodels/login.js
@@ -65,7 +65,7 @@
                 }
 
                 var url = pageUrl + '?course_id=' + context.course.id;
-                var actor = xApiInitializer.createActor(viewModel.username, viewModel.usermail);
+                var actor = xApiInitializer.createActor(viewModel.username(), viewModel.usermail());
                 xApiInitializer.init(actor, title, url).then(function () {
                     startCourse();
                 }).fail(function (reason) {
