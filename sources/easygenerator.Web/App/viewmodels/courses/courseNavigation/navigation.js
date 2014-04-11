90c99b6a08c1d4af690600b7de01b47c483c0274
YuliaSavchuk <yulia.savchuk3@gmail.com>
2014-04-11 08:03:34 +0000

    EWD-23430 - Create button to preview course
    Former-commit-id: 2b51a63bd7c54d1cae0401dd97b93540107ba641

--- sources/easygenerator.Web/App/viewmodels/courses/courseNavigation/navigation.js
+++ sources/easygenerator.Web/App/viewmodels/courses/courseNavigation/navigation.js
@@ -1,17 +1,29 @@
-﻿define(['viewmodels/courses/courseNavigation/items/create', 'viewmodels/courses/courseNavigation/items/design', 'viewmodels/courses/courseNavigation/items/publish'],
-    function (CreateNavigationItem, DesignNavigationItem, PublishNavigationItem) {
-        var items = [];
+﻿define(['viewmodels/courses/courseNavigation/items/create', 'viewmodels/courses/courseNavigation/items/design', 'viewmodels/courses/courseNavigation/items/publish', 'eventTracker', 'plugins/router'],
+    function (CreateNavigationItem, DesignNavigationItem, PublishNavigationItem, eventTracker, router) {
+        "use strict";
+
+        var events = {
+            previewCourse: 'Preview course'
+        };
+
+        var navigationItems = [];
         var activate = function () {
-            this.items = [
+            this.navigationItems = [
                 new CreateNavigationItem(),
                 new DesignNavigationItem(),
                 new PublishNavigationItem()
             ];
         };
 
+        var previewCourse = function () {
+            eventTracker.publish(events.previewCourse);
+            router.openUrl('/preview/' + router.routeData().courseId);
+        };
+
         return {
             activate: activate,
-            items: items
+            previewCourse: previewCourse,
+            navigationItems: navigationItems
         };
 
     });
--- sources/easygenerator.Web/App/viewmodels/courses/courseNavigation/navigation.spec.js
+++ sources/easygenerator.Web/App/viewmodels/courses/courseNavigation/navigation.spec.js
@@ -1,22 +1,52 @@
 ﻿define(['viewmodels/courses/courseNavigation/navigation'],
     function (navigation) {
+        "use strict";
+
+        var eventTracker = require('eventTracker');
+        var router = require('plugins/router');
 
         describe('viewmodel [courseNavigation]', function () {
 
-            describe('items:', function() {
-                it('should be array', function() {
-                    expect(navigation.items).toBeArray();
+            beforeEach(function () {
+                spyOn(eventTracker, 'publish');
+                spyOn(router, 'openUrl');
+            });
+
+            describe('items:', function () {
+                it('should be array', function () {
+                    expect(navigation.navigationItems).toBeArray();
                 });
             });
 
-            describe('activate:', function() {
-                it('should be function', function() {
+            describe('activate:', function () {
+                it('should be function', function () {
                     expect(navigation.activate).toBeFunction();
                 });
 
-                it('should fill items collection', function() {
+                it('should fill items collection', function () {
                     navigation.activate();
-                    expect(navigation.items.length).toBe(3);
+                    expect(navigation.navigationItems.length).toBe(3);
+                });
+            });
+
+            describe('previewCourse', function () {
+                it('should be function', function () {
+                    expect(navigation.previewCourse).toBeFunction();
+                });
+
+                it('should send \'Preview course\' event', function () {
+                    navigation.previewCourse();
+                    expect(eventTracker.publish).toHaveBeenCalledWith('Preview course');
+                });
+
+                it('should open url', function () {
+                    var courseId = 'courseId';
+                    router.routeData({
+                        courseId: courseId
+                    });
+
+                    navigation.previewCourse();
+                    expect(router.openUrl).toHaveBeenCalledWith('/preview/' + courseId);
                 });
             });
         });
