ad5780e93b07ed093ce1070cb7a1805757988881
andreydrebot <a.drebot@gmail.com>
2014-04-18 12:58:48 +0000

    Objectives viewmodel refactoring
    Former-commit-id: 220df7f5e11f3ff570512656d86cd2501017b830

--- sources/easygenerator.Web/App/viewmodels/objectives/objectives.js
+++ sources/easygenerator.Web/App/viewmodels/objectives/objectives.js
@@ -12,142 +12,129 @@
                 deleteObjectives: "Delete selected objectives"
             };
 
-        var
-            objectives = ko.observableArray([]),
-            lastVisitedObjective = '',
-            currentLanguage = '',
+        var viewModel = {
+            objectives: ko.observableArray([]),
 
-            //#region Navigation
-
-            navigateToCreation = function () {
-                eventTracker.publish(events.navigateToCreation);
-                router.navigate('objective/create');
-            },
-
-            navigateToDetails = function (item) {
-                eventTracker.publish(events.navigateToDetails);
-                router.navigate('objective/' + item.id);
-            },
-
-            navigateToCourses = function () {
-                eventTracker.publish(events.navigateToCourses);
-                router.navigate('courses');
-            },
-
-            //#endregion Navigation
-
-            //#region Delete objective
-
-            getSelectedObjectives = function () {
-                return _.reject(objectives(), function (objective) {
-                    return objective.isSelected && !objective.isSelected();
-                });
-            },
-
-            enableDeleteObjectives = ko.computed(function () {
-                return getSelectedObjectives().length > 0;
-            }),
-
-            deleteSelectedObjectives = function () {
-                eventTracker.publish(events.deleteObjectives);
-
-                var selectedObjectives = getSelectedObjectives();
-                if (selectedObjectives.length == 0) {
-                    throw "No selected objectives to delete";
-                }
-
-                if (selectedObjectives.length > 1) {
-                    notify.error(localizationManager.localize('deleteSeveralObjectivesError'));
-                    return undefined;
-                }
-
-                var selectedObjective = selectedObjectives[0];
-
-                if (!selectedObjective.canBeDeleted) {
-                    notify.error(localizationManager.localize('objectiveCannnotBeDeleted'));
-                    return undefined;
-                }
-
-                objectiveRepository.removeObjective(selectedObjective.id).then(function () {
-                    objectives(_.reject(objectives(), function (objective) {
-                        return objective.id === selectedObjective.id;
-                    }));
-                    notify.saved();
-                });
-            },
-
-            //#endregion Delete objective
-
-            //#region Objective selection
-
-            toggleObjectiveSelection = function (objective) {
-
-                if (_.isNullOrUndefined(objective)) {
-                    throw 'Objective is null or undefined';
-                }
-
-                if (!ko.isObservable(objective.isSelected)) {
-                    throw 'Objective does not have isSelected observable';
-                }
-
-                objective.isSelected(!objective.isSelected());
-                eventTracker.publish(objective.isSelected() ? events.selectObjective : events.unselectObjective);
-            },
-
-            //#endregion Objective selection
-
-             activate = function () {
-
-                 this.lastVisitedObjective = clientContext.get('lastVisitedObjective');
-                 clientContext.set('lastVisitedObjective', null);
-                 this.currentLanguage = localizationManager.currentLanguage;
-
-                 return objectiveRepository.getCollection().then(function (receivedObjectives) {
-                     return courseRepository.getCollection().then(function (courses) {
-                         var includedObjectives = _.chain(courses).map(function (course) {
-                             return course.objectives;
-                         }).flatten().uniq().value();
-
-                         var array = _.chain(receivedObjectives)
-                             .sortBy(function (item) { return -item.createdOn; })
-                             .map(function (item) {
-                                 return {
-                                     id: item.id,
-                                     title: item.title,
-                                     image: item.image,
-                                     questionsCount: item.questions.length,
-                                     modifiedOn: item.modifiedOn,
-                                     isSelected: ko.observable(false),
-                                     canBeDeleted: (function (currentItem) {
-                                         if (item.questions.length > 0)
-                                             return false;
-
-                                         return (!_.find(includedObjectives, function (objective) {
-                                             return objective.id === currentItem.id;
-                                         }));
-                                     })(item)
-                                 };
-                             }).value();
-
-                         objectives(array);
-                     });
-                 });
-             };
-
-        return {
-            objectives: objectives,
-            currentLanguage: currentLanguage,
-            
-            toggleObjectiveSelection: toggleObjectiveSelection,
+            lastVisitedObjective: '',
+            currentLanguage: '',
 
             navigateToCreation: navigateToCreation,
             navigateToDetails: navigateToDetails,
             navigateToCourses: navigateToCourses,
 
-            enableDeleteObjectives: enableDeleteObjectives,
             deleteSelectedObjectives: deleteSelectedObjectives,
-            lastVisitedObjective: lastVisitedObjective,
+            toggleObjectiveSelection: toggleObjectiveSelection,
 
             activate: activate
         };
+
+        viewModel.enableDeleteObjectives = ko.computed(function () {
+            return getSelectedObjectives().length > 0;
+        });
+
+        return viewModel;
+
+        function navigateToCreation() {
+            eventTracker.publish(events.navigateToCreation);
+            router.navigate('objective/create');
+        }
+
+        function navigateToDetails(item) {
+            eventTracker.publish(events.navigateToDetails);
+            router.navigate('objective/' + item.id);
+        }
+
+        function navigateToCourses() {
+            eventTracker.publish(events.navigateToCourses);
+            router.navigate('courses');
+        }
+
+        function toggleObjectiveSelection(objective) {
+            if (_.isNullOrUndefined(objective)) {
+                throw 'Objective is null or undefined';
+            }
+
+            if (!ko.isObservable(objective.isSelected)) {
+                throw 'Objective does not have isSelected observable';
+            }
+
+            objective.isSelected(!objective.isSelected());
+            eventTracker.publish(objective.isSelected() ? events.selectObjective : events.unselectObjective);
+        }
+
+        function getSelectedObjectives() {
+            return _.reject(viewModel.objectives(), function (objective) {
+                return objective.isSelected && !objective.isSelected();
+            });
+        }
+
+        function deleteSelectedObjectives() {
+            eventTracker.publish(events.deleteObjectives);
+
+            var selectedObjectives = getSelectedObjectives();
+            if (selectedObjectives.length == 0) {
+                throw "No selected objectives to delete";
+            }
+
+            if (selectedObjectives.length > 1) {
+                notify.error(localizationManager.localize('deleteSeveralObjectivesError'));
+                return undefined;
+            }
+
+            var selectedObjective = selectedObjectives[0];
+
+            if (!selectedObjective.canBeDeleted) {
+                notify.error(localizationManager.localize('objectiveCannnotBeDeleted'));
+                return undefined;
+            }
+
+            objectiveRepository.removeObjective(selectedObjective.id).then(function () {
+                viewModel.objectives(_.reject(viewModel.objectives(), function (objective) {
+                    return objective.id === selectedObjective.id;
+                }));
+                notify.saved();
+            });
+        }
+
+
+        function activate() {
+
+            viewModel.lastVisitedObjective = clientContext.get('lastVisitedObjective');
+            clientContext.set('lastVisitedObjective', null);
+
+            viewModel.currentLanguage = localizationManager.currentLanguage;
+
+            return objectiveRepository.getCollection().then(function (receivedObjectives) {
+                return courseRepository.getCollection().then(function (courses) {
+                    var includedObjectives = _.chain(courses).map(function (course) {
+                        return course.objectives;
+                    }).flatten().uniq().value();
+
+                    var array = _.chain(receivedObjectives)
+                        .sortBy(function (item) { return -item.createdOn; })
+                        .map(function (item) {
+                            return {
+                                id: item.id,
+                                title: item.title,
+                                image: item.image,
+                                questionsCount: item.questions.length,
+                                modifiedOn: item.modifiedOn,
+                                isSelected: ko.observable(false),
+                                canBeDeleted: (function (currentItem) {
+                                    if (item.questions.length > 0)
+                                        return false;
+
+                                    return (!_.find(includedObjectives, function (objective) {
+                                        return objective.id === currentItem.id;
+                                    }));
+                                })(item)
+                            };
+                        }).value();
+
+                    viewModel.objectives(array);
+                });
+            });
+        };
+
+
     });
