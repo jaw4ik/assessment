4836d79448fd6a7157006f2d8d3f093b10d34b64
BOBO4KA <kuchuk.v@gmail.com>
2014-04-25 12:55:03 +0000

    EWD-23474 - There is no spinner on signin button if tracking was blocked.
    Former-commit-id: e202a1bdff626b51d70d690ed631e75c7536cc54

--- sources/easygenerator.Web/Scripts/account/signin.js
+++ sources/easygenerator.Web/Scripts/account/signin.js
@@ -3,144 +3,131 @@
 app.signinViewModel = function () {
     "use strict";
 
+    var viewModel = {
+        username: ko.observable(),
+        password: ko.observable(),
+        isPasswordVisible: ko.observable(),
+        forgotPasswordSent: ko.observable(false),
+        forgotPasswordEnabled: ko.observable(true),
+        isSigninRequestPending: ko.observable(false),
+        errorMessage: ko.observable(),
 
-    var
-        username = (function () {
-            var value = ko.observable();
-            value.isValid = ko.computed(function () {
-                return !!(value() && app.constants.patterns.email.test(value().trim()));
-            });
-            value.isModified = ko.observable(false);
-            value.markAsModified = function () {
-                value.isModified(true);
-            };
-            return value;
-        })(),
-        password = ko.observable(),
+        togglePasswordVisibility: togglePasswordVisibility,
+        submit: submit,
+        forgotPassword: forgotPassword
+    };
 
-        isPasswordVisible = ko.observable(),
-        togglePasswordVisibility = function () {
-            isPasswordVisible(!isPasswordVisible());
-        },
+    viewModel.username.isModified = ko.observable(false);
+    viewModel.username.markAsModified = markAsModified;
 
-        forgotPasswordSent = ko.observable(false),
-        errorMessage = ko.observable(),
-        hasError = ko.computed(function () {
-            return !!(errorMessage() && errorMessage().length);
-        }),
-
-        canSubmit = ko.computed(function () {
-            forgotPasswordSent(false);
-            return !!(username.isValid() && password() && password().trim().length);
-        }),
-
-        submit = function () {
-            if (!canSubmit()) {
-                return;
-            }
-
-            errorMessage("");
-            forgotPasswordSent(false);
-
-            var data = {
-                username: username().trim().toLowerCase(),
-                password: password()
-            };
-
-            var that = this;
-
-            that.isSigninRequestPending(true);
-
-            $.ajax({
-                url: '/api/user/signin',
-                data: data,
-                type: 'POST'
-            }).done(function (response) {
-                if (response) {
-                    if (response.success) {
-                        app.trackEvent(app.constants.events.signin, { username: data.username }).done(function () {
-                            app.openHomePage();
-                        });
-                    } else {
-                        if (response.message) {
-                            that.isSigninRequestPending(false);
-                            that.errorMessage(response.message);
-                        } else {
-                            throw 'Error message is not defined';
-                        }
-                    }
-                } else {
-                    throw 'Response is not an object';
-                }
-            }).fail(function (reason) {
-                that.isSigninRequestPending(false);
-                that.errorMessage(reason);
-            });
-        },
-
-        forgotPasswordEnabled = ko.observable(true),
-        forgotPassword = function () {
-            if (!username.isValid() || !forgotPasswordEnabled()) {
-                return;
-            }
-
-            var data = {
-                email: username()
-            };
-
-            var that = this;
-
-            forgotPasswordEnabled(false);
-
-            $.ajax({
-                url: '/api/user/forgotpassword',
-                data: data,
-                type: 'POST'
-            }).done(function (response) {
-                if (response) {
-                    if (response.success) {
-                        that.forgotPasswordSent(true);
-                        that.errorMessage('');
-                    } else {
-                        if (response.message) {
-                            that.errorMessage(response.message);
-                        } else {
-                            throw 'Error message is not defined';
-                        }
-                    }
-                } else {
-                    throw 'Response is not an object';
-                }
-            }).fail(function (reason) {
-                that.errorMessage(reason);
-            }).always(function () {
-                forgotPasswordEnabled(true);
-            });
-        },
-
-        isSigninRequestPending = ko.observable(false);
-
-    ko.computed(function () {
-        var subscription = username() & password();
-        errorMessage("");
+    viewModel.username.isValid = ko.computed(function () {
+        return !!(viewModel.username() && app.constants.patterns.email.test(viewModel.username().trim()));
     });
 
-    return {
-        username: username,
-        password: password,
-        isPasswordVisible: isPasswordVisible,
-        togglePasswordVisibility: togglePasswordVisibility,
+    viewModel.hasError = ko.computed(function () {
+        return !!(viewModel.errorMessage() && viewModel.errorMessage().length);
+    });
 
-        canSubmit: canSubmit,
-        submit: submit,
+    viewModel.canSubmit = ko.computed(function () {
+        viewModel.forgotPasswordSent(false);
+        return !!(viewModel.username.isValid() && viewModel.password() && viewModel.password().trim().length);
+    });
 
-        forgotPasswordEnabled: forgotPasswordEnabled,
-        forgotPassword: forgotPassword,
-        forgotPasswordSent: forgotPasswordSent,
+    ko.computed(function () {
+        var subscription = viewModel.username() & viewModel.password();
+        viewModel.errorMessage("");
+    });
 
-        hasError: hasError,
-        errorMessage: errorMessage,
-        isSigninRequestPending: isSigninRequestPending
-    };
+    return viewModel;
+
+    function markAsModified() {
+        viewModel.username.isModified(true);
+    }
+
+    function togglePasswordVisibility() {
+        viewModel.isPasswordVisible(!viewModel.isPasswordVisible());
+    }
+
+    function submit() {
+        if (!viewModel.canSubmit()) {
+            return;
+        }
+
+        viewModel.errorMessage("");
+        viewModel.forgotPasswordSent(false);
+        viewModel.isSigninRequestPending(true);
+
+        var data = {
+            username: viewModel.username().trim().toLowerCase(),
+            password: viewModel.password()
+        };
+
+        var requestArgs = {
+            url: '/api/user/signin',
+            data: data,
+            type: 'POST'
+        };
+
+        $.ajax(requestArgs).done(function (response) {
+            if (response) {
+                if (response.success) {
+                    app.trackEvent(app.constants.events.signin, { username: data.username }).done(function () {
+                        app.openHomePage();
+                    });
+                } else {
+                    if (response.message) {
+                        viewModel.isSigninRequestPending(false);
+                        viewModel.errorMessage(response.message);
+                    } else {
+                        throw 'Error message is not defined';
+                    }
+                }
+            } else {
+                throw 'Response is not an object';
+            }
+        }).fail(function (reason) {
+            viewModel.isSigninRequestPending(false);
+            viewModel.errorMessage(reason);
+        });
+    }
+
+    function forgotPassword() {
+        if (!viewModel.username.isValid() || !viewModel.forgotPasswordEnabled()) {
+            return;
+        }
+
+        viewModel.forgotPasswordEnabled(false);
+
+        var data = {
+            email: username()
+        };
+
+        var requestArgs = {
+            url: '/api/user/forgotpassword',
+            data: data,
+            type: 'POST'
+        };
+
+        $.ajax(requestArgs).done(function (response) {
+            if (response) {
+                if (response.success) {
+                    viewModel.forgotPasswordSent(true);
+                    viewModel.errorMessage('');
+                } else {
+                    if (response.message) {
+                        viewModel.errorMessage(response.message);
+                    } else {
+                        throw 'Error message is not defined';
+                    }
+                }
+            } else {
+                throw 'Response is not an object';
+            }
+        }).fail(function (reason) {
+            viewModel.errorMessage(reason);
+        }).always(function () {
+            viewModel.forgotPasswordEnabled(true);
+        });
+    }
 
 };
