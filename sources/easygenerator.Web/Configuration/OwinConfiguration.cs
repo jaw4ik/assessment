812795d78e920733cf06ddae9096eed5df5f532f
andreydrebot <a.drebot@gmail.com>
2014-05-21 05:39:06 +0000

    EWD-23524 - webSockets errors.
    Former-commit-id: 4a4fe51834d2893d04565947720dc3a5128db88b

--- sources/easygenerator.Web/Configuration/OwinConfiguration.cs
+++ sources/easygenerator.Web/Configuration/OwinConfiguration.cs
@@ -1,39 +1,15 @@
-﻿using System;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Elmah;
-using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
-using Microsoft.Owin;
-using Microsoft.Owin.Diagnostics;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(easygenerator.Web.Configuration.OwinConfiguration))]
 
 namespace easygenerator.Web.Configuration
 {
-    public class ErrorHandlingPipelineModule : HubPipelineModule
-    {
-        protected override void OnIncomingError(ExceptionContext exceptionContext, IHubIncomingInvokerContext invokerContext)
-        {
-            ErrorLog.GetDefault(null).Log(new Error(exceptionContext.Error));
-            if (exceptionContext.Error.InnerException != null)
-            {
-                ErrorLog.GetDefault(null).Log(new Error(exceptionContext.Error.InnerException));
-            }
-            base.OnIncomingError(exceptionContext, invokerContext);
-        }
-    }
-
     public class OwinConfiguration
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             app.MapSignalR();
-            app.UseErrorPage();
-            GlobalHost.HubPipeline.AddModule(new ErrorHandlingPipelineModule());
-
         }
     }
 }
