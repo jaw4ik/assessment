e60f71efc71e664e92cde3664b46a711ba3f0885
YuliaSavchuk <yulia.savchuk3@gmail.com>
2014-06-12 14:18:39 +0000

    EWD-23624 - Apply course entity changes immediately
    Former-commit-id: 0dc146bb14c97a1e9ac1d7cc5566620ff07d3b1d

---
+++ sources/easygenerator.Web/App/synchronization/handlers/questionEventHandler.js
@@ -0,0 +1,10 @@
+﻿define(['synchronization/handlers/question/titleUpdated', 'synchronization/handlers/question/contentUpdated'],
+    function (titleUpdated, contentUpdated) {
+        "use strict";
+
+        return {
+            titleUpdated: titleUpdated,
+            contentUpdated: contentUpdated
+        };
+
+    });
