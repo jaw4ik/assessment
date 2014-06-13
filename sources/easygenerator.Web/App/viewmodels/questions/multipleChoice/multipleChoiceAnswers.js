f1d8868077c78af08b0c1a9c7d68647d8117dc2e
LeonidPelyhivskyi <plo.91ua@gmail.com>
2014-06-13 12:31:38 +0000

    EWD-23536 - Multiple choice question
    Former-commit-id: 986dfd666e62677869f52e16b9b56f7ee09c08ef

--- sources/easygenerator.Web/App/viewmodels/questions/multipleChoice/multipleChoiceAnswers.js
+++ sources/easygenerator.Web/App/viewmodels/questions/multipleChoice/multipleChoiceAnswers.js
@@ -10,20 +10,22 @@
             var answersViewModel = vmAnswers(questionId, answers);
 
             answersViewModel.toggleCorrectness = function (answer) {
-                    eventTracker.publish(events.toggleAnswerCorrectness);
-                    var currentCorrectAnswer = _.find(answersViewModel.answers(), function (answerOption) {
-                        return answerOption.isCorrect();
-                    });
-                    if (!_.isNullOrUndefined(currentCorrectAnswer) && currentCorrectAnswer != answer) {
-                        currentCorrectAnswer.isCorrect(false);
+                eventTracker.publish(events.toggleAnswerCorrectness);
+                var currentCorrectAnswer = _.find(answersViewModel.answers(), function (answerOption) {
+                    return answerOption.isCorrect();
+                });
+                if (!_.isNullOrUndefined(currentCorrectAnswer) && currentCorrectAnswer != answer) {
+                    currentCorrectAnswer.isCorrect(false);
+                    if (currentCorrectAnswer.text() == currentCorrectAnswer.original.text) {
                         repository.updateCorrectness(questionId, currentCorrectAnswer.id(), false).then(function () {
                             notify.saved();
                         });
                     }
-                    var isCorrect = !answer.isCorrect();
+                }
+                var isCorrect = !answer.isCorrect();
 
-                    answer.isCorrect(isCorrect);
-                };
+                answer.isCorrect(isCorrect);
+            };
 
             return answersViewModel;
         };
