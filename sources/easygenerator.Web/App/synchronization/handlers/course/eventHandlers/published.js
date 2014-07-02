bf3b1931771efdd1133b368c1005452130127ddd
YuliaSavchuk <yulia.savchuk3@gmail.com>
2014-07-02 07:07:15 +0000

    EWD-23740 - Publish course changes are not applied for collaborator
    Former-commit-id: 1fabc8c1e7cf62a76f26a0058311d26fdc434ab3

--- sources/easygenerator.Web/App/synchronization/handlers/course/eventHandlers/published.js
+++ sources/easygenerator.Web/App/synchronization/handlers/course/eventHandlers/published.js
@@ -2,7 +2,7 @@
     function (guard, app, constants, dataContext) {
         "use strict";
 
-        return function(courseId, publicationUrl) {
+        return function (courseId, publicationUrl) {
             guard.throwIfNotString(courseId, 'CourseId is not a string');
             guard.throwIfNotString(publicationUrl, 'PublicationUrl is not a string');
 
--- sources/easygenerator.Web/App/synchronization/handlers/course/handler.js
+++ sources/easygenerator.Web/App/synchronization/handlers/course/handler.js
@@ -1,6 +1,6 @@
 ﻿define(['synchronization/handlers/course/eventHandlers/deleted', 'synchronization/handlers/course/eventHandlers/introductionContentUpdated',
     'synchronization/handlers/course/eventHandlers/objectiveRelated', 'synchronization/handlers/course/eventHandlers/objectivesReordered',
-'synchronization/handlers/course/eventHandlers/objectivesUnrelated', 'synchronization/handlers/course/eventHandlers/objectivesUnrelated',
+'synchronization/handlers/course/eventHandlers/objectivesUnrelated', 'synchronization/handlers/course/eventHandlers/published',
 'synchronization/handlers/course/eventHandlers/templateUpdated', 'synchronization/handlers/course/eventHandlers/titleUpdated',
 'synchronization/handlers/course/eventHandlers/objectivesReplaced'],
     function (deleted, introductionContentUpdated, objectiveRelated, objectivesReordered, objectivesUnrelated, published, templateUpdated, titleUpdated, objectivesReplaced) {
