c511def12397aa696d066d096238f0f7f13eb000
LeonidPelyhivskyi <plo.91ua@gmail.com>
2014-07-18 14:06:45 +0000

    EWD-23703 - Incorrect action after clicking on Enter in answer options in MC and MS
    Former-commit-id: 4fabb968c3d110fb8047f2a3e9dab3e2125bc55f

---
+++ sources/easygenerator.Web/Scripts/knockoutBindings/selectTextInAnswerBinding.js
@@ -0,0 +1,18 @@
+﻿ko.bindingHandlers.selectTextInAnswer = {
+    init: function (element) {
+        var $element = $(element),
+            activeClass = 'active';
+
+        $element.on('blur', function () {
+            $element.removeTextSelection();
+            $element.removeClass(activeClass);
+        });
+
+        $element.click(function () {
+            if (!$element.hasClass(activeClass)) {
+                element.select();
+                $element.addClass(activeClass);
+            }
+        });
+    }
+};
