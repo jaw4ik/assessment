d142ef096dbd619af6728770de562f06a937b57e
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2014-07-21 12:56:06 +0000

    EWD-23717 - Separated "information" content. Not final version of functionality.
    Former-commit-id: 9c79c10355bb3cf327b8e0b31e132b00e5cdce23

---
+++ sources/easygenerator.DomainModel/Entities/Questions/InformationContent.cs
@@ -0,0 +1,11 @@
+﻿namespace easygenerator.DomainModel.Entities.Questions
+{
+    public class InformationContent : Question
+    {
+        protected internal InformationContent() { }
+        protected internal InformationContent(string title, string createdBy)
+            : base(title, createdBy)
+        {
+        }
+    }
+}
