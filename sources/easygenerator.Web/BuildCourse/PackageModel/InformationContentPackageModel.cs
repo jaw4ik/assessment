e249e2962ca984f451cab3261ff5af5eca0f925b
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2014-07-21 12:59:57 +0000

    EWD-23717 - Separated "information" content. Not final version of functionality.
    Former-commit-id: e51bd47d1311887512b60c584ef5d9e83c49c887

---
+++ sources/easygenerator.Web/BuildCourse/PackageModel/InformationContentPackageModel.cs
@@ -0,0 +1,12 @@
+﻿using easygenerator.DomainModel.Entities.Questions;
+
+namespace easygenerator.Web.BuildCourse.PackageModel
+{
+    public class InformationContentPackageModel : QuestionPackageModel
+    {
+        public override string Type
+        {
+            get { return Question.QuestionTypes.InformationContent; }
+        }
+    }
+}
---
+++ sources/easygenerator.Web/Controllers/Api/InformationContentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Mvc;
+using easygenerator.DomainModel;
+using easygenerator.DomainModel.Entities;
+using easygenerator.DomainModel.Entities.Questions;
+using easygenerator.DomainModel.Events;
+using easygenerator.DomainModel.Events.QuestionEvents;
+using easygenerator.Infrastructure;
+using easygenerator.Web.Components;
+using easygenerator.Web.Components.ActionFilters.Permissions;
+using easygenerator.Web.Extensions;
+
+namespace easygenerator.Web.Controllers.Api
+{
+    public class InformationContentController : DefaultController
+    {
+        private readonly IEntityFactory _entityFactory;
+        private readonly IDomainEventPublisher _eventPublisher;
+
+        public InformationContentController(IEntityFactory entityFactory, IDomainEventPublisher eventPublisher)
+        {
+            _entityFactory = entityFactory;
+            _eventPublisher = eventPublisher;
+        }
+
+        [HttpPost]
+        [EntityCollaborator(typeof(Objective))]
+        [Route("api/question/" + Question.QuestionTypes.InformationContent + "/create")]
+        public ActionResult Create(Objective objective, string title)
+        {
+            if (objective == null)
+            {
+                return JsonLocalizableError(Errors.ObjectiveNotFoundError, Errors.ObjectiveNotFoundResourceKey);
+            }
+
+            var question = _entityFactory.InformationContent(title, GetCurrentUsername());
+
+            objective.AddQuestion(question, GetCurrentUsername());
+            _eventPublisher.Publish(new QuestionCreatedEvent(question));
+
+            return JsonSuccess(new { Id = question.Id.ToNString(), CreatedOn = question.CreatedOn });
+        }
+    }
+}
