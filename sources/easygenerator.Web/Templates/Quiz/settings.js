ced107d2ae6d3df98d5f2e0de2aa7e61e68663d0
BOBO4KA <kuchuk.v@gmail.com>
2014-08-19 12:40:46 +0000

    Update templates
    Former-commit-id: 8c46d41e17633b0310e6f07b0a4ffe850c095297

--- sources/easygenerator.Web/Templates/Quiz/settings.js
+++ sources/easygenerator.Web/Templates/Quiz/settings.js
@@ -1,20 +1,25 @@
 ﻿{
-    "logo": {
-        "url": ""
-    },
-    "xApi": {
-        "enabled": false,
-        "lrs": {
-            "uri": "http://cloud.scorm.com/ScormEngineInterface/TCAPI/public/",
-            "credentials": {
-                "username": "",
-                "password": ""
-            },
-          "authenticationRequired": false
-        },
-        "allowedVerbs": []
-    },
-    "masteryScore": {
-        "score": "75"
-    }
+	"logo": {
+		"url": ""
+	},
+	"xApi": {
+		"enabled": false,
+		"lrs": {
+			"uri": "http://cloud.scorm.com/ScormEngineInterface/TCAPI/public/statements?limit=25&related_activities=false&related_agents=false",
+			"authenticationRequired": false,
+			"credentials": {
+				
+			}
+		},
+		"allowedVerbs": ["started",
+		"stopped",
+		"experienced",
+		"mastered",
+		"answered",
+		"passed",
+		"failed"]
+	},
+	"masteryScore": {
+		"score": 75
+	}
 }
