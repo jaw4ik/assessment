f049a34f03bb2ff4cffce5db4c7d75311c6c3f32
andreydrebot <a.drebot@gmail.com>
2014-08-19 10:44:14 +0000

    EWD-23979 - Add Pesonolized learning template into product
    Former-commit-id: 862cf1fe3725c1385ab4593d93a6d9bd105f05ff

--- sources/easygenerator.Web/Templates/Personalized learning/settings.js
+++ sources/easygenerator.Web/Templates/Personalized learning/settings.js
@@ -1,1 +1,1 @@
-{"logo":{"url":""},"xApi":{"enabled":false,"lrs":{"uri":"http://cloud.scorm.com/Scormasdasdasdas","authenticationRequired":false,"credentials":{"username":"","password":""}},"allowedVerbs":["started","stopped","experienced","mastered","answered","passed","failed"]}}
+{"logo":{"url":""},"xApi":{"enabled":false,"lrs":{"uri":"http://cloud.scorm.com/ScormEngineInterface/TCAPI/public","authenticationRequired":false,"credentials":{"username":"","password":""}},"allowedVerbs":["started","stopped","experienced","mastered","answered","passed","failed"]}}
