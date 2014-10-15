89230102a97b62ded8818c0f7c67c9ad16842517
YuliaSavchuk <yulia.savchuk3@gmail.com>
2014-10-15 10:40:48 +0000

    EWD-24150 - Nothing says me that image is loading in Drag and Drop
    Former-commit-id: ee716ab0b6353f026ac4054e8bfbcb9c058e886c

--- sources/easygenerator.Web/App/viewmodels/questions/dragAndDrop/bindingHandlers/backgroundBindingHandler.js
+++ sources/easygenerator.Web/App/viewmodels/questions/dragAndDrop/bindingHandlers/backgroundBindingHandler.js
@@ -1,42 +1,41 @@
 ﻿define(['knockout'], function (ko) {
 
-    ko.bindingHandlers.background = {
-        init: function(element) {
-            $(element)
-                .css('background-position', '0 0')
-                .css('background-repeat', 'no-repeat');
-        },
-        update: function(element, valueAccessor) {
-            var value = valueAccessor();
-            if (value) {
-                var src = value();
-                var image = new Image();
-                image.onload = function () {
+	ko.bindingHandlers.background = {
+		init: function (element) {
+			$(element)
+				.css('background-position', '0 0')
+				.css('background-repeat', 'no-repeat');
+		},
+		update: function (element, valueAccessor) {
+			var value = valueAccessor();
+			if (value) {
+				var src = value();
+				var image = new Image();
+				image.onload = function () {
+					
+					$(element).animate({ opacity: 0 }, 200, function () {
+						$(element).css('background-image', 'url(' + src + ')')
+							.css('width', image.width)
+							.animate({ height: image.height }, 200, function () {
+								if (_.isFunction(value.onload)) {
+									value.onload(image.width, image.height);
+								}
 
-                    $(element).animate({ opacity: 0 }, 200, function() {
-                        $(element).css('background-image', 'url(' + src + ')')
-                            .css('width', image.width)
-                            .animate({ height: image.height }, 200, function () {
-                                $(element).animate({ opacity: 1 }, 300, function () {
-                                    if (_.isFunction(value.onload)) {
-                                        value.onload(image.width, image.height);
-                                    }
-                                });
-                            });
-                    });
-                    
+								$(element).animate({ opacity: 1 }, 300);
+							});
+					});
 
-                    if (ko.isWriteableObservable(value.width)) {
-                        value.width(image.width);
-                    }
+					if (ko.isWriteableObservable(value.width)) {
+						value.width(image.width);
+					}
 
-                    if (ko.isWriteableObservable(value.height)) {
-                        value.height(image.height);
-                    }
-                };
-                image.src = src;
-            }
-        }
-    };
+					if (ko.isWriteableObservable(value.height)) {
+						value.height(image.height);
+					}
+				};
+				image.src = src;
+			}
+		}
+	};
 
 });
