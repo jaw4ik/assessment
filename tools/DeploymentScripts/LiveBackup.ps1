9aae1c20a35930a6e312647df5b451e21cf10206
dmitry-zaets <dmitry.zaets@gmail.com>
2014-11-03 13:29:27 +0000

    Added Live Backup script. Not tested on Live!
    Former-commit-id: 81db5cdb5837be00a618b032e041348a099f3a91

---
+++ tools/DeploymentScripts/LiveBackup.ps1
@@ -0,0 +1,63 @@
+$ErrorActionPreference = "Stop"
+
+$title = "EG Live Backup Script:"
+$message = "Script will stop Live Website and start Maintenance Website.`nAre you sure that you want to run this script?"
+
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+    "Stop Live Website, start Maintenance Website and make all backups."
+
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+    "Exit."
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+
+$result = $host.ui.PromptForChoice($title, $message, $options, 1) 
+
+switch ($result)
+{
+	1 {Exit}
+}
+
+echo "Stop Live website"
+C:\Windows\System32\inetsrv\appcmd.exe stop site /site.name:"live.easygenerator.com/"
+echo "Start Maintenance website"
+C:\Windows\System32\inetsrv\appcmd.exe start site /site.name:"live.easygenerator.com(maintenance)/"
+
+$date = Get-Date -UFormat "%d.%m.%Y"
+$7zAdd ="cmd /C 'C:\Program Files\7-Zip\7z.exe' a "
+$websiteBackupsFolder = "D:\Website_backups\"
+
+echo "Creating database backup:"
+
+$dbName = "live.easygenerator.com"
+$dbBackupName = $dbName + "-" + $date + ".bak"
+$dbBackupPath = "D:\SQL_Backup\" + $dbBackupName 
+$query = "BACKUP DATABASE [" + $dbName + "] TO DISK='" + $dbBackupPath + "' WITH STATS"
+
+SqlCmd -E -S .\sqlexpress -Q $query
+
+echo "Creating website backup:"
+
+$sourcePath = "D:\Applications\live.easygenerator.com\*"
+$websiteBackupsPath = "'" + $websiteBackupsFolder + "live.easygenerator.com-website-backup-" + $date + ".zip'"
+
+$backupCommand = $7zAdd + $websiteBackupsPath + " " + $sourcePath
+
+Invoke-Expression -command "$backupCommand"
+
+echo "Creating filestorage backup:"
+
+$sourcePath = "D:\Applications\live.easygenerator.com``(FileStorage``)\*"
+$filestorageBackupsPath = "'" + $websiteBackupsFolder + "live.easygenerator.com-fileStorage-backup-" + $date + ".zip'"
+
+$backupCommand = $7zAdd + $filestorageBackupsPath + " " + $sourcePath
+
+Invoke-Expression -command "$backupCommand"
+
+write-host "Backups are done!" -foregroundcolor Green
+write-host "Database: $dbBackupPath" -foregroundcolor Yellow
+write-host "Website: $websiteBackupsPath" -foregroundcolor Yellow
+write-host "File Storage: $filestorageBackupsPath" -foregroundcolor Yellow
+write-host "Warning: Live website is still stopped!" -foregroundcolor Red
+
+
