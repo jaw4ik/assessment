ba0eae5cdaa9cb078c692e7513c71fc097beb028
andreydrebot <a.drebot@gmail.com>
2014-11-06 12:26:36 +0000

    EWD-24182 - Add language support to Simple course: Define custom language
    Former-commit-id: a4b911ceb404db27dc0e9b59509b2d7411cd5e89

--- sources/easygenerator.Web/Templates/Simple course/settings.js
+++ sources/easygenerator.Web/Templates/Simple course/settings.js
@@ -1,4 +1,4 @@
-ï»¿{
+{
     "logo": {
         "url": ""
     },
@@ -8,7 +8,7 @@
     "xApi": {
         "enabled": false,
         "lrs": {
-            "uri": "http://cloud.scorm.com/ScormEngineInterface/TCAPI/public/statements",
+            "uri": "http://cloud.scorm.com/ScormEngineInterface/TCAPI/public/statements/",
             "credentials": {
                 "username": "",
                 "password": ""
@@ -19,6 +19,49 @@
     },
     "masteryScore": {
         "score": "75"
-    }
+    },
+    "translations": [
+            { "key": "[course]", "value": "Course" },
+            { "key": "[start course]", "value": "Start course" },
+            { "key": "[finish course]", "value": "Finish course" },
+            { "key": "[learning objectives]", "value": "Learning objectives:" },
+            { "key": "[start]", "value": "Start" },
+            { "key": "[home]", "value": "Home" },
+            { "key": "[learning content]", "value": "Learning content" },
+            { "key": "[submit]", "value": "Submit" },
+            { "key": "[try again]", "value": "Try again" },
+            { "key": "[next]", "value": "Next" },
+            { "key": "[correct answer]", "value": "Correct answer" },
+            { "key": "[incorrect answer]", "value": "Incorrect answer" },
+            { "key": "[previous question]", "value": "previous" },
+            { "key": "[next question]", "value": "next" },
+            { "key": "[text matching question hint]", "value": "Drag items from right column to the left to match the pairs" },
+            { "key": "[text matching question drop here]", "value": "Drop here" },
+            { "key": "[statement question true text]", "value": "True" },
+            { "key": "[statement question false text]", "value": "False" },
+            { "key": "[drag and drop question all texts are placed]", "value": "All texts are placed" },
+            { "key": "[drag and drop question drop here]", "value": "Drop here" },
+            { "key": "[fill in the blank choose answer]", "value": "Choose the answer..." },
+            { "key": "[thank you message]", "value": "Thank you. It is now safe to close this page." },
+            { "key": "[there are no questions]", "value": "No questions" },
+            { "key": "[browser not supported]", "value": "Your browser is currently not supported." },
+            { "key": "[browser not supported hint]", "value": "Don’t worry, there is an easy fix. All you have to do is click one of the icons below and follow the instructions." },
+            { "key": "[page not found title]", "value": "Page not found (404)" },
+            { "key": "[page not found message]", "value": "Sorry, the page you have been looking for has not been found. Try checking the URL on errors, use the navigation above or click 'Home' link below." },
+            { "key": "[tracking and tracing header]", "value": "Your credentials for progress tracking" },
+            { "key": "[tracking and tracing hint]", "value": "Please enter your credentials and click 'Start and report my progress' to enable progress tracking. Otherwise, click 'Do not report, just start'." },
+            { "key": "[tracking and tracing name field]", "value": "Your name" },
+            { "key": "[tracking and tracing email field]", "value": "Your e-mail" },
+            { "key": "[tracking and tracing name is not valid]", "value": "Fill in your name" },
+            { "key": "[tracking and tracing email is not valid]", "value": "Enter a valid e-mail" },
+            { "key": "[tracking and tracing skip reporting]", "value": "Do not report, just start" },
+            { "key": "[tracking and tracing start]", "value": "Start and report my progress" },
+            { "key": "[tracking and tracing error]", "value": "Something is wrong" },
+            { "key": "[tracking and tracing error hint]", "value": "If you continue without restarting, your learning progress will not be reported." },
+            { "key": "[tracking and tracing restart course]", "value": "Restart course" },
+            { "key": "[tracking and tracing continue anyway]", "value": "Continue anyway" },
+            { "key": "[tracking and tracing reporting progress]", "value": "reporting progress..." },
+            { "key": "[tracking and tracing not supported]", "value": "Progress tracking cannot be established" },
+            { "key": "[tracking and tracing not supported hint]", "value": "Sorry, this course does not support progress tracking in Internet Explorer 9. Please use one of the following browser: Chrome, Firefox, Safari, IE10+ in order to track your progress, or just start the course without tracking." }
+        ]
 }
-
