199326a0733b00d079db5f7bf5daf4b411c1a6d1
Andrey Drebot <a.drebot@gmail.com>
2014-11-13 14:26:11 +0200

    Question type directives

--- js/app.js
+++ js/app.js
@@ -1,19 +1,70 @@
-﻿'use strict';
-var app = angular.module('quiz', []);
+﻿(function () {
+    'use strict';
 
-app.controller('QuizController', ['$http', function ($http) {
-    var that = this;
+    var app = angular.module('quiz', []);
 
-    that.title = '';
+    app.controller('QuizController', [
+        '$http', function ($http) {
+            var that = this;
 
-    $http.get('/content/data.js').success(function (response) {
-        that.title = response.title;
+            that.title = '';
+            that.questions = [];
+
+            $http.get('/content/data.js').success(function (response) {
+                that.title = response.title;
+
+                var result = [];
+                if (Array.isArray(response.objectives)) {
+                    response.objectives.forEach(function (dto) {
+                        if (Array.isArray(dto.questions)) {
+                            dto.questions.forEach(function (dtq) {
+                                if (dtq) {
+                                    if (dtq.type == "singleSelectText") {
+                                        result.push(new SinglselectText(dtq.title, dtq.answers));
+                                    }
+
+                                    if (dtq.type == "dragAndDropText") {
+                                        result.push(new DragAndDropText(dtq.title, dtq.background));
+                                    }
+                                }
+                            });
+                        }
+
+                    });
+                }
+
+                that.questions = result;
+
+            });
 
 
+            function SinglselectText(title, answers) {
+                this.title = title;
+                this.answers = answers;
+                this.type = "SinglselectText";
+            }
 
+            function DragAndDropText(title, background) {
+                this.title = title;
+                this.background = background;
+                this.type = "DragAndDropText";
+            }
+
+        }
+    ]);
+
+    app.directive('singleselectText', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/singleSelectText.html'
+        };
     });
-}]);
 
+    app.directive('dragAndDropText', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/dragAndDropText.html'
+        };
+    });
 
-
-
+}());
