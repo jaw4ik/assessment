02105affa1c0daabd42bbbd16c9b2df4765300cc
Andrey Drebot <a.drebot@gmail.com>
2014-11-13 13:21:43 +0200

    Getting data from published content was added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-﻿$(function () {
-    'use strict';
-    var app = angular.module('quiz', []);
+﻿'use strict';
+var app = angular.module('quiz', []);
 
-});
+app.controller('QuizController', ['$http', function ($http) {
+    var that = this;
+
+    that.title = '';
+
+    $http.get('/content/data.js').success(function (response) {
+        that.title = response.title;
+
+
+
+    });
+}]);
+
 
 
 
