aa380351f14365ca9bff4deaf51d39f8bc45c7d4
Andrey Drebot <a.drebot@gmail.com>
2014-11-17 17:03:06 +0200

    dataContext was moved to a separate file

--- src/app/app.js
+++ src/app/app.js
@@ -3,23 +3,32 @@
 
     var app = angular.module('quiz', ['ngRoute']);
 
-    app.config([
-        '$routeProvider', function ($routeProvider) {
-            $routeProvider
-                .when('/questions', {
-                    templateUrl: 'views/questions.html',
-                    controller: 'QuestionController',
-                    controllerAs: 'quiz'
-                })
-                .when('/summary', {
-                    templateUrl: 'views/summary.html',
-                    controller: 'SummaryController',
-                    controllerAs: 'summary'
-                })
-                .otherwise({
-                    redirectTo: '/questions'
-                });
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/questions', {
+                templateUrl: 'views/questions.html',
+                controller: 'QuestionController',
+                controllerAs: 'quiz',
+                resolve: {
+                    questions: function (dataContext) {
+                        return dataContext.initialize();
+                    }
+                }
+            })
+            .when('/summary', {
+                templateUrl: 'views/summary.html',
+                controller: 'SummaryController',
+                controllerAs: 'summary',
+                resolve: {
+                    questions: function (dataContext) {
+                        return dataContext.initialize();
+                    }
+                }
+            })
+            .otherwise({
+                redirectTo: '/questions'
+            });
 
-        }]);
+    }]);
 
 }());
---
+++ src/app/data/dataContext.js
@@ -0,0 +1,113 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('quiz').factory('dataContext', dataContext);
+
+    function dataContext($q, $http) {
+        common.$q = $q;
+        common.$http = $http;
+
+        return {
+            initialize: initialize,
+            getQuiz: getQuiz
+        };
+    }
+
+    var
+        self = {
+            quiz: undefined
+        },
+        common = {
+            $q: undefined,
+            $http: undefined
+        }
+    ;
+
+    function initialize() {
+        return common.$http.get('../content/data.js').success(function (response) {
+
+            var questions = [];
+            if (Array.isArray(response.objectives)) {
+                response.objectives.forEach(function (dto) {
+                    if (Array.isArray(dto.questions)) {
+                        dto.questions.forEach(function (dtq) {
+                            if (dtq) {
+                                var question;
+
+                                if (dtq.type === 'singleSelectText') {
+                                    question = new SinglselectText(dtq.title, dtq.answers);
+                                }
+                                if (dtq.type === 'statement') {
+                                    question = new Statement(dtq.title, dtq.answers);
+                                }
+
+                                if (question) {
+                                    question.title = dtq.title;
+
+                                    if (dtq.hasContent) {
+                                        question.contentUrl = '../content/' + dto.id + '/' + dtq.id + '/content.html';
+                                    }
+
+                                    questions.push(question);
+                                }
+
+                            }
+
+                        });
+                    }
+
+                });
+            }
+
+            self.quiz = new Quiz(response.title, questions);
+
+            return self.quiz;
+        });
+
+    }
+
+    function getQuiz() {
+        return common.$q.when(self.quiz);
+    }
+
+    function Quiz(title, questions) {
+        var that = this;
+
+        that.title = '';
+        that.questions = questions || [];
+    }
+
+    function SinglselectText(title, options) {
+        var that = this;
+        that.title = title;
+
+        that.answers = options.map(function (option) {
+            return {
+                text: option.text,
+                checked: false
+            };
+        });
+        that.checkAnswer = function (answer) {
+            that.answers.forEach(function (item) {
+                item.checked = false;
+            });
+            answer.checked = true;
+        };
+    }
+
+    function Statement(title, options) {
+        var that = this;
+        that.title = title;
+
+        that.answers = options.map(function (option) {
+            return {
+                text: option.text,
+                state: undefined
+            };
+        });
+        that.checkAnswer = function (answer) {
+            answer.checked = true;
+        };
+    }
+
+}());
