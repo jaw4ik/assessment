abd0dfab9353fb7142cf0ed3900458e00e288c04
Andrey Drebot <a.drebot@gmail.com>
2014-11-19 18:00:37 +0200

    Move radio to a separate directive

---
+++ src/app/directives/widgets/radio.js
@@ -0,0 +1,18 @@
+﻿(function () {
+
+    angular.module('quiz')
+        .directive('radio', radio);
+
+    function radio() {
+        return {
+            scope: {
+                checked: '=',
+                title: '@'
+            },
+            replace: true,
+            restrict: 'E',
+            template: '<label class="radio" ng-class="{ checked: checked }">{{title}}</label>'
+        }
+    }
+
+}());
