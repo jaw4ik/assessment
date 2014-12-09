34ced3c2fea058b2ef56f43449db43d2af818675
NadiaGamula <nadia.zakharova@gmail.com>
2014-12-09 15:30:42 +0000

    EWD-24314 - Tracking and Tracing is enabled by default
    Former-commit-id: ae93634becd43e5033e9379c6aff1b2165b1329a

---
+++ sources/easygenerator.DataAccess/Migrations/201412051440452_AddDefaultCourseTemplateSettingsWithDisabledLRS.cs
@@ -0,0 +1,22 @@
+namespace easygenerator.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDefaultCourseTemplateSettingsWithDisabledLRS : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO CourseTemplateSettings " +
+                "SELECT NEWID(), '{ \"logo\": {}, \"xApi\": { \"lrs\": { \"credentials\": {} } } }', c.CreatedBy, GETDATE(), c.ModifiedBy, GETDATE(), c.Template_Id, c.Id, NULL " +
+                "FROM Courses c " +
+                "LEFT JOIN CourseTemplateSettings cts " +
+                "ON c.Id = cts.Course_Id AND c.Template_Id = cts.Template_Id " +
+                "WHERE c.BuildOn IS NOT NULL AND cts.Id IS NULL");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
