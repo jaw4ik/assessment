4bfb217bc01b9f940e8bbd1568f1eba56614b42b
Romsters <petriv.roma@gmail.com>
2014-12-16 11:09:15 +0200

    after review 'dragAndDropText' add width

---
+++ src/app/directives/adjustDropspotWidth.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    angular.module('quiz')
+        .directive('adjustDropspotWidth', adjustDropspotWidth);
+
+    function adjustDropspotWidth() {
+        
+        return {
+            restrict: 'A',
+            link: function ($scope, element) {
+                var $element = $(element);
+                var $question = $(element).closest('.question');
+                addDragHandler($element, $question);
+                
+                $element.on('dragstop', function () {
+                    addDragHandler($element, $question);
+                    $('.dropspot.ui-droppable', $question).css('width', '');
+                });
+            }
+        };
+
+        function addDragHandler($element, $question) {
+            $element.one('drag', function () {
+                $('.dropspot.ui-droppable.active', $question).css('width', $element.css('width'));
+            });
+        }
+    }
+}());
