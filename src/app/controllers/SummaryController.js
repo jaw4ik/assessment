cec5085750ebec6f81538c7578e28fcb56cfc290
Romsters <petriv.roma@gmail.com>
2015-01-05 16:36:43 +0200

    prohibited to continue course after finish

--- src/app/controllers/SummaryController.js
+++ src/app/controllers/SummaryController.js
@@ -5,9 +5,9 @@
         .module('quiz')
         .controller('SummaryController', SummaryController);
 
-    SummaryController.$inject = ['dataContext', '$location', 'quiz'];
+    SummaryController.$inject = ['dataContext', '$location', '$timeout', 'quiz'];
 
-    function SummaryController(dataContext, $location, quiz) {
+    function SummaryController(dataContext, $location, $timeout, quiz) {
         var that = this;
 
         that.title = '"' + quiz.title + '"';
@@ -21,14 +21,20 @@
         that.progress = quiz.getResult().toFixed();
         that.masteryScore = 90;
         that.reachMasteryScore = that.progress >= that.masteryScore;
+        that.finished = false;
 
         that.tryAgain = function () {
+            if (that.finished) return;
             $location.path('/');
         };
 
         that.finish = function () {
+            if (that.finished) return;
+            that.finished = true;
             window.close();
-            alert('Course can be closed');
+            $timeout(function() {
+                 alert('Thank you, you can close the page now');
+            }, 100);
         };
     }
 
