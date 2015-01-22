15a37d6621dbf01211a55744ec865ff9b02c17be
Dmitry Zaets <dmitry.zaets@gmail.com>
2015-01-22 17:50:32 +0200

    Moved image loader service functionality into image loader directive

--- src/app/directives/widgets/imageLoader.js
+++ src/app/directives/widgets/imageLoader.js
@@ -1,11 +1,7 @@
 ﻿(function () {
-
     angular.module('quiz')
-        .directive('imageLoader', imageLoader);
-
-    imageLoader.$inject = ['imageLoaderService'];
-
-    function imageLoader(imageLoaderService) {
+    .directive('imageLoader', imageLoader);
+    function imageLoader() {
         return {
             restrict: 'E',
             scope: {
@@ -17,43 +13,41 @@
             templateUrl: 'app/views/widgets/imageLoader.html',
             link: link
         };
-
         function link(scope, element) {
             var $element = $(element);
             var $imageLoaderIcon = $('.image-loader-icon', $element);
-
+            var image = new Image();
+            image.className = 'image';
+            image.style.display = 'none';
+            image.style.width = 'auto';
+            image.style.height = 'auto';
+            if (!scope.scaleBySmallerSide) {
+                image.style.maxWidth = width + 'px';
+                image.style.maxHeight = height + 'px';
+            }
+            $element.append(image);
+            var $image = $(image);
             scope.$watch('url', function (url) {
-                $('.image', $element).remove();
+                $image.hide();
                 $element.addClass('loading');
                 $imageLoaderIcon.show();
                 if (url) {
                     $element.addClass('loading');
-
                     var width = scope.width;
                     var height = scope.height;
                     var resizedImageUrl = '';
                     if (scope.scaleBySmallerSide) {
                         resizedImageUrl = url + '?height=' + height + '&width=' + width + '&scaleBySmallerSide=true';
                     } else {
-                        var maxSize = width > height ? width : height; // grab image with bigger size to avoid reloading afre screen rotation
+                        var maxSize = width > height ? width : height; // grab image with bigger size to avoid reloading after screen rotation
                         resizedImageUrl = url + '?height=' + maxSize + '&width=' + maxSize;
                     }
-                    imageLoaderService.load(resizedImageUrl).then(function (image) {
-                        if (image) {
-                            image.className = 'image';
-                            image.style.display = 'none';
-                            image.style.width = 'auto';
-                            image.style.height = 'auto';
-                            if (!scope.scaleBySmallerSide) {
-                                image.style.maxWidth = width + 'px';
-                                image.style.maxHeight = height + 'px';
-                            }
-                            $element.append(image);
-                            $imageLoaderIcon.hide();
-                            $element.removeClass('loading');
-                            $(image).fadeIn();
-                        }
-                    });
+                    image.onload = function () {
+                        $imageLoaderIcon.hide();
+                        $element.removeClass('loading');
+                        $image.fadeIn();
+                    };
+                    image.src = resizedImageUrl;
                 }
             });
         }
