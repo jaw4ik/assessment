b7046737450756b8f27d35aff6e21aad5e2778ef
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-03-17 10:35:31 +0200

    Improve "Course editor "page: co-authoring

--- sources/easygenerator.Web/Scripts/knockoutBindings/dialogBinding.js
+++ sources/easygenerator.Web/Scripts/knockoutBindings/dialogBinding.js
@@ -6,7 +6,9 @@
             $container = $('.tb-main'),
             speed = 200,
             isShown = valueAccessor().isShown,
-            autoclose = ko.unwrap(valueAccessor().autoclose) || false;
+            autoclose = ko.unwrap(valueAccessor().autoclose) || false,
+            onHide = valueAccessor().onHide,
+            scrollLocker = createScrollLocker();
 
         if (isShown()) {
             show();
@@ -19,8 +21,11 @@
 
             $.when($blockout).done(function () {
                 $blockout.fadeIn(speed);
-                $element.fadeIn(speed);
-                $element.find('.autofocus').first().focus();
+                $element.fadeIn(speed, function () {
+                    $element.find('.autofocus').focus();
+                    //bug fix when blocked out page is scrolled together with dialog scrolling
+                    scrollLocker.lockScroll();
+                });
             });
 
             if (autoclose) {
@@ -32,10 +37,64 @@
 
         function hide() {
             $element.fadeOut(speed, function () {
-                $('.modal-dialog-blockout').fadeOut(speed, function() {
+                $('.modal-dialog-blockout').fadeOut(speed, function () {
+                    scrollLocker.releaseScroll();
                     $(this).remove();
+
+                    if (_.isFunction(onHide)) {
+                        onHide();
+                    }
                 });
             });
         }
+
+        function createScrollLocker() {
+            var eventNames = 'DOMMouseScroll mousewheel';
+
+            return {
+                lockScroll: lockScroll,
+                releaseScroll: releaseScroll
+            }
+
+            function lockScroll() {
+                $('.scrollable', $element).on(eventNames, trapScroll);
+                $element.on(eventNames, preventScroll);
+            }
+
+            function releaseScroll() {
+                $('.scrollable', $element).off(eventNames, trapScroll);
+                $element.off(eventNames, preventScroll);
+            }
+
+            function trapScroll(ev) {
+                var $this = $(this),
+                    scrollTop = this.scrollTop,
+                    scrollHeight = this.scrollHeight,
+                    height = $this.height(),
+                    delta = (ev.type == 'DOMMouseScroll' ? ev.originalEvent.detail * -40 : ev.originalEvent.wheelDelta),
+                    up = delta > 0;
+
+                if (scrollHeight === 0 || height === 0) {
+                    return 0;
+                }
+
+                var scrollCount = Math.ceil(scrollHeight / 120) - 1;
+                if (scrollCount <= 0)
+                    return preventScroll(ev);
+
+                var scrollDist = Math.ceil(scrollHeight / scrollCount),
+                scrollDelta = up ? scrollDist * -1 : scrollDist;
+
+                $this.scrollTop(scrollTop + scrollDelta);
+                return preventScroll(ev);
+            }
+
+            function preventScroll(ev) {
+                ev.stopPropagation();
+                ev.preventDefault();
+                ev.returnValue = false;
+                return false;
+            }
+        };
     }
 };
