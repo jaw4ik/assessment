55188e4b4c03dd8c2053bb0cce243c881674e74e
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-04-06 09:43:08 +0300

    Added course state repository

---
+++ sources/easygenerator.DataAccess/Repositories/CourseStateRepository.cs
@@ -0,0 +1,19 @@
+﻿using easygenerator.DomainModel.Entities;
+using easygenerator.DomainModel.Repositories;
+using System;
+using System.Linq;
+
+namespace easygenerator.DataAccess.Repositories
+{
+    public class CourseStateRepository : Repository<CourseState>, ICourseStateRepository
+    {
+        public CourseStateRepository(IDataContext dataContext) : base(dataContext)
+        {
+        }
+
+        public CourseState GetByCourseId(Guid courseId)
+        {
+            return _dataContext.GetSet<CourseState>().SingleOrDefault(e => e.Course.Id == courseId);
+        }
+    }
+}
