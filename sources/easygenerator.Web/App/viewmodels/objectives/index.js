4f60d2b36b891b7d004ab5f59a91cdf6cf8ba1fa
Andrey Drebot <a.drebot@gmail.com>
2015-04-06 11:47:34 +0300

    Add routing to objectives

--- sources/easygenerator.Web/App/viewmodels/objectives/index.js
+++ sources/easygenerator.Web/App/viewmodels/objectives/index.js
@@ -9,6 +9,20 @@
            { route: ':objectiveId/questions/:questionId', moduleId: 'viewmodels/questions/question', title: 'Question', settings: { localizationKey: 'questionProperties' } }
        ]).buildNavigationModel();
 
+    childRouter.isViewReady = ko.observable();
+    childRouter.on('router:navigation:processing').then(function (instruction, router) {
+        if (instruction.config.moduleId !== router.isViewReady()) {
+            router.isViewReady(false);
+        }
+    });
+    childRouter.on('router:navigation:composition-complete').then(function (instance, instruction, router) {
+        if (instance) {
+            setTimeout(function () {
+                router.isViewReady(instance.__moduleId__);
+            }, 250);
+        }
+    });
+
     return {
         router: childRouter,
         activate: function () {
