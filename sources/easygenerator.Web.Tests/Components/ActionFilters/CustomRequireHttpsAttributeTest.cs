9321bfe05269e0fa23886e32786b39b889c7fb79
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-04-08 13:39:40 +0300

    EG user api over http (for testing).

--- sources/easygenerator.Web.Tests/Components/ActionFilters/CustomRequireHttpsAttributeTest.cs
+++ sources/easygenerator.Web.Tests/Components/ActionFilters/CustomRequireHttpsAttributeTest.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Activities.Expressions;
 using System.Net;
-using System.Net.Http;
-using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
-using easygenerator.Infrastructure;
 using easygenerator.Web.Components.ActionFilters;
 using easygenerator.Web.Components.ActionResults;
-using easygenerator.Web.Components.BundleTransforms;
+using easygenerator.Web.Components.Configuration;
+using easygenerator.Web.Components.Configuration.ExternalApi;
 using easygenerator.Web.Tests.Utils;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -28,14 +24,21 @@ namespace easygenerator.Web.Tests.Components.ActionFilters
 
         private AuthorizationContext _authorizationContext;
         private HttpRequestBase _httpRequest;
+        private ConfigurationReader _configurationReader;
         
         [TestInitialize]
         public void InitializeContext()
         {
-            _customRequireHttpsAttribute = new CustomRequireHttpsAttribute();
 
             _authorizationContext = Substitute.For<AuthorizationContext>();
             _httpRequest = Substitute.For<HttpRequestBase>();
+            _configurationReader = Substitute.For<ConfigurationReader>();
+            var externalApiSection = Substitute.For<ExternalApiSection>();
+            externalApiSection.RequiresHttps = false;
+
+            _configurationReader.ExternalApi.Returns(externalApiSection);
+
+            _customRequireHttpsAttribute = new CustomRequireHttpsAttribute(_configurationReader);
             
 
             _authorizationContext.HttpContext = Substitute.For<HttpContextBase>();
@@ -62,6 +65,20 @@ namespace easygenerator.Web.Tests.Components.ActionFilters
         }
 
         [TestMethod]
+        public void OnAuthorization_ShouldNotSetResult_WhenHttpsIsTurnedOff()
+        {
+            //Arrange
+            _authorizationContext.Result = new HttpStatusCodeResult(HttpStatusCode.OK);
+            _configurationReader.ExternalApi.RequiresHttps = false;
+
+            //Act
+            _customRequireHttpsAttribute.OnAuthorization(_authorizationContext);
+
+            //Assert
+            _authorizationContext.Result.Should().BeHttpStatusCodeResultWithStatus((int)HttpStatusCode.OK);
+        }
+
+        [TestMethod]
         public void OnAuthorization_ShouldNotSetActionResult_WhenIsUrlSchemeIsHttps()
         {
             //Arrange
@@ -81,6 +98,7 @@ namespace easygenerator.Web.Tests.Components.ActionFilters
             //Arrange
             _httpRequest.Url.Returns(_httpUri);
             _httpRequest.IsSecureConnection.Returns(false);
+            _configurationReader.ExternalApi.RequiresHttps = true;
             
             //Act
             _customRequireHttpsAttribute.OnAuthorization(_authorizationContext);
