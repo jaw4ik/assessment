0604044b7c50885a4a8ab929e33b01f32984f6bc
Andrey Drebot <a.drebot@gmail.com>
2015-04-11 10:49:31 +0300

    Update route titles

--- sources/easygenerator.Web/App/viewmodels/courses/index.js
+++ sources/easygenerator.Web/App/viewmodels/courses/index.js
@@ -5,8 +5,8 @@
        .makeRelative({
            fromParent: true,
        }).map([
-           { route: '', moduleId: 'viewmodels/courses/courses', title: 'Hello World', type: 'intro', nav: true, hash: '#courses' },
-           { route: ':courseId*details', moduleId: 'viewmodels/courses/course/index', title: 'Hello World', type: 'intro', nav: true, hash: '#courses/:courseId' }
+           { route: '', moduleId: 'viewmodels/courses/courses', hash: '#courses', title: 'Courses', settings: { localizationKey: 'courses' } },
+           { route: ':courseId*details', moduleId: 'viewmodels/courses/course/index', hash: '#courses/:courseId', title: 'Course', settings: { localizationKey: 'course' } }
        ]).mapUnknownRoutes('viewmodels/errors/404', '404').buildNavigationModel();
 
 
--- sources/easygenerator.Web/App/viewmodels/objectives/index.js
+++ sources/easygenerator.Web/App/viewmodels/objectives/index.js
@@ -4,7 +4,7 @@
        .makeRelative({
            fromParent: true
        }).map([
-           { route: '', moduleId: 'viewmodels/objectives/objectives', title: 'Hello World' },
+           { route: '', moduleId: 'viewmodels/objectives/objectives', title: 'Objectives', settings: { localizationKey: 'learningObjectives' } },
            { route: ':objectiveId', moduleId: 'viewmodels/objectives/objective', title: 'Learning Objective', settings: { localizationKey: 'objectiveProperties' } },
            { route: ':objectiveId/questions/:questionId', moduleId: 'viewmodels/questions/question', title: 'Question', settings: { localizationKey: 'questionProperties' } }
        ]).buildNavigationModel();
