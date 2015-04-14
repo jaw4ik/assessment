7593c3c088a9f2aad9dc6f7c1c55ccb48be26a1c
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-04-14 09:44:36 +0300

    Changes made during course update are not tracked - add missing files

---
+++ sources/easygenerator.Web/InMemoryStorages/CourseStateStorage/ICourseInfoInMemoryStorage.cs
@@ -0,0 +1,16 @@
+﻿using easygenerator.DomainModel.Entities;
+
+namespace easygenerator.Web.InMemoryStorages.CourseStateStorage
+{
+    public interface ICourseInfoInMemoryStorage
+    {
+        //DateTime BuildStartedOn(Course course);
+        //void SetBuildStartedOn(Course course, DateTime buildStartedOn);
+
+        //DateTime ChangedOn(Course course);
+        //void SetChangedOn(Course course, DateTime changedOn);
+        CourseInfo GetCourseInfo(Course course);
+        void SaveCourseInfo(Course course, CourseInfo info);
+        void RemoveCourseInfo(Course course);
+    }
+}
