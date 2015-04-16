77218d5b5779e8cafa3e2ddd42a31190cb5c68e1
Andrey Drebot <a.drebot@gmail.com>
2015-04-16 10:13:58 +0300

    Remove old course navigation

--- sources/easygenerator.Web/App/viewmodels/courses/courseNavigation/items/publish.js
+++
@@ -1,6 +0,0 @@
-﻿define(['viewmodels/courses/courseNavigation/items/navigationItem'],
-    function (NavigationItem) {
-        return function () {
-            NavigationItem.call(this, 'publish', 'coursePublishItem', 'Navigate to publish course');
-        };
-    });
