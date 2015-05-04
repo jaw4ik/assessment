3438cbb57f6e37f7a6b16fc5301c25cbdb07365d
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2015-05-04 09:24:47 +0300

    EWD-24703 - Extend reports on Results step: fix tests for questionStatement.

---
+++ sources/easygenerator.Web/App/reporting/viewmodels/questionStatement.spec.js
@@ -0,0 +1,57 @@
+﻿define(['reporting/viewmodels/questionStatement', 'reporting/viewmodels/expandableStatement'], function (QuestionStatement, ExpandableStatement) {
+    "use strict";
+    describe('viewmodel [QuestionStatement]', function () {
+
+        it('should be constructor function', function () {
+            expect(QuestionStatement).toBeFunction();
+        });
+
+        describe('[ctor]', function () {
+            var lrsStatement,
+                statement;
+
+            beforeEach(function () {
+                lrsStatement = {
+                    score: 50
+                };
+                spyOn(ExpandableStatement, 'call').and.callThrough();
+                statement = new QuestionStatement(lrsStatement);
+            });
+
+            it('should call ctor of ExpandableStatement with proper args', function () {
+                var args = ExpandableStatement.call.calls.mostRecent().args;
+                expect(args[0]).toBe(statement);
+                expect(args[1]).toBe(lrsStatement);
+                expect(args[2]).toBeNull();
+            });
+
+            it('should set isExpandable to false', function () {
+                lrsStatement.attemptId = 'attemptId';
+                statement = new QuestionStatement(lrsStatement);
+                expect(statement.isExpandable).toBeFalsy();
+            });
+
+            it('should set isExpanded to null', function () {
+                expect(statement.isExpanded).toBeNull();
+            });
+
+            it('should set children to null', function () {
+                expect(statement.children).toBeNull();
+            });
+
+            describe('when lrsStatement.score is 100', function () {
+                it('should set correct prop to true', function () {
+                    lrsStatement.score = 100;
+                    statement = new QuestionStatement(lrsStatement);
+                    expect(statement.correct).toBeTruthy();
+                });
+            });
+
+            describe('when lrsStatement.score does not equal to 100', function () {
+                it('should set correct prop to false', function () {
+                    expect(statement.correct).toBeFalsy();
+                });
+            });
+        });
+    });
+});
