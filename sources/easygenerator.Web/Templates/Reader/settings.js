9c5e9b0a65cfb5b3ee825cde72fc98cf1f675101
Yuriy Savchuk <urec.savchuk@gmail.com>
2015-05-16 16:26:03 +0300

    Background settings added to Reader template.

--- sources/easygenerator.Web/Templates/Reader/settings.js
+++ sources/easygenerator.Web/Templates/Reader/settings.js
@@ -4,5 +4,11 @@
     },
     "languages": {
         "selected": "en"
-    }
+    },
+    "background": {
+		"image": {
+		    "src": "http://p1.pichost.me/i/40/1630921.jpg",
+			"type": "default"
+		}
+	}
 }
