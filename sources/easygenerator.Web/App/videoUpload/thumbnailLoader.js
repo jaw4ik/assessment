fe2296ab0e7b7963be63b9187cc747d4f237b196
Romsters <petriv.roma@gmail.com>
2015-05-22 15:26:19 +0300

    upload video refactoring

--- sources/easygenerator.Web/App/videoUpload/thumbnailLoader.js
+++ sources/easygenerator.Web/App/videoUpload/thumbnailLoader.js
@@ -1,4 +1,4 @@
-﻿define(['constants'], function (constants) {
+﻿define(['./commands/vimeo'], function (vimeo) {
 
     return {
         getThumbnailUrls: getThumbnailUrls
@@ -6,32 +6,14 @@
 
     function getThumbnailUrls(videos) {
         var arrayPromises = [];
-        _.each(videos, function(video) {
-            arrayPromises.push(getThumbnailUrl(video.vimeoId).then(function(thumbnailUrl) {
-                video.thumbnailUrl = thumbnailUrl;
-            }));
+        _.each(videos, function (video) {
+            if (video.vimeoId) {
+                arrayPromises.push(vimeo.getThumbnailUrl(video.vimeoId).then(function (thumbnailUrl) {
+                    video.thumbnailUrl = thumbnailUrl;
+                }));
+            }
         });
 
         return $.when.apply($, arrayPromises);
-    }
-
-    function getThumbnailUrl(vimeoId) {
-        var deferred = $.Deferred();
-
-        $.ajax({
-            url: constants.messages.storage.video.thumbnailLoadUrl + vimeoId + '.json',
-            method: 'GET',
-            global: false
-        }).then(function(data) {
-            if (!data.length || !data[0]['thumbnail_medium']) {
-                deferred.resolve(constants.messages.storage.video.defaultThumbnailUrl);
-                return;
-            }
-            deferred.resolve(data[0]['thumbnail_medium']);
-        }).fail(function() {
-            deferred.resolve(constants.messages.storage.video.defaultThumbnailUrl);
-        });
-
-        return deferred.promise();
     }
 });
