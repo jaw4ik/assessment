cf9da0e421ded220823d1db0068eb361aa803e36
Dmitry Zaets <dmitry.zaets@gmail.com>
2015-05-26 16:19:37 +0300

    Fix build

---
+++ sources/easygenerator.Web/Templates/Personalized learning/content/data.js
@@ -0,0 +1,375 @@
+{
+    "id" : "b5113dc335764bf181e9ce9033460090",
+	"title" : "The lava lamp (DEMO course)",
+	"hasIntroductionContent" : true,
+	"objectives" : [{
+	    "id" : "47bcd990a3e14555b30c901f5c4cd4db",
+	    "title" : "1. At the end of this lesson, you will to state some interesting facts about the Soap Lake Lava Lamp (Knowledge Level).",
+	    "questions" : [{
+	        "id" : "865927b4ecfc4360b8d8663692153754",
+	        "title" : "Name the two reasons why the Soap Lake Lava Lamp was conceived?",
+	        "hasContent" : true,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "f53085b623a648aea0b420027bf0e28c",
+	            "text" : "The lamp's supposed soothing, curative qualities should bring to mind similar qualities in the lake's water ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "3571aa2905a44680b02b5020cecaa64d",
+	            "text" : "Soap Lake is the American base of the Mathmos company.",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "d413636a8e37450295b685b0ccc44da3",
+	            "text" : "The designer Brent Blake has been one of the lead designers at Mathmos for years",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "5fcf2826ea2f4114bd33995e67129ca1",
+	            "text" : "The lava lamp would remind people of the huge prehistoric lava flow that shaped the region ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "1d7a8c812df7491b86ba3e661d2f4bd1"
+	        }
+	        ]
+	    }, {
+	        "id" : "58e75a8cab58416d98e9c7815735eb38",
+	        "title" : "What was Blake Brent’s occupation before he moved to Soap Lake?",
+	        "hasContent" : true,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "72953fcee1ef40f380e16f4d1a9ebf8e",
+	            "text" : "He was an architect",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "6892131ae3634e09a1ee74ad11e59fa6",
+	            "text" : "He was lead designer at Mathmos USA",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "cf27a5b9a50a44a5b8e9d5a144de2c8f",
+	            "text" : "He was an accountant",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "763e11ee435c43d1b2b5f90cca550787",
+	            "text" : "He was a primary school teacher",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "5d9da5c41b3b4dcdbf5c4b6e2993ad0f"
+	        }
+	        ]
+	    }, {
+	        "id" : "3d28438eaa884942906aea505c1c23e9",
+	        "title" : "What is the principle function of the Soap Lake Lava Lamp?",
+	        "hasContent" : true,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "18573c558cd743659ae52e87ef11d6af",
+	            "text" : "Daily",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "03e09dccaafc45bcbce975ff09d73e5c",
+	            "text" : "Weekly",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "85f8acb46caa421894f09d147768d2d4",
+	            "text" : "Hourly",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "e11b7e1f07074d4bbf23a973f8eb8e1d",
+	            "text" : "Monthly",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "ab4f67be0b044bb7bb1af66d352fcbc5"
+	        }
+	        ]
+	    }, {
+	        "id" : "45f96efa172b46198373f2bf99b6d8fa",
+	        "title" : "When was the opening of the Soap Lake lava lamp?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "598629be3a194381b1586818d9e35bda",
+	            "text" : "It opened in September 2012",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "69dfa246b931464ca5406dfbddf18892",
+	            "text" : "It opened in September 2013",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "ff59416f31434656a4ffad4b337f5a56",
+	            "text" : "The Soap Lake lava lamp project hasn't been realized yet.",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "87505a982aa3466da858d87d6417a97d",
+	            "text" : "It opened in September 2011",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "65549fcfa0e04f5d8b7b23c2c4144c3c"
+	        }
+	        ]
+	    }, {
+	        "id" : "131290ee438946938d5afaebde79f204",
+	        "title" : "Which two of the following facts apply to the Soap Lake lava lamp?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "50d4a5030d09484dba242a2eb08ec440",
+	            "text" : "Laser Light Wattage Usage Per Projector: 5.5 to 8 Watts",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "7bd094f68a9c417bb6683d9ca5303f58",
+	            "text" : "The overall Soap Lake Lava®Lamp height from average grade: 65 Feet",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "05fae91c7c4245feb7ddb4781f029b3f",
+	            "text" : "The case construction is cast concrete clad with metallic oxide glazed porcelain tiles",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "dead37723cbc4f77996ab71377670d66",
+	            "text" : "The Soap Lake Lava®Lamp idea was originally conceived by Local Artist Brent Blake in 2002",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "251cd60243324001a70e35259a909828"
+	        }
+	        ]
+	    }
+	    ]
+	}, {
+	    "id" : "7734a48e60d74da5ba9062df797aac8c",
+	    "title" : "2. At the end of this lesson, you will be able to  produce a simple version lava lamp of your own (Application Level)",
+	    "questions" : [{
+	        "id" : "d91c6dbd93914a7cae53f1f0533a0dfc",
+	        "title" : "Put the 5 production steps in the right order",
+	        "hasContent" : true,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "b77ab2c3de9649a6acad0c338d075619",
+	            "text" : "A, B, C, D, E",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "71682d95bc0347599d17c36d449adb3d",
+	            "text" : "D, C, B, E, A",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "3853f299e70d40f6b07cd94d6190d0e3",
+	            "text" : "A, B, C, E, D",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "a654f54b9b9242799087faba0c340b9a",
+	            "text" : "C, D, B, E, A ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "1bd3a43ad55d41d48cfd12e9ef18340c"
+	        }, {
+	            "id" : "c79b6956a1624817ac63a1f8729c6d76"
+	        }
+	        ]
+	    }
+	    ]
+	}, {
+	    "id" : "1b4769525d54430295cc1f0971833894",
+	    "title" : "3. At the end of this lesson, you will be able to summarize the former and current name of the company that produces the original lava lamp (Comprehension Level)",
+	    "questions" : [{
+	        "id" : "b33d6ef71e504337a74443b0fad006ee",
+	        "title" : "What are the founding years of the companies Crestworth and Mathmos, that produce(d) the lava lamps that are invented by Edward Craven-Walker?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "95fc6e26e85f4c128d862da62f1271de",
+	            "text" : "Crestworth (1960) - Mathmos (from 1995)",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "d06bb3079f2c4f4abaa02e1133705c07",
+	            "text" : "Crestworth (1961) - Mathmos (from 1994)",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "3231c05f9dfa4ff285807cda241d2181",
+	            "text" : "Crestworth (1963) - Mathmos (from 1992)",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "20054ba74b264c83b4ffead6cfaaa1cf",
+	            "text" : "Crestworth (1962) - Mathmos (from 1993)",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "d0aabb7b5a9d447bbe16d4c585d659bf"
+	        }
+	        ]
+	    }, {
+	        "id" : "e830a771bb504b0897fa6a84d63b9e74",
+	        "title" : "What are the names of the two companies that produce(d) the lava lamps that are invented by Edward Craven-Walker?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "9d9217c93a63400f8fa10ebdad3ca3d7",
+	            "text" : "Crestworth",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "1ea2358446a247ebb3eb35b0d9c51475",
+	            "text" : "Mathmos",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "f755676a2a344eef9dc548c54a9a8981",
+	            "text" : "Craven-Walker lava lamps ltd.",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "629b04e9ba7d4128bee09ae0d8cd98f9",
+	            "text" : "Astro ltd.",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "320d7b8700cb40808541cd9c190ba852"
+	        }
+	        ]
+	    }
+	    ]
+	}, {
+	    "id" : "1bf90b8ae7004f19afaca406756b2285",
+	    "title" : "4. At the end of this lesson, you will be able to summarize at least two names for a lava lamp (Knowledge Level)",
+	    "questions" : [{
+	        "id" : "b2e3d50ff73f4d919d1b15d509f7ab94",
+	        "title" : "Which 2 alternative names do you know for a lava lamp?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "ccced6ab8f834b9cb45b04553c3fd499",
+	            "text" : "Astro Baby lamp or Space lamp",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "f1bd4e46d5a54ec1bb4d44174be29194",
+	            "text" : "Space lamp or Cosmos lamp",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "31b0143e646344128e0e556f8646ade0",
+	            "text" : "Astro or Liquid motion lamp ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "e7e8dbc289054795822f695f555d2c44",
+	            "text" : "Cosmos lamp or Astro lamp",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "2009f765ea234c58be76c1860d5dd52d"
+	        }
+	        ]
+	    }, {
+	        "id" : "f1e2713b353249e1af7f30f55d5d009c",
+	        "title" : "What were the names of the first two lava lamps that were launched in 1963?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "d2ba354c1609454b9cba03cd029f7c48",
+	            "text" : "Cosmos and Astro Baby",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "b7a034fe4615420a9769223d4b5f0fac",
+	            "text" : "Cosmos and Astro",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "a4d7023058a24dca994865cd367ccf60",
+	            "text" : "Cosmos Baby and Astro Baby",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "76016f7cee9a40dbab25a660d19718a6",
+	            "text" : "Astro and Astro Baby ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "6c13c8a406224eb48b49b8af9af81eb1"
+	        }
+	        ]
+	    }
+	    ]
+	}, {
+	    "id" : "601aa6bb572943e1b343d21174890d01",
+	    "title" : "5. At the end of this lesson, you will be able to define what a lava lamp is, who its inventor is and how the idea came about (Knowledge Level)",
+	    "questions" : [{
+	        "id" : "36f6ded9efcc46659c1c41f5e2ee81d1",
+	        "title" : "Which of the following 3 characteristics apply to the lava lamp?",
+	        "hasContent" : false,
+	        "type" : "multipleSelect",
+	        "answers" : [{
+	            "id" : "373e86c912c04b68a91b18d117561124",
+	            "text" : "The lava lamp is a decorative novelty item, invented by British accountant Edward Craven-Walker. ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "3daac157199b4a03be36b0d47d3b019d",
+	            "text" : "The lamp contains blobs of colored wax inside a glass vessel filled with clear liquid. ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "6be5888a1ee5485ba337dce2aaf395a0",
+	            "text" : "The appearance of the wax is suggestive of Pahoehoe lava, hence the name. ",
+	            "isCorrect" : true,
+	            "group" : "00000000000000000000000000000000"
+	        }, {
+	            "id" : "20ea73d9e7a24216889eeb39ce96d8c0",
+	            "text" : "The lava lamp was invented in 1968 ",
+	            "isCorrect" : false,
+	            "group" : "00000000000000000000000000000000"
+	        }
+	        ],
+	        "learningContents" : [{
+	            "id" : "04043e4217014d03875f493bfa7cf1d2"
+	        }
+	        ]
+	    }
+	    ]
+	}
+	]
+}
