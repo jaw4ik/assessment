fe22e3a9c9d6a6d9d174f02f45084d9e64637997
Dmitry Zaets <dmitry.zaets@gmail.com>
2015-05-27 14:19:13 +0300

    Added equation editor plugin. Seted up equation editor plugin to use http://latex.staging.easygenerator.com

---
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/eqneditor/lang/uk.js
@@ -0,0 +1,8 @@
+﻿CKEDITOR.plugins.setLang('eqneditor', 'uk', {
+    title: 'Редактор Формул',
+    menu: 'Математичні',
+    toolbar: 'Вставити Формулу',
+    edit: 'Редагувати Формулу',
+    equationTitle: 'Формула (LaTeX):',
+    preview: 'Перегляд:'
+});
