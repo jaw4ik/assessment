ded11bf3d545183d362092b49d4dd65189828873
Romsters <petriv.roma@gmail.com>
2015-06-09 12:24:55 +0300

    "undo delete learning content" add scrollToBinding

---
+++ sources/easygenerator.Web/Scripts/knockoutBindings/scrollToBinding.js
@@ -0,0 +1,8 @@
+﻿ko.bindingHandlers.scrollToElement = {
+    init: function (element) {
+        var $scrollElement = $(element);
+        $('html, body').animate({
+            scrollTop: $scrollElement.offset().top - $scrollElement.height() * 2
+        }, 300);
+    }
+};
