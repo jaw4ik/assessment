02d7b4e1efadd9299e018f19d727cb971f01a774
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2015-07-10 16:19:01 +0300

    EWD-25189 - LTI: merge with staging: create separate project for the LTI, extend middleware functionality to check auth path.

---
+++ sources/easygenerator.Lti/Owin.Security/LtiAuthHandler.cs
@@ -0,0 +1,19 @@
+﻿using LtiLibrary.Owin.Security.Lti;
+using System;
+using System.Threading.Tasks;
+
+namespace easygenerator.Lti.Owin.Security
+{
+    public class LtiAuthHandler : LtiAuthenticationHandler
+    {
+        public override async Task<bool> InvokeAsync()
+        {
+            var authOptions = (LtiAuthOptions)Options;
+            if (authOptions.AuthPath == null || Context.Request.Path.Value.EndsWith(authOptions.AuthPath))
+            {
+                return await base.InvokeAsync();
+            }
+            return false;
+        }
+    }
+}
---
+++ sources/easygenerator.Lti/Owin.Security/LtiAuthOptions.cs
@@ -0,0 +1,9 @@
+﻿using LtiLibrary.Owin.Security.Lti;
+
+namespace easygenerator.Lti.Owin.Security
+{
+    public class LtiAuthOptions : LtiAuthenticationOptions
+    {
+        public string AuthPath { get; set; }
+    }
+}
