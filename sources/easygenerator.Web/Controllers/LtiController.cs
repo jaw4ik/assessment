8907badc12efd746bb76b555eb43b73d3572f69b
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2015-08-01 14:56:59 +0300

    EWD-25189 - LTI: merge with staging: change lti controller to return auth tokens.

--- sources/easygenerator.Web/Controllers/LtiController.cs
+++ sources/easygenerator.Web/Controllers/LtiController.cs
@@ -1,6 +1,9 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using easygenerator.Auth.Models;
 using easygenerator.Web.Components.ActionFilters;
 using easygenerator.Web.Components;
+using easygenerator.Infrastructure;
 
 namespace easygenerator.Web.Controllers
 {
@@ -8,21 +11,24 @@ namespace easygenerator.Web.Controllers
     [AllowAnonymous]
     public class LtiController : DefaultController
     {
+        private readonly IDictionaryStorage _storage;
+
+        public LtiController(IDictionaryStorage storage)
+        {
+            _storage = storage;
+        }
+
         [HttpPost]
         [Route("lti/authenticate")]
         public ActionResult Authenticate()
-        {
-            //if (grant_type == "password")
-            //{
-            //    var user = _repository.GetUserByEmail(username);
-            //    if (user != null && user.VerifyPassword(password))
-            //    {
-            //        var tokens = _tokenProvider.GenerateTokens(username, Request.Url.Host, endpoints);
-            //        return JsonSuccess(tokens);
-            //    }
-            //}
-            //return JsonError(AccountRes.Resources.IncorrectEmailOrPassword);
-            return null;
+         {
+            var tokens = _storage.Get<List<TokenModel>>(Lti.Constants.TokensStorageKey);
+            if (tokens != null)
+            {
+                return JsonSuccess(tokens);
+            }
+
+            return JsonError("Cannot authenticate lti user.");
         }
-    }   
+    }
 }
