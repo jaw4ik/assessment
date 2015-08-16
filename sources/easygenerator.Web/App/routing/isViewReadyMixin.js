c96538b1e315e77e9828d23e6d589e6cb1a9ff4e
Andrey Drebot <a.drebot@gmail.com>
2015-08-16 10:34:14 +0300

    isViewReady should be set only when new instruction is executing

--- sources/easygenerator.Web/App/routing/isViewReadyMixin.js
+++ sources/easygenerator.Web/App/routing/isViewReadyMixin.js
@@ -6,18 +6,22 @@
 
     function assign(router, permanent) {
 
+        var moduleId;
+
         router.isViewReady = ko.observable();
         router.on('router:navigation:processing').then(function (instruction, router) {
+            moduleId = instruction.config.moduleId;
             if (instruction.config.moduleId !== router.isViewReady() || permanent) {
                 router.isViewReady(false);
             }
         });
         router.on('router:navigation:composition-complete').then(function (instance, instruction, router) {
-            if (instance) {
-                setTimeout(function () {
-                    router.isViewReady(permanent || instance.__moduleId__);
-                }, 250);
+            if (_.isNullOrUndefined(instance) || instance.__moduleId__ !== moduleId) {
+                return;
             }
+            setTimeout(function () {
+                router.isViewReady(permanent || moduleId);
+            }, 250);
         });
 
     }
