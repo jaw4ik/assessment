54b94613336c6f7906eb01ae09e227e0049f92e9
NadiaGamula <nadia.zakharova@gmail.com>
2015-08-20 14:50:26 +0300

    Update resources bat

--- tools/easyresources/generate-resources.bat
+++ tools/easyresources/generate-resources.bat
@@ -3,4 +3,5 @@ call easyresources generate -t "Personalized learning" -o "_Personalized learnin
 call easyresources generate -t "Exam" -o "_Exam/" 
 call easyresources generate -t "Assessment" -o "_Assessment/" 
 call easyresources generate -t "Reader" -o "_Reader/"
+call easyresources generate -t "Quiz for Learni" -o "_Quiz for Learni/"
 pause
