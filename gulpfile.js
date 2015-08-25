45eb18e0510f94877575494764350aa5821cc977
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-08-25 17:22:09 +0300

    Add version with deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
-
 /// <binding ProjectOpened='watch' />
 var gulp = require('gulp'),
     del = require('del'),
     args = require('yargs').argv,
     MSTest = require('mstest'),
-    Q = require('q');
+    Q = require('q'),
+    GitHubApi = require('github'),
+    runSequence = require('run-sequence'),
+    fs = require('fs'),
+    xmlpoke = require('xmlpoke');
 
 var $ = require('gulp-load-plugins')({
     lazy: true
@@ -14,12 +17,14 @@ var config = {
         less: {
             src: ['./sources/easygenerator.Web/Content/**/*.less'],
             dest: './sources/easygenerator.Web/Content',
-
             browsers: ['last 1 Chrome version', 'last 1 Firefox version', 'last 1 Explorer version', 'last 1 Safari version']
         }
     },
-    outputDirectory = args.output || 'D:\\Applications\\easygenerator',
-    instance = args.instance || 'Release';
+    outputDirectory = args.output || 'D:/Applications/easygenerator',
+    instance = args.instance || 'Release',
+    version = typeof args.version === 'string' && args.version !== '' ? args.version : '1.0.0',
+    createTags = args.createTags;
+
 
 gulp.task('styles', function () {
     return gulp.src(config.less.src)
@@ -48,16 +53,18 @@ gulp.task('watch', ['styles'], function () {
     gulp.watch(config.less.src, ['styles']);
 });
 
-gulp.task('build-main-project', ['build-web-config'], function () {
-    return gulp.src('.\\sources\\easygenerator.Web\\easygenerator.Web.csproj')
+/*#region build*/
+
+gulp.task('build-main-project', function () {
+    return gulp.src('./sources/easygenerator.Web/easygenerator.Web.csproj')
         .pipe($.msbuild({
-            //stdout: true,
+            stdout: true,
             errorOnFail: true,
             targets: ['Clean', 'Build'],
             maxBuffer: 1024 * 1000,
             toolsVersion: 12.0,
             properties: {
-                OutDir: outputDirectory + '\\bin',
+                OutDir: outputDirectory + '/bin',
                 WebProjectOutputDir: outputDirectory,
                 DebugSymbols: false,
                 DebugType: 'none',
@@ -68,9 +75,9 @@ gulp.task('build-main-project', ['build-web-config'], function () {
 });
 
 gulp.task('build-web-config', function () {
-    return gulp.src('.\\tools\\WebConfigTransform\\Transform.proj')
+    return gulp.src('./tools/WebConfigTransform/Transform.proj')
         .pipe($.msbuild({
-            //stdout: true,
+            stdout: true,
             targets: ['Transform'],
             errorOnFail: true,
             maxBuffer: 1024 * 1000,
@@ -78,15 +85,15 @@ gulp.task('build-web-config', function () {
             properties: {
                 Instance: instance,
                 Configuration: 'Release',
-                OutputPath: '.\\tools\\WebConfigTransform'
+                OutputPath: './tools/WebConfigTransform'
             }
         }));
 });
 
-gulp.task('build-unit-tests', ['build-main-project'], function () {
-    return gulp.src('.\\sources\\**\\**Tests.csproj')
+gulp.task('build-unit-tests', function () {
+    return gulp.src('./sources/**/**Tests.csproj')
         .pipe($.msbuild({
-            //stdout: true,
+            stdout: true,
             errorOnFail: true,
             maxBuffer: 1024 * 1000,
             toolsVersion: 12.0,
@@ -98,12 +105,21 @@ gulp.task('build-unit-tests', ['build-main-project'], function () {
         }));
 });
 
-gulp.task('run-nunit-tests', ['build-unit-tests'], function (cb) {
+gulp.task('build', function(cb){
+    runSequence('clean', 'build-main-project', 'build-unit-tests', 'build-web-config', 'styles', cb)
+});
+
+/*#endregion*/
+
+/*#region run tests*/
+
+gulp.task('run-server-tests', function (cb) {
     var testsPath = [
-        '.\\sources\\easygenerator.DomainModel.Tests\\bin\\Debug\\easygenerator.DomainModel.Tests.dll',
-        '.\\sources\\easygenerator.DataAccess.Tests\\bin\\Debug\\easygenerator.DataAccess.Tests.dll',
-        '.\\sources\\easygenerator.Infrastructure.Tests\\bin\\easygenerator.Infrastructure.Tests.dll',
-        '.\\sources\\easygenerator.Web.Tests\\bin\\Debug\\easygenerator.Web.Tests.dll'
+        './sources/easygenerator.DomainModel.Tests/bin/Debug/easygenerator.DomainModel.Tests.dll',
+        './sources/easygenerator.DataAccess.Tests/bin/Debug/easygenerator.DataAccess.Tests.dll',
+        './sources/easygenerator.Infrastructure.Tests/bin/easygenerator.Infrastructure.Tests.dll',
+        './sources/easygenerator.Web.Tests/bin/Debug/easygenerator.Web.Tests.dll',
+        './sources/easygenerator.Auth.Tests/bin/Debug/easygenerator.Auth.Tests.dll'
     ];
 
     var defer = Q.defer();
@@ -130,35 +146,139 @@ gulp.task('run-nunit-tests', ['build-unit-tests'], function (cb) {
                 } else {
                     defer.resolve();
                 }
-                
             }
         });
     }
 });
 
-gulp.task('run-jasmine-tests', ['run-nunit-tests'], function (cb) {
+gulp.task('run-jasmine-tests', function (cb) {
     var gulp_grunt = require('gulp-grunt');
     gulp_grunt.tasks()['grunt-default'](cb);
 });
 
-gulp.task('deploy', ['run-jasmine-tests', 'styles'], function (callback) {
-    del([outputDirectory + '\\*debug.config',
-        outputDirectory + '\\*release.config',
-        outputDirectory + '\\packages.config',
-        outputDirectory + '\\bin\\*.config',
-        outputDirectory + '\\bin\\*.xml',
-        outputDirectory + '\\**\\*.pdb',
-        outputDirectory + '\\**\\*spec.js',
-        outputDirectory + '\\apple-touch-icon*',
-        outputDirectory + '\\Scripts\\*.map',
-        outputDirectory + '\\human.txt',
-        outputDirectory + '\\Content\\*.less',
-        outputDirectory + '\\Scripts\\jasmine'], { force: true }, function () {
-            gulp.src('.\\sources\\easygenerator.Web\\Content\\*.css').pipe(gulp.dest(outputDirectory + '\\Content'));
-            gulp.src('.\\sources\\easygenerator.Web\\App\\main-built.js').pipe(gulp.dest(outputDirectory + '\\App'));
-            gulp.src('.\\tools\\WebConfigTransform\\' + instance + '.config').pipe($.rename('Web.config')).pipe(gulp.dest(outputDirectory));
-            del(['.\\tools\\WebConfigTransform\\' + instance + '.config'], { force: true }, callback);
-        });
+gulp.task('run-unit-tests', function(cb){
+    runSequence('run-server-tests', 'run-jasmine-tests', cb);
 });
 
-gulp.task('build', ['clean', 'deploy'], function () {});
+/*#endregion*/
+
+/*#region deploy*/
+
+gulp.task('deploy', function (cb) {
+    runSequence('build', 'deploy-download-folder', 'deploy-css', 'deploy-main-built-js', 'deploy-web-config', 'remove-extra-files', 'add-version', 'run-unit-tests', function(){
+        if(createTags){
+            runSequence('create-tags', cb);
+        }else{
+            cb();
+        }
+    });
+});
+
+gulp.task('deploy-download-folder', function(cb){
+    var folderToCreate = outputDirectory + '/Download';
+    if (!fs.existsSync(folderToCreate)){
+        fs.mkdirSync(folderToCreate);
+    }
+    cb();
+});
+
+gulp.task('deploy-css', function(){
+    return gulp.src('./sources/easygenerator.Web/Content/*.css')
+        .pipe(gulp.dest(outputDirectory + '/Content'));
+});
+
+gulp.task('deploy-main-built-js', function(){
+    return gulp.src('./sources/easygenerator.Web/App/main-built.js')
+        .pipe(gulp.dest(outputDirectory + '/App'));
+});
+
+gulp.task('deploy-web-config', function(){
+    return gulp.src('./tools/WebConfigTransform/' + instance + '.config')
+        .pipe($.rename('Web.config'))
+        .pipe(gulp.dest(outputDirectory));
+});
+
+gulp.task('remove-extra-files', function(cb){
+    del([outputDirectory + '/*debug.config',
+        outputDirectory + '/*release.config',
+        outputDirectory + '/packages.config',
+        outputDirectory + '/bin/*.config',
+        outputDirectory + '/bin/*.xml',
+        outputDirectory + '/**/*.pdb',
+        outputDirectory + '/**/*spec.js',
+        outputDirectory + '/apple-touch-icon*',
+        outputDirectory + '/Scripts/*.map',
+        outputDirectory + '/humans.txt',
+        outputDirectory + '/Content/*.less',
+        outputDirectory + '/Scripts/jasmine',
+        'tools/WebConfigTransform/' + instance + '.config'], { force: true }, cb);
+});
+
+/*#endregion*/
+
+gulp.task('add-version', function(cb){
+    xmlpoke(outputDirectory +'/Web.config', function(webConfig){
+        webConfig.withBasePath('configuration')
+            .setOrAdd("appSettings/add[@key='version']/@value", version);
+    });
+    cb();
+});
+
+gulp.task('create-tags', function(){
+    // token for easygenerator-ci (replace with one if you want to crete realeses from your name)
+    var authToken = '4a6abc571a3ebeac204f1980e81b1474a0aa1d5f',
+        reposOwner = 'easygenerator',
+        reposList = [
+            'easygenerator',
+            'simple',
+            'exam',
+            'personalized-learning',
+            'quiz-v2',
+            'reader',
+            'lango-personalized-learning',
+            'quiz-marketing',
+            'quiz-for-learni',
+            'lango-simple',
+            'simple-ie10'
+        ],
+        github = new GitHubApi({
+            // required
+            version: "3.0.0",
+            // optional
+            protocol: "https",
+            timeout: 5000
+        });
+
+	github.authenticate({
+	    type: "oauth",
+	    token: authToken
+	});
+    
+    var defer = Q.defer();
+
+    createTag(reposList.shift(), defer);
+
+    return defer.promise;
+
+	function createTag(repoName, defer){
+		github.releases.createRelease({
+			owner: reposOwner,	
+			repo: repoName,
+			tag_name: version,
+			name: 'Release v'+ version
+		}, function(err, result){
+			if(err === null){
+				console.log(repoName + ' - Created');
+			} else {
+				console.log(repoName + ' - Failed');
+			}
+            
+            var nextRepo = reposList.shift();
+            if (typeof nextRepo !== 'undefined') {
+                createTag(nextRepo, defer);
+            } else {
+                defer.resolve();
+            }
+		});
+	}
+});
