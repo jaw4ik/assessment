cbd0cc030c567ca1727e67fd89a4ebe8545bfadc
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-08-31 18:33:28 +0300

    Test all npm packages with changed user on server

--- deploy.bat
+++ deploy.bat
@@ -2,10 +2,18 @@
 
 COLOR
 
+SET DeploymentDirectory=%1
+SET Instance=%2
+SET Version = %3
+
+IF "%1"=="" SET DeploymentDirectory="D:\Applications\easygenerator"
+IF "%2"=="" SET Instance="Release"
+IF "%3"=="" SET Version="1.0.0"
+
 call npm install
 IF NOT %ERRORLEVEL% == 0 GOTO ERROR
 
-call node node_modules/gulp/bin/gulp build
+call node node_modules/gulp/bin/gulp deploy --output=%DeploymentDirectory% --instance=%Instance% --version=%Version%
 IF NOT %ERRORLEVEL% == 0 GOTO ERROR
 
 ECHO Success!!!
