a025aa38c41111290921becc75f9a7c6bb2ee11d
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-09-03 14:01:10 +0300

    Update Brazil translations

---
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/eqneditor/lang/pt-br.js
@@ -0,0 +1,8 @@
+CKEDITOR.plugins.setLang('eqneditor', 'pt-br', {
+    title: 'Editor de equações',
+    menu: 'Matemática',
+    toolbar: 'Insira equação',
+    edit: 'Editar equação',
+    equationTitle: 'Equação (LaTeX):',
+    preview: 'Visualização'
+});
