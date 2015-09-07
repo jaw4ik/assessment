fbef633ee6f91afa63c6f50bfa93d69dfd296a9a
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-09-07 17:09:20 +0300

    Add custom scroll bars for create course pop-up

--- sources/easygenerator.Web/App/components/bindingHandlers/customScrollbarBindingHandler.js
+++ sources/easygenerator.Web/App/components/bindingHandlers/customScrollbarBindingHandler.js
@@ -2,32 +2,67 @@
 
     ko.bindingHandlers.scrollbar = {
         init: function (element, valueAccessor) {
-            var $element = $(element),
-                byClass = valueAccessor().byClass,
-                scrollbar = null,
-                options = {
+            var byClass = valueAccessor().byClass,
+                checkDOMChanges = valueAccessor().checkDOMChanges,
+                scrollToEndAfterDOMChanged = valueAccessor().scrollToEndAfterDOMChanged,
+                customScroll = null,
+                scrollOptions = {
                     mouseWheel: true,
                     disableMouse: true,
                     scrollbars: 'custom',
-                    interactiveScrollbars: true
+                    interactiveScrollbars: true,
+                    probeType: 2
+                },
+                cssClasses = {
+                    scrollStarted: 'cs-scroll-started',
+                    scrollEnabled: 'cs-scroll-enabled'
                 };
 
-            if (byClass) {
-                scrollbar = new IScroll($element.find('.' + byClass)[0], options);
-            } else {
-                scrollbar = new IScroll(element, options);
+            var customScrollbarContainer = byClass ? element.getElementsByClassName(byClass)[0] : element;
+
+            customScroll = new IScroll(customScrollbarContainer, scrollOptions);
+
+            if (checkDOMChanges) {
+                refreshScrollAfterDomChanged();
             }
 
-            _.defer(function() {
-                scrollbar.refresh();
+            customScroll.on('scroll', function () {
+                if (this.y < 0) {
+                    customScrollbarContainer.classList.add(cssClasses.scrollStarted);
+                } else {
+                    customScrollbarContainer.classList.remove(cssClasses.scrollStarted);
+                }
             });
 
+            if (customScroll.hasVerticalScroll) {
+                customScrollbarContainer.classList.add(cssClasses.scrollEnabled);
+            }
+
             ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
-                scrollbar.destroy();
-                scrollbar = null;
+                customScroll.destroy();
+                customScroll = null;
+                clearInterval(interval);
             });
 
             document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+            function refreshScrollAfterDomChanged() {
+                var scrollHeight = customScroll.scroller.clientHeight;
+                var interval = setInterval(function () {
+                    if (scrollHeight === customScroll.scroller.clientHeight) {
+                        return;
+                    }
+                    scrollHeight = customScroll.scroller.clientHeight;
+                    customScroll.refresh();
+                    if (scrollToEndAfterDOMChanged) {
+                        customScroll.scrollTo(0, customScroll.maxScrollY);
+                    }
+                }, 500);
+
+                ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+                    clearInterval(interval);
+                });
+            }
         }
     };
 
