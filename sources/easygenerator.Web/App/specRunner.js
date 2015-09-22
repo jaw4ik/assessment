bf4e42f442943709cc987f2eecf687fcf8769573
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2015-09-22 14:11:14 +0300

    Fix tests for resultsBase viewmodel. Enable all client side tests.

--- sources/easygenerator.Web/App/specRunner.js
+++ sources/easygenerator.Web/App/specRunner.js
@@ -24,7 +24,7 @@
         return Q;
     });
 
-    require(['bootstrapper'], function(bootstrapper) {
+    require(['bootstrapper'], function (bootstrapper) {
         bootstrapper.run();
 
         var specs = [
@@ -116,7 +116,7 @@
             'reporting/viewmodels/questionStatement.spec',
             'reporting/viewmodels/objectiveStatement.spec',
             'reporting/viewmodels/finishStatement.spec',
-            //'reporting/viewmodels/resultsBase.spec',
+            'reporting/viewmodels/resultsBase.spec',
 
             //#endregion
 
@@ -216,6 +216,7 @@
             'viewmodels/learningPaths/learningPath/details.spec',
             'viewmodels/learningPaths/learningPath/publish.spec',
             'viewmodels/learningPaths/learningPath/courseBrief.spec',
+            'viewmodels/learningPaths/learningPath/results.spec',
             'viewmodels/learningPaths/learningPath/actions/download.spec',
             'viewmodels/learningPaths/learningPath/actions/publish.spec',
 
@@ -239,6 +240,7 @@
             'viewmodels/courses/course/design/templateBrief.spec',
             'viewmodels/courses/course/configure.spec',
             'viewmodels/courses/course/publish.spec',
+            'viewmodels/courses/course/results.spec',
             'viewmodels/courses/publishingActions/publishingAction.spec',
             'viewmodels/courses/publishingActions/build.spec',
             'viewmodels/courses/publishingActions/publish.spec',
@@ -368,7 +370,7 @@
             '../Scripts/common/serviceUnavailableAjaxErrorHandler.spec'
         ];
 
-        require(specs, function() {
+        require(specs, function () {
             env.execute();
         });
     });
