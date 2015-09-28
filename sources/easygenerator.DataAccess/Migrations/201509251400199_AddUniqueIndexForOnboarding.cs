f75119da11cf8805f407d5879d0510bc718dc038
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2015-09-28 09:51:33 +0300

    Add migration to delete duplicated onboardings if exists.

--- sources/easygenerator.DataAccess/Migrations/201509251400199_AddUniqueIndexForOnboarding.cs
+++ sources/easygenerator.DataAccess/Migrations/201509251400199_AddUniqueIndexForOnboarding.cs
@@ -7,6 +7,7 @@ namespace easygenerator.DataAccess.Migrations
     {
         public override void Up()
         {
+            Sql("WITH cte AS (SELECT ROW_NUMBER() OVER (PARTITION BY UserEmail ORDER BY (SELECT 0)) RN FROM dbo.Onboardings) DELETE FROM cte WHERE  RN > 1");
             DropIndex("dbo.Onboardings", "Onboardings_UserEmail");
             CreateIndex("dbo.Onboardings", "UserEmail", unique: true, name: "Onboardings_UserEmail");
         }
