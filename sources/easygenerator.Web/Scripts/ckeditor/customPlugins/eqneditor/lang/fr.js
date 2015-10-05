6fb66cb3d339c180d9c7de63d891dfb6fe63a949
ivanmuzika <ivan.muzika@gmail.com>
2015-10-05 11:28:31 +0300

    Add French for server resources and for ckeditor custom plugins.

---
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/eqneditor/lang/fr.js
@@ -0,0 +1,8 @@
+﻿CKEDITOR.plugins.setLang('eqneditor', 'fr', {
+    title: 'Éditeur d\'Équations',
+    menu: 'Mathématiques',
+    toolbar: 'Insérer une Équation',
+    edit: 'Éditer une Équation',
+    equationTitle: 'Équation (LaTeX):',
+    preview: 'Aperçu:'
+});
---
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/videolibrary/lang/fr.js
@@ -0,0 +1,7 @@
+﻿CKEDITOR.plugins.setLang('videolibrary', 'fr', {
+    addFromLibrary: 'Ajouter une vidéo de la vidéothèque',
+    videoLibrary: 'Vidéothèque',
+    noVideos: 'Aucune vidéo pour le moment',
+    somethingWentWrong: 'Une erreur s\'est produite. ',
+    tryAgain: 'Veuillez réessayer'
+});
