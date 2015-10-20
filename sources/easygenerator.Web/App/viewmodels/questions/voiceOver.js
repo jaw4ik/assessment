dd0197a18aa9013dd3d3c3dfa54fc9b7a0920ff5
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-10-20 13:42:14 +0300

    Add voice over events

--- sources/easygenerator.Web/App/viewmodels/questions/voiceOver.js
+++ sources/easygenerator.Web/App/viewmodels/questions/voiceOver.js
@@ -1,5 +1,12 @@
-﻿define(['guard', 'repositories/questionRepository', 'notify', 'dialogs/audio/audioLibrary', 'constants'],
-    function (guard, repository, notify, audioLibraryDialog, constants) {
+﻿define(['guard', 'repositories/questionRepository', 'notify', 'dialogs/audio/audioLibrary', 'constants', 'eventTracker'],
+    function (guard, repository, notify, audioLibraryDialog, constants, eventTracker) {
+
+        var events = {
+            chooseVoiceOverFromAudioLibrary: 'Open \'Choose voice over from audio library\' dialog',
+            addVoiceOver: 'Add voice over',
+            changeVoiceOver: 'Change voice over',
+            deleteVoiceOver: 'Delete voice over'
+        };
 
         var voiceOverViewModel = function (questionId, voiceOverEmbed) {
 
@@ -27,19 +34,22 @@
             };
 
             function update() {
+                eventTracker.publish(events.chooseVoiceOverFromAudioLibrary);
                 audioLibraryDialog.show(null, onAudioSelected);
             }
 
             function remove() {
+                eventTracker.publish(events.deleteVoiceOver);
                 return repository.updateVoiceOver(questionId, null).then(function () {
                     viewModel.title(null);
                     notify.saved();
-                }).catch(function(s) {
+                }).catch(function (s) {
                     debugger;
                 });
             }
 
             function onAudioSelected(audio) {
+                eventTracker.publish(viewModel.title() ? events.changeVoiceOver : events.addVoiceOver);
                 guard.throwIfNotAnObject(audio, 'Audio is not an object');
                 guard.throwIfNotString(audio.vimeoId, 'VimeoId is not a string');
                 guard.throwIfNotString(audio.title, 'Title is not a string');
--- sources/easygenerator.Web/App/viewmodels/questions/voiceOver.spec.js
+++ sources/easygenerator.Web/App/viewmodels/questions/voiceOver.spec.js
@@ -1,172 +1,211 @@
 ﻿define(function (require) {
-    "use strict";
+	"use strict";
 
-    var
+	var
         ctor = require('viewmodels/questions/voiceOver'),
         repository = require('repositories/questionRepository'),
         notify = require('notify'),
-		audioLibraryDialog = require('dialogs/audio/audioLibrary')
-    ;
+		audioLibraryDialog = require('dialogs/audio/audioLibrary'),
+		eventTracker = require('eventTracker')
+	;
 
-    describe('viewModel [voiceOver]', function () {
+	describe('viewModel [voiceOver]', function () {
 
-        var viewModel;
-        var questionId = 'questionId',
+		var viewModel;
+		var questionId = 'questionId',
 			embed = "embed";
 
-        beforeEach(function () {
-            viewModel = ctor(questionId, embed);
+		beforeEach(function () {
+			viewModel = ctor(questionId, embed);
 
-            spyOn(notify, 'success');
-            spyOn(notify, 'saved');
+			spyOn(notify, 'success');
+			spyOn(notify, 'saved');
+			spyOn(eventTracker, 'publish');
 
-            spyOn(audioLibraryDialog, 'show');
-        });
+			spyOn(audioLibraryDialog, 'show');
+		});
 
-        describe('title', function () {
+		describe('title', function () {
 
-            it('should be observable', function () {
-                expect(viewModel.title).toBeObservable();
-            });
+			it('should be observable', function () {
+				expect(viewModel.title).toBeObservable();
+			});
 
-        });
+		});
 
-        describe('getTitle', function () {
+		describe('getTitle', function () {
 
-            describe('when embed code is not a string', function () {
+			describe('when embed code is not a string', function () {
 
-                it('should return null', function () {
-                    expect(viewModel.getTitle()).toEqual(null);
-                });
+				it('should return null', function () {
+					expect(viewModel.getTitle()).toEqual(null);
+				});
 
-            });
+			});
 
-            it('should return title from embed code', function () {
-                expect(viewModel.getTitle('<iframe title="title"></iframe>')).toEqual('title');
-            });
+			it('should return title from embed code', function () {
+				expect(viewModel.getTitle('<iframe title="title"></iframe>')).toEqual('title');
+			});
 
-        });
+		});
 
-        describe('update:', function () {
-            it('should show audia library dialog', function () {
-                viewModel.update();
-                expect(audioLibraryDialog.show).toHaveBeenCalled();
-            });
-        });
+		describe('update:', function () {
+			it('should publish \'Open \'Choose voice over from audio library\' dialog\' event', function () {
+				viewModel.update();
+				expect(eventTracker.publish).toHaveBeenCalledWith('Open \'Choose voice over from audio library\' dialog');
+			});
 
-        describe('onAudioSelected', function () {
+			it('should show audia library dialog', function () {
+				viewModel.update();
+				expect(audioLibraryDialog.show).toHaveBeenCalled();
+			});
+		});
 
-            var updateDefer;
-            beforeEach(function () {
-                updateDefer = Q.defer();
-                spyOn(repository, 'updateVoiceOver').and.returnValue(updateDefer.promise);
-                spyOn(viewModel, 'getEmbedCode').and.returnValue('embed');
-                updateDefer.resolve();
-            });
+		describe('onAudioSelected', function () {
+		    describe('when voice over title is not defined', function() {
+		        beforeEach(function() {
+		            viewModel.title(null);
+		        });
 
-            describe('when audio is not an object', function () {
+		        it('should publish \'Add voice over\' event', function () {
+		        	viewModel.onAudioSelected({
+		        		title: 'title',
+		        		vimeoId: 'vimeoId'
+		        	});
+		        	expect(eventTracker.publish).toHaveBeenCalledWith('Add voice over');
+		        });
+		    });
 
-                it('should throw exception', function () {
-                    var f = function () {
-                        viewModel.onAudioSelected();
-                    };
+		    describe('when voice over title is defined', function () {
+		    	beforeEach(function () {
+		    		viewModel.title('title');
+		    	});
 
-                    expect(f).toThrow();
-                });
+		    	it('should publish \'Change voice over\' event', function () {
+		    		viewModel.onAudioSelected({
+		    			title: 'title',
+		    			vimeoId: 'vimeoId'
+		    		});
+		    		expect(eventTracker.publish).toHaveBeenCalledWith('Change voice over');
+		    	});
+		    });
 
-            });
+			var updateDefer;
+			beforeEach(function () {
+				updateDefer = Q.defer();
+				spyOn(repository, 'updateVoiceOver').and.returnValue(updateDefer.promise);
+				spyOn(viewModel, 'getEmbedCode').and.returnValue('embed');
+				updateDefer.resolve();
+			});
 
-            describe('when vimeoId is not a string', function () {
+			describe('when audio is not an object', function () {
 
-                it('should throw exception', function () {
-                    var f = function () {
-                        viewModel.onAudioSelected({ title: 'title' });
-                    };
+				it('should throw exception', function () {
+					var f = function () {
+						viewModel.onAudioSelected();
+					};
 
-                    expect(f).toThrow();
-                });
+					expect(f).toThrow();
+				});
 
-            });
+			});
 
-            describe('when video title is not a string', function () {
+			describe('when vimeoId is not a string', function () {
 
-                it('should throw exception', function () {
-                    var f = function () {
-                        viewModel.onAudioSelected({ vimeoId: 'vimeoId' });
-                    };
+				it('should throw exception', function () {
+					var f = function () {
+						viewModel.onAudioSelected({ title: 'title' });
+					};
 
-                    expect(f).toThrow();
-                });
+					expect(f).toThrow();
+				});
 
-            });
+			});
 
-            it('should set voice over title', function () {
-                viewModel.title(null);
-                viewModel.onAudioSelected({
-                    title: 'title',
-                    vimeoId: 'vimeoId'
-                });
-                expect(viewModel.title()).toEqual('title');
-            });
+			describe('when video title is not a string', function () {
 
-            it('should send request to server', function () {
-                viewModel.onAudioSelected({
-                    title: 'title',
-                    vimeoId: 'vimeoId'
-                });
-                expect(repository.updateVoiceOver).toHaveBeenCalledWith(questionId, 'embed');
-            });
+				it('should throw exception', function () {
+					var f = function () {
+						viewModel.onAudioSelected({ vimeoId: 'vimeoId' });
+					};
 
-            describe('and when voice over is updated', function () {
+					expect(f).toThrow();
+				});
 
-                it('should show notification', function (done) {
-                    viewModel.onAudioSelected({
-                        title: 'title',
-                        vimeoId: 'vimeoId'
-                    });
-                    updateDefer.promise.fin(function () {
-                        expect(notify.saved).toHaveBeenCalled();
-                        done();
-                    });
-                });
+			});
 
-            });
+			it('should set voice over title', function () {
+				viewModel.title(null);
+				viewModel.onAudioSelected({
+					title: 'title',
+					vimeoId: 'vimeoId'
+				});
+				expect(viewModel.title()).toEqual('title');
+			});
 
-        });
+			it('should send request to server', function () {
+				viewModel.onAudioSelected({
+					title: 'title',
+					vimeoId: 'vimeoId'
+				});
+				expect(repository.updateVoiceOver).toHaveBeenCalledWith(questionId, 'embed');
+			});
 
-        describe('remove', function () {
+			describe('and when voice over is updated', function () {
 
-            var updateDefer;
-            beforeEach(function () {
-                updateDefer = Q.defer();
-                spyOn(repository, 'updateVoiceOver').and.returnValue(updateDefer.promise);
-                updateDefer.resolve();
-            });
+				it('should show notification', function (done) {
+					viewModel.onAudioSelected({
+						title: 'title',
+						vimeoId: 'vimeoId'
+					});
+					updateDefer.promise.fin(function () {
+						expect(notify.saved).toHaveBeenCalled();
+						done();
+					});
+				});
 
-            it('should send request to server', function () {
-                viewModel.remove();
-                expect(repository.updateVoiceOver).toHaveBeenCalledWith(questionId, null);
-            });
+			});
 
-            describe('and when voice over is removed', function () {
+		});
 
-                it('should clear title', function (done) {
-                    viewModel.title('title');
-                    viewModel.remove().then(function () {
-                        expect(viewModel.title()).toEqual(null);
-                        done();
-                    });
-                });
+		describe('remove', function () {
 
-                it('should show notification', function (done) {
-                    viewModel.remove().then(function () {
-                        expect(notify.saved).toHaveBeenCalled();
-                        done();
-                    });
-                });
+			it('should publish \'Delete voice over\' event', function () {
+			    viewModel.remove();
+				expect(eventTracker.publish).toHaveBeenCalledWith('Delete voice over');
+			});
 
-            });
+			var updateDefer;
+			beforeEach(function () {
+				updateDefer = Q.defer();
+				spyOn(repository, 'updateVoiceOver').and.returnValue(updateDefer.promise);
+				updateDefer.resolve();
+			});
 
-        });
-    });
+			it('should send request to server', function () {
+				viewModel.remove();
+				expect(repository.updateVoiceOver).toHaveBeenCalledWith(questionId, null);
+			});
+
+			describe('and when voice over is removed', function () {
+
+				it('should clear title', function (done) {
+					viewModel.title('title');
+					viewModel.remove().then(function () {
+						expect(viewModel.title()).toEqual(null);
+						done();
+					});
+				});
+
+				it('should show notification', function (done) {
+					viewModel.remove().then(function () {
+						expect(notify.saved).toHaveBeenCalled();
+						done();
+					});
+				});
+
+			});
+
+		});
+	});
 })
