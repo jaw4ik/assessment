885fa39bf416e52dc4a7e7abfb9e0ddc64c45b32
ivanmuzika <ivan.muzika@gmail.com>
2015-11-03 13:44:45 +0200

    Update client localization. Add Dutch and German.

--- sources/easygenerator.Web/App/localization/localizationManager.js
+++ sources/easygenerator.Web/App/localization/localizationManager.js
@@ -3,7 +3,7 @@
 
     var 
         defaultCulture = 'en',
-        supportedCultures = ['en', 'uk', 'zh-cn', 'pt-br'],
+        supportedCultures = ['en', 'uk', 'zh-cn', 'pt-br', 'nl', 'de'],
         translations = null,
         currentCulture = '',
         currentLanguage = '',
