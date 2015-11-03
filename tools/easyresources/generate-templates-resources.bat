1bf722c6e5579cdb9d13ae7d2b8f694c5444c40a
YuliaSavchuk <yulia.savchuk3@gmail.com>
2015-11-03 11:36:13 +0200

    Update Simple course template

--- tools/easyresources/generate-templates-resources.bat
+++ tools/easyresources/generate-templates-resources.bat
@@ -2,12 +2,10 @@
 
 set templates[0]=Simple course
 set templates[1]=Personalized learning
-set templates[2]=Exam
-set templates[3]=Assessment
-set templates[4]=Reader
-set templates[5]=Quiz for Learni
-set templates[6]=Simple course PDF
+set templates[2]=Assessment
+set templates[3]=Reader
+set templates[4]=Quiz for Learni
 
-for /F "tokens=2 delims=[=]" %%i in ('set templates[') do call easyresources generate template -w "%%templates[%%i]%%" -o "_templates/_%%templates[%%i]%%" 
+for /F "tokens=2 delims=[=]" %%i in ('set templates[') do call easyresources generate template --worksheetName "%%templates[%%i]%%" --outputPath "_templates/_%%templates[%%i]%%" --excludeLocalizations "ar"
 
 pause
