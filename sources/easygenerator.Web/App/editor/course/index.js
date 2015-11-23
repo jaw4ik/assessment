6a167687922c58d7c7a468dea3f17df5c0baac70
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-11-23 12:43:00 +0200

    Change module to es2015

--- sources/easygenerator.Web/App/editor/course/index.js
+++ sources/easygenerator.Web/App/editor/course/index.js
@@ -11,45 +11,24 @@ var eventsForCourseContent = {
     endEditText: 'End editing introduction'
 };
 
-function activate(courseId) {
-    return co.call(this, function*() {
-        var course = yield repository.getById(courseId);
-        this.id = course.id;
-        this.createdBy = course.createdBy;
-        this.objectives(_.map(course.objectives, objective => objective));
-        this.courseIntroductionContent = vmContentField(course.introductionContent, eventsForCourseContent, false, content => repository.updateIntroductionContent(course.id, content));
-    });
-}
-
-export default {
-    id: '',
-    createdBy: '',
-    objectives: ko.observableArray([]),
-    eventTracker: eventTracker,
-    localizationManager: localizationManager,
-    courseIntroductionContent: null,
-    activate: activate
-}
-
-//export default class{
-//    constructor () {
-//        this.id = '';
-//        this.createdBy = '';
-//        this.objectives = ko.observableArray([]);
-//        this.eventTracker = eventTracker;
-//        this.localizationManager = localizationManager;
-//        this.courseIntroductionContent = null;
-//        this.viewUrl = 'editor/course/index';
-//    }
-//    activate(courseId) {
-//        debugger;
-//        return co.call(this, function*() {
-//            let course = yield repository.getById(courseId);
-//            this.id = course.id;
-//            this.createdBy = course.createdBy;
-//            this.objectives(_.map(course.objectives, objective => objective));
-//            this.courseIntroductionContent = vmContentField(course.introductionContent, eventsForCourseContent, false, content => repository.updateIntroductionContent(course.id, content));
-//        });
-//    }
-//};
+export default class{
+    constructor () {
+        this.id = '';
+        this.createdBy = '';
+        this.objectives = ko.observableArray([]);
+        this.eventTracker = eventTracker;
+        this.localizationManager = localizationManager;
+        this.courseIntroductionContent = null;
+        this.viewUrl = 'editor/course/index';
+    }
+    activate(courseId) {
+        return co.call(this, function*() {
+            let course = yield repository.getById(courseId);
+            this.id = course.id;
+            this.createdBy = course.createdBy;
+            this.objectives(_.map(course.objectives, objective => objective));
+            this.courseIntroductionContent = vmContentField(course.introductionContent, eventsForCourseContent, false, content => repository.updateIntroductionContent(course.id, content));
+        });
+    }
+};
 export var __useDefault = true;
