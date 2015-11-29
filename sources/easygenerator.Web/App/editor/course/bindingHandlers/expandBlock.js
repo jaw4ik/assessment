7a38a2053e2cb9facb904f304741e16d929d791e
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-11-29 18:02:50 +0200

    Change expandBlock binding hadler.

--- sources/easygenerator.Web/App/editor/course/bindingHandlers/expandBlock.js
+++ sources/easygenerator.Web/App/editor/course/bindingHandlers/expandBlock.js
@@ -6,59 +6,33 @@ import $ from 'jquery';
 var findElementHeight = element => {
     let $element = $(element);
     let height = 0;
-    let elementPadding;
-}
-
-
-
-
-var findChildrenHeight = element => {
-        
-
-
-
-
-
-        let height = 0;
-        debugger;
-        $(element).children().each((index, item) => {
-            height = height + $(item).outerHeight(true);
-        });
-
-        let paddingHeight = $(element).outerHeight() - height;
-
-        //let children = element.childNodes,
-        //    elementStyles = window.getComputedStyle(element),
-        //    height = parseInt(elementStyles.getPropertyValue('padding-top')) + parseInt(elementStyles.getPropertyValue('padding-bottom'));
-
-        //_.each(children, child => {
-        //    if (child.nodeType !== 1) {
-        //        return;
-        //    }
-        //    let childStyles = window.getComputedStyle(child);
-        //    let childHeight = parseInt(childStyles.getPropertyValue('height'));
-        //    let childMargins = parseInt(childStyles.getPropertyValue('margin-top')) + parseInt(childStyles.getPropertyValue('margin-bottom'));
-        //    height += childHeight + childMargins;
-        //});
-
-        return height + paddingHeight;
-    },
-    getHeight = (element, expanded) => expanded ? findChildrenHeight(element) : 0;
-
+    let elementPadding = 0;
+    
+    $element.children().each((index, item)=>{
+        height += $(item).outerHeight(true);
+    })
+    
+    elementPadding = parseInt($element.css('padding-top')) + parseInt($element.css('padding-bottom'));
+    
+    return height + elementPadding;
+};
+var getHeight = (element, expanded) => expanded ? findElementHeight(element) : 0;
 
 ko.bindingHandlers.expandBlock = {
     init: (element, valueAccessors) => {
-        const expanded = ko.utils.unwrapObservable(valueAccessors().expanded),
-            duration = ko.utils.unwrapObservable(valueAccessors().duration) || '0.3s';
-
-        element.style.height = `${getHeight(element, expanded)}px`;
-        element.style.overflow = 'hidden';
-        element.style.transition = `all ${duration} linear`;
+        let $element = $(element);
+        let expanded = ko.utils.unwrapObservable(valueAccessors().expanded);
+        let duration = ko.utils.unwrapObservable(valueAccessors().duration) || '0.3s';
+            
+        $element.css('height', getHeight(element, expanded));
+        $element.css('overflow', 'hidden');
+        $element.css('transition', `height ${duration} linear`);
     },
     update: (element, valueAccessors) => {
-        const expanded = ko.utils.unwrapObservable(valueAccessors().expanded);
+        let $element = $(element);
+        let expanded = ko.utils.unwrapObservable(valueAccessors().expanded);
 
-        element.style.height = `${getHeight(element, expanded)}px`;
+        $element.css('height', getHeight(element, expanded));
     }
 };
 
