805d96e5a03ddc4dc5fe84aef1c1c088093adad3
unknown <yr.savchuk@dev-ua-ws111.corp.ism.nl>
2015-11-30 10:21:39 +0200

    ES7 async/await functionality configured.

--- sources/easygenerator.Web/config.js
+++ sources/easygenerator.Web/config.js
@@ -4,7 +4,9 @@ System.config({
   babelOptions: {
     "optional": [
       "optimisation.modules.system",
-      "runtime"
+      "runtime",
+      "es7.asyncFunctions",
+      "asyncToGenerator"
     ]
   },
   paths: {
