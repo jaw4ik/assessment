71f333f7cbab2a83b3ebff7acd06cd9ced6591ae
NadiaGamula <nadia.zakharova@gmail.com>
2015-12-13 11:34:28 +0200

    Move courseComments and commentRepository to ES 6

--- sources/easygenerator.Web/App/repositories/commentRepository.js
+++ sources/easygenerator.Web/App/repositories/commentRepository.js
@@ -1,76 +1,73 @@
-﻿define(['http/apiHttpWrapper', 'guard', 'models/comment'],
-    function (apiHttpWrapper, guard, Comment) {
-        "use strict";
+﻿import _ from 'underscore';
+import apiHttpWrapper from 'http/apiHttpWrapper';
+import guard from 'guard';
+import Comment from 'models/comment';
 
-        var repository = {
-            getCollection: getCollection,
-            removeComment: removeComment,
-            restoreComment: restoreComment
-        };
+class CommentRepository {
+    getCollection(courseId) {
+        return Q.fcall(() => {
+            guard.throwIfNotString(courseId, 'Course id is not a string');
 
-        return repository;
+            return apiHttpWrapper.post('api/comments', { courseId: courseId }).then((response) => {
+                guard.throwIfNotAnObject(response, 'Response is not an object');
+                guard.throwIfNotArray(response.Comments, 'Comments is not an array');
 
-        function getCollection(courseId) {
-            return Q.fcall(function () {
-                guard.throwIfNotString(courseId, 'Course id is not a string');
-
-                return apiHttpWrapper.post('api/comments', { courseId: courseId }).then(function (response) {
-                    guard.throwIfNotAnObject(response, 'Response is not an object');
-                    guard.throwIfNotArray(response.Comments, 'Comments is not an array');
-
-                    return _.map(response.Comments, function (comment) {
-                        return new Comment({
-                            id: comment.Id,
-                            text: comment.Text,
-                            email: comment.CreatedBy,
-                            name: comment.CreatedByName,
-                            createdOn: comment.CreatedOn
-                        });
+                return _.map(response.Comments, (comment) => {
+                    return new Comment({
+                        id: comment.Id,
+                        text: comment.Text,
+                        email: comment.CreatedBy,
+                        name: comment.CreatedByName,
+                        createdOn: comment.CreatedOn
                     });
                 });
             });
-        }
-
-        function removeComment(courseId, commentId) {
-            return Q.fcall(function () {
-                guard.throwIfNotString(courseId, 'Course id is not a string');
-                guard.throwIfNotString(commentId, 'Comment id is not a string');
-
-                var data = {
-                    courseId: courseId,
-                    commentId: commentId
-                };
-
-                return apiHttpWrapper.post('api/comment/delete', data).then(function (response) {
-                    guard.throwIfNotBoolean(response, 'Response is not a boolean');
-
-                    return response;
-                });
-            });
-        }
-
-        function restoreComment(courseId, comment) {
-            return Q.fcall(function () {
-                guard.throwIfNotString(courseId, 'Course id is not a string');
-                guard.throwIfNotAnObject(comment, 'Comment data is not an object');
-                guard.throwIfNotString(comment.text, 'Comment text is not a string');
-                guard.throwIfNotString(comment.name, 'Comment name is not a string');
-                guard.throwIfNotString(comment.email, 'Comment email is not a string');
-                guard.throwIfNotDate(new Date(comment.createdOn), 'Comment createdOn is not a date');
-
-                var data = {
-                    courseId: courseId,
-                    text: comment.text,
-                    createdByName: comment.name,
-                    createdBy: comment.email,
-                    createdOn: comment.createdOn
-                };
-                return apiHttpWrapper.post('api/comment/restore', data).then(function (response) {
-                    guard.throwIfNotString(response, 'Response is not a string');
-
-                    return response;
-                });
-            });
-        }
+        });
     }
-);
+
+    removeComment(courseId, commentId) {
+        return Q.fcall(() => {
+            guard.throwIfNotString(courseId, 'Course id is not a string');
+            guard.throwIfNotString(commentId, 'Comment id is not a string');
+
+            let data = {
+                courseId: courseId,
+                commentId: commentId
+            };
+
+            return apiHttpWrapper.post('api/comment/delete', data).then((response) => {
+                guard.throwIfNotBoolean(response, 'Response is not a boolean');
+
+                return response;
+            });
+        });
+    }
+
+    restoreComment(courseId, comment) {
+        return Q.fcall(() => {
+            guard.throwIfNotString(courseId, 'Course id is not a string');
+            guard.throwIfNotAnObject(comment, 'Comment data is not an object');
+            guard.throwIfNotString(comment.text, 'Comment text is not a string');
+            guard.throwIfNotString(comment.name, 'Comment name is not a string');
+            guard.throwIfNotString(comment.email, 'Comment email is not a string');
+            guard.throwIfNotDate(new Date(comment.createdOn), 'Comment createdOn is not a date');
+
+            let data = {
+                courseId: courseId,
+                text: comment.text,
+                createdByName: comment.name,
+                createdBy: comment.email,
+                createdOn: comment.createdOn
+            };
+            return apiHttpWrapper.post('api/comment/restore', data).then((response) => {
+                guard.throwIfNotString(response, 'Response is not a string');
+
+                return response;
+            });
+        });
+    }
+}
+
+
+let viewModel = new CommentRepository();
+export default viewModel;
--- sources/easygenerator.Web/App/viewmodels/courses/courseComments.js
+++ sources/easygenerator.Web/App/viewmodels/courses/courseComments.js
@@ -1,102 +1,109 @@
-﻿define(['durandal/app','guard', 'userContext', 'repositories/commentRepository', 'eventTracker', 'constants',
-    'plugins/router', 'notify', 'localization/localizationManager'],
-    function (app, guard, userContext, commentRepository, eventTracker, constants, router, notify, localizationManager) {
+﻿import ko from 'knockout';
+import _ from 'underscore';
+import app from 'durandal/app';
+import constants from 'constants';
+import guard from 'guard';
+import userContext from 'userContext';
+import commentRepository from 'repositories/commentRepository';
+import eventTracker from 'eventTracker';
+import router from 'plugins/router';
+import notify from 'notify';
+import localizationManager from 'localization/localizationManager';
 
-        var viewModel = {
-            courseId: null,
-            isCommentsLoading: ko.observable(),
-            comments: ko.observableArray([]),
-            hasAccessToComments: ko.observable(userContext.hasStarterAccess()),
-            activate: activate,
-            openUpgradePlanUrl: openUpgradePlanUrl,
-            removeComment: removeComment,
-            restoreComment: restoreComment,
-            hide: hide,
-            deletedByCollaborator: deletedByCollaborator
-        };
+class CourseComments {
 
-        app.on(constants.messages.course.comment.deletedByCollaborator, viewModel.deletedByCollaborator);
-
-        return viewModel;
-
-        function activate(courseId) {
-            return Q.fcall(function () {
-                guard.throwIfNotString(courseId, 'Course id is not a string');
-
-                viewModel.courseId = courseId;
-                viewModel.isCommentsLoading(true);
-
-                return userContext.identify().then(function () {
-                    viewModel.hasAccessToComments(userContext.hasStarterAccess());
-
-                    if (userContext.hasStarterAccess()) {
-                        return commentRepository.getCollection(courseId).then(function (comments) {
-                            viewModel.comments(_.map(comments, function(item) {
-                                return {
-                                    id: ko.observable(item.id),
-                                    text: item.text,
-                                    email: item.email,
-                                    name: item.name,
-                                    createdOn: item.createdOn,
-                                    isDeleted: ko.observable(false)
-                                };
-                            }));
-                        });
-                    }
-                }).fin(function () {
-                    viewModel.isCommentsLoading(false);
-                });
-            });
-        }
-
-        function openUpgradePlanUrl() {
-            eventTracker.publish(constants.upgradeEvent, constants.upgradeCategory.externalReview);
-            router.openUrl(constants.upgradeUrl);
-        }
-
-        function removeComment(comment) {
-            return commentRepository.removeComment(viewModel.courseId, comment.id()).then(function (success) {
-                if (success) {
-                    comment.isDeleted(true);
-                    notify.saved();
-                } else {
-                    throw "Comment is not deleted";
-                }
-            }).fail(function() {
-                notify.error(localizationManager.localize('commentWasNotDeletedError'));
-            });
-        }
-
-        function restoreComment(comment) {
-            return commentRepository.restoreComment(viewModel.courseId, comment).then(function (restoredId) {
-                if (!_.isNullOrUndefined(restoredId)) {
-                    comment.isDeleted(false);
-                    comment.id(restoredId);
-                    notify.saved();
-                } else {
-                    throw "Comment is not restored";
-                }
-            }).fail(function() {
-                notify.error(localizationManager.localize('commentWasNotRestoredError'));
-            });
-        }
-
-        function deletedByCollaborator(courseId, commentId) {
-            if (viewModel.courseId !== courseId) {
-                return;
-            }
-
-            deleteFromViewModel(commentId);
-        }
-
-        function hide(comment) {
-            deleteFromViewModel(comment.id());
-        }
-
-        function deleteFromViewModel(commentId) {
-            viewModel.comments(_.reject(viewModel.comments(), function (item) {
-                return item.id() === commentId;
-            }));
-        }
+    constructor() {
+        this.courseId = null;
+        this.isCommentsLoading = ko.observable();
+        this.comments = ko.observableArray([]);
+        this.hasAccessToComments = ko.observable(userContext.hasStarterAccess());
     }
-);
+
+        
+    activate(courseId) {
+        let that = this;
+
+        return Q.fcall(() => {
+            guard.throwIfNotString(courseId, 'Course id is not a string');
+
+            that.courseId = courseId;
+            that.isCommentsLoading(true);
+
+            return userContext.identify().then(() => {
+                this.hasAccessToComments(userContext.hasStarterAccess());
+
+                if (userContext.hasStarterAccess()) {
+                    return commentRepository.getCollection(courseId).then((comments) => {
+                        that.comments(_.map(comments, (item) => {
+                            return {
+                                id: ko.observable(item.id),
+                                text: item.text,
+                                email: item.email,
+                                name: item.name,
+                                createdOn: item.createdOn,
+                                isDeleted: ko.observable(false)
+                            };
+                        }));
+                    });
+                }
+            }).fin(() => {
+                that.isCommentsLoading(false);
+            });
+        });        
+    }
+
+    openUpgradePlanUrl() {
+        eventTracker.publish(constants.upgradeEvent, constants.upgradeCategory.externalReview);
+        router.openUrl(constants.upgradeUrl);
+    }
+
+    removeComment(comment) {
+        return commentRepository.removeComment(this.courseId, comment.id()).then((success) => {
+            if (success) {
+                comment.isDeleted(true);
+                notify.saved();
+            } else {
+                throw "Comment is not deleted";
+            }
+        }).fail(() => {
+            notify.error(localizationManager.localize('commentWasNotDeletedError'));
+        });
+    }
+
+    restoreComment(comment) {
+        return commentRepository.restoreComment(this.courseId, comment).then((restoredId) => {
+            if (!_.isNull(restoredId) && !_.isUndefined(restoredId)) {
+                comment.isDeleted(false);
+                comment.id(restoredId);
+                notify.saved();
+            } else {
+                throw "Comment is not restored";
+            }
+        }).fail(() => {
+            notify.error(localizationManager.localize('commentWasNotRestoredError'));
+        });
+    }
+
+    deletedByCollaborator(courseId, commentId) {
+        if (this.courseId !== courseId) {
+            return;
+        }
+
+        this.deleteFromViewModel(commentId);
+    }
+
+    hide(comment) {
+        this.deleteFromViewModel(comment.id());
+    }
+
+    deleteFromViewModel(commentId) {
+        this.comments(_.reject(this.comments(), (item) => {
+            return item.id() === commentId;
+        }));
+    }
+}
+
+let viewModel = new CourseComments();
+app.on(constants.messages.course.comment.deletedByCollaborator, viewModel.deletedByCollaborator.bind(viewModel));
+
+export default viewModel;
