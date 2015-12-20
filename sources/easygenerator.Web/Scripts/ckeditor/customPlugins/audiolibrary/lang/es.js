eeda928d4743dd489c5ba9ebf5a78cf508e64592
NadiaGamula <nadia.zakharova@gmail.com>
2015-12-20 14:23:15 +0200

    Move all server resources
    Add spanish to CKEditor

---
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/audiolibrary/lang/es.js
@@ -0,0 +1,7 @@
+﻿CKEDITOR.plugins.setLang('audiolibrary', 'es', {
+    addFromLibrary: 'Biblioteca de audio',
+    audioLibrary: 'Biblioteca de audioy',
+    noAudio: 'Todavía no hay ningún archivo de audio',
+    somethingWentWrong: 'Algo salió mal. ',
+    tryAgain: 'Inténtelo de nuevo'
+});
--- sources/easygenerator.Web/Scripts/ckeditor/customPlugins/fileuploader/plugin.js
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/fileuploader/plugin.js
@@ -2,7 +2,7 @@
 
     CKEDITOR.plugins.fileuploader = {
         requires: 'dialogui',
-        lang: 'en,uk,zh-cn,pt-br,de,nl',
+        lang: 'en,uk,zh-cn,pt-br,de,nl,es',
 
         onLoad: function () {
 
