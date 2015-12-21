5e267b6804a81f7cc582af0e111c48248ea7ab96
NadiaGamula <nadia.zakharova@gmail.com>
2015-12-21 14:06:42 +0200

    Fix bugs in resources

--- sources/easygenerator.Web/Scripts/ckeditor/customPlugins/audiolibrary/lang/es.js
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/audiolibrary/lang/es.js
@@ -1,6 +1,6 @@
 ﻿CKEDITOR.plugins.setLang('audiolibrary', 'es', {
     addFromLibrary: 'Biblioteca de audio',
-    audioLibrary: 'Biblioteca de audioy',
+    audioLibrary: 'Biblioteca de audio',
     noAudio: 'Todavía no hay ningún archivo de audio',
     somethingWentWrong: 'Algo salió mal. ',
     tryAgain: 'Inténtelo de nuevo'
