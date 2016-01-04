5ecdfe2a9da949f0eab4aecc5dcaa5c5ed8a89be
NadiaGamula <nadia.zakharova@gmail.com>
2016-01-04 13:35:46 +0200

    Update app, server and CKEditor resources

--- sources/easygenerator.Web/Scripts/ckeditor/customPlugins/documentembed/lang/es.js
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/documentembed/lang/es.js
@@ -1,31 +1,31 @@
 ﻿CKEDITOR.plugins.setLang('documentembed', 'es', {
-    ebmedDocument: 'Embed document [es]',
-    embedCodeMissing: 'Embed code is missing [es]',
-    pasteEmbedCode: 'Paste embed code here... [es]',
-    howItWork: 'Here is how it works: [es]',
-    thatIt: 'That\'s it! [es]',
-    googleDocuments: 'Google documents [es]',
-    googleDocumentsTitle: 'Embed your documents using Google Drive service. It\'s free! [es]',
-    slideshare: 'SlideShare [es]',
-    slideshareTitle: 'Embed your presentations using SlideShare service. It\'s free! [es]',
-    other: 'Other [es]',
-    otherTitle: 'Embed your documents from any other services that support "embedding". [es]',
+    ebmedDocument: 'Insertar documento',
+    embedCodeMissing: 'El código para insertar no se encuentra',
+    pasteEmbedCode: 'Pegue el código para insertar aquí...',
+    howItWork: 'Funciona del siguiente modo:',
+    thatIt: '¡Eso es!',
+    googleDocuments: 'Documentos de Google',
+    googleDocumentsTitle: 'Inserte sus documentos mediante el servicio Google Drive. ¡Es gratuito!',
+    slideshare: 'SlideShare',
+    slideshareTitle: 'Inserte sus presentaciones mediante el servicio SlideShare. ¡Es gratuito!',
+    other: 'Otro',
+    otherTitle: 'Inserte sus documentos desde cualquier otro servicio que admita la función de inserción.',
     googleDocumentsExplanation: '<ol>' +
-                        '<li>Go to your <a href="https://www.google.com/intl/es/drive/" target="_blank">Google Drive</a>. [es]</li>' +
-                        '<li>Open the document and select "Publish to the Web" under the "File" menu. [es]</li>' +
-                        '<li>If the document has not been published yet - click the "Publish" button. [es]</li>' +
-                        '<li>Open the "Embed" tab and copy the provided embed code. [es]</li>' +
-                        '<li>Paste this code into the field below. [es]</li>' +
+                        '<li>Vaya a su <a href=""https://www.google.com/intl/en/drive/"" target=""_blank"">Google Drive</a>.</li>' +
+                        '<li>Abra el documento y seleccione ""Publicar en la web"" en el menú ""Archivo"".</li>' +
+                        '<li>Si el documento todavía no se ha publicado, haga clic en el botón ""Publicar"".</li>' +
+                        '<li>Abra la pestaña ""Insertar"" y copie el código para insertar proporcionado.</li>' +
+                        '<li>Pegue dicho código en el campo a continuación.</li>' +
                         '</ol>',
     slideshareExplanation: '<ol>' +
-                        '<li>Visit <a href="http://www.slideshare.net/" target="_blank">SlideShare website</a> and Sign up. [es]</li>' +
-                        '<li>Upload your presentation to SlideShare. [es]</li>' +
-                        '<li>Open your presentation and click the "Share" button below the player. [es]</li>' +
-                        '<li>Copy the Embed code that\'s provided and paste this code into the field below. [es]</li>' +
+                        '<li>Visite el sitio web de <a href=""http://www.slideshare.net/"" target=""_blank"">SlideShare</a> y regístrese.</li>' +
+                        '<li>Cargue su presentación en SlideShare.</li>' +
+                        '<li>Abra la presentación y haga clic en el botón ""Compartir"" situado bajo el reproductor.</li>' +
+                        '<li>Copie el código para insertar que se le ha proporcionado y péguelo en el campo a continuación.</li>' +
                         '</ol>',
     otherDocumentsExplanation: '<ol>' +
-                        '<li>Go to your service. [es]</li>' +
-                        '<li>Copy the embed code of the appropriate document. [es]</li>' +
-                        '<li>Paste this code into the field below. [es]</li>' +
+                        '<li>Acceda a su servicio.</li>' +
+                        '<li>Copie el código para insertar correspondiente al documento en cuestión.</li>' +
+                        '<li>Pegue dicho código en el campo a continuación.</li>' +
                         '</ol>'
 });
--- sources/easygenerator.Web/Scripts/ckeditor/customPlugins/documentembed/lang/zh-cn.js
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/documentembed/lang/zh-cn.js
@@ -1,31 +1,41 @@
 ﻿CKEDITOR.plugins.setLang('documentembed', 'zh-cn', {
-    ebmedDocument: 'Embed document [zh-cn]',
-    embedCodeMissing: 'Embed code is missing [zh-cn]',
-    pasteEmbedCode: 'Paste embed code here... [zh-cn]',
-    howItWork: 'Here is how it works: [zh-cn]',
-    thatIt: 'That\'s it! [zh-cn]',
-    googleDocuments: 'Google documents [zh-cn]',
-    googleDocumentsTitle: 'Embed your documents using Google Drive service. It\'s free! [zh-cn]',
-    slideshare: 'SlideShare [zh-cn]',
-    slideshareTitle: 'Embed your presentations using SlideShare service. It\'s free! [zh-cn]',
-    other: 'Other [zh-cn]',
-    otherTitle: 'Embed your documents from any other services that support "embedding". [zh-cn]',
+    ebmedDocument: '嵌入文档',
+    embedCodeMissing: '缺少嵌入代码',
+    pasteEmbedCode: '在此处粘贴嵌入代码...',
+    howItWork: '以下是其工作原理：',
+    thatIt: '就是这样！',
+    googleDocuments: 'Google 文档',
+    googleDocumentsTitle: '使用 Google Drive 服务嵌入您的文档。完全免费！',
+    slideshare: 'SlideShare',
+    slideshareTitle: '使用 SlideShare 服务嵌入您的演示文档。完全免费！',
+    other: '其他',
+    otherTitle: '从任何其他支持“嵌入”的服务中嵌入您的文档。',
     googleDocumentsExplanation: '<ol>' +
-                        '<li>Go to your <a href="https://www.google.com/intl/zh-ch/drive/" target="_blank">Google Drive</a>. [zh-cn]</li>' +
-                        '<li>Open the document and select "Publish to the Web" under the "File" menu. [zh-cn]</li>' +
-                        '<li>If the document has not been published yet - click the "Publish" button. [zh-cn]</li>' +
-                        '<li>Open the "Embed" tab and copy the provided embed code.</li> [zh-cn]' +
-                        '<li>Paste this code into the field below.</li> [zh-cn]' +
+                        '<li>转至 <a href=""https://www.google.com/intl/en/drive/"" target=""_blank"">Google drive</a>。</li>' +
+                        '<li>打开文档并在“文件”菜单下选择“发布至 Web”。</li>' +
+                        '<li>如果文档仍未发布 - 单击“发布”按钮。</li>' +
+                        '<li>打开“嵌入”选项卡，并复制提供的嵌入代码。' +
+                        '<li>将此代码粘贴至以下字段。' +
                         '</ol>',
     slideshareExplanation: '<ol>' +
-                        '<li>Visit <a href="http://www.slideshare.net/" target="_blank">SlideShare website</a> and Sign up.</li> [zh-cn]' +
-                        '<li>Upload your presentation to SlideShare. [zh-cn]</li>' +
-                        '<li>Open your presentation and click the "Share" button below the player.</li> [zh-cn]' +
-                        '<li>Copy the Embed code that\'s provided and paste this code into the field below. [zh-cn]</li>' +
+                        '<li>访问 <a href=""http://www.slideshare.net/"" target=""_blank"">SlideShare 网站</a>并登录。' +
+                        '<li>将演示文档上传至 SlideShare。</li>' +
+                        '<li>打开演示文档并单击播放器下的“分享”按钮。' +
+                        '<li>复制提供的嵌入代码并粘贴至以下字段。</li>' +
                         '</ol>',
     otherDocumentsExplanation: '<ol>' +
-                        '<li>Go to your service. [zh-cn]</li>' +
-                        '<li>Copy the embed code of the appropriate document. [zh-cn]</li>' +
-                        '<li>Paste this code into the field below. [zh-cn]</li>' +
+                        '<li>转至您的服务。</li>' +
+                        '<li>复制相应文档的嵌入代码。</li>' +
+                        '<li>将此代码粘贴至以下字段。</li>' +
                         '</ol>'
 });
+
+
+
+
+
+
+
+
+
+
