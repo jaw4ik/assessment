b6e54cc694404e11743a2e87773c21d024d987fd
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2016-01-11 14:57:58 +0200

    Add model for Publication.

---
+++ sources/easygenerator.PublicationServer/Models/Publication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace easygenerator.PublicationServer.Models
+{
+    public class Publication
+    {
+        public Guid Id { get; set; }
+        public DateTime PublishedBy { get; set; }
+        public DateTime PublishedOn { get; set; }
+        public bool IsSearchable { get; set; }
+        public Guid? SearchId { get; set; }
+    }
+}
