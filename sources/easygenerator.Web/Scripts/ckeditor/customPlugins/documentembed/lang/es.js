2bc5bccb88d9385c917df40a155cf9523000240b
NadiaGamula <nadia.zakharova@gmail.com>
2016-01-13 15:19:07 +0200

    Update server resources and fix jasmine tests in firefox

--- sources/easygenerator.Web/Scripts/ckeditor/customPlugins/documentembed/lang/es.js
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/documentembed/lang/es.js
@@ -11,14 +11,14 @@
     other: 'Otro',
     otherTitle: 'Inserte sus documentos desde cualquier otro servicio que admita la función de inserción.',
     googleDocumentsExplanation: '<ol>' +
-                        '<li>Vaya a su <a href=""https://www.google.com/intl/en/drive/"" target=""_blank"">Google Drive</a>.</li>' +
+                        '<li>Vaya a su <a href="https://www.google.com/intl/en/drive/" target=""_blank"">Google Drive</a>.</li>' +
                         '<li>Abra el documento y seleccione ""Publicar en la web"" en el menú ""Archivo"".</li>' +
                         '<li>Si el documento todavía no se ha publicado, haga clic en el botón ""Publicar"".</li>' +
                         '<li>Abra la pestaña ""Insertar"" y copie el código para insertar proporcionado.</li>' +
                         '<li>Pegue dicho código en el campo a continuación.</li>' +
                         '</ol>',
     slideshareExplanation: '<ol>' +
-                        '<li>Visite el sitio web de <a href=""http://www.slideshare.net/"" target=""_blank"">SlideShare</a> y regístrese.</li>' +
+                        '<li>Visite el sitio web de <a href="http://www.slideshare.net/" target=""_blank"">SlideShare</a> y regístrese.</li>' +
                         '<li>Cargue su presentación en SlideShare.</li>' +
                         '<li>Abra la presentación y haga clic en el botón ""Compartir"" situado bajo el reproductor.</li>' +
                         '<li>Copie el código para insertar que se le ha proporcionado y péguelo en el campo a continuación.</li>' +
