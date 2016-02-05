ade1c4e9b4e62c05ad4cb6f5076ef3ce4b65e556
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2016-02-05 10:18:06 +0200

    Fix issue with too long index field.

--- tools/PublicationServerDBCreation/createPublicationServerDb.sql
+++ tools/PublicationServerDBCreation/createPublicationServerDb.sql
@@ -12,7 +12,7 @@ CREATE TABLE [dbo].[Publications](
 	[OwnerEmail] [nvarchar](254) NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
 	[ModifiedOn] [datetime] NOT NULL,
-	[PublicPath] [nvarchar](512) NOT NULL,
+	[PublicPath] [nvarchar](400) NOT NULL,
  CONSTRAINT [PK_Publications] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
