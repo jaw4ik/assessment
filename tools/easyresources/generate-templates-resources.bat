6163456b59aa3aa349cfa9755f722c5886e233b1
jaw4ik <urec.savchuk@gmail.com>
2016-06-17 09:28:46 +0300

    Vietnamese language added to Application and Simple course

--- tools/easyresources/generate-templates-resources.bat
+++ tools/easyresources/generate-templates-resources.bat
@@ -4,6 +4,7 @@ set templates[0]=Simple course
 set templates[1]=Personalized learning
 set templates[2]=Assessment
 set templates[3]=Reader
+set templates[4]=Nielsen and ICEMD Simple course
 
 for /F "tokens=2 delims=[=]" %%i in ('set templates[') do call easyresources generate template --worksheetName "%%templates[%%i]%%" --outputPath "_templates/_%%templates[%%i]%%" --excludeLocalizations "ar"
 
