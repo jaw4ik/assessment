dbcf352adaf5a4daf03aa237b3bd3becbb9c60d4
NadiaGamula <nadia.zakharova@gmail.com>
2016-07-01 14:29:18 +0300

    Update resources (#602)
    * Update resources
    
    * Update resources in templates

--- sources/easygenerator.Web/Scripts/ckeditor/customPlugins/audiolibrary/lang/pt-br.js
+++ sources/easygenerator.Web/Scripts/ckeditor/customPlugins/audiolibrary/lang/pt-br.js
@@ -2,6 +2,6 @@
     addFromLibrary: 'Add audio from library [pt-br]',
     audioLibrary: 'Audio library [pt-br]',
     noAudio: 'No audio yet [pt-br]',
-    somethingWentWrong: 'Algo não funcionou corretamente. ',
+    somethingWentWrong: 'Ocorreu um erro.',
     tryAgain: 'Tente novamente'
 });
