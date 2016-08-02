6e59862b4b53fffd5e6876d184bc1565ba440462
Roman Petriv <petriv.roma@gmail.com>
2016-08-02 19:53:13 +0300

    fix bug with unsaved themes in Simple course (add fix for colors)

--- sources/easygenerator.Web/App/mappers/themeModelMapper.js
+++ sources/easygenerator.Web/App/mappers/themeModelMapper.js
@@ -1,13 +1,30 @@
 ﻿import ThemeModel from 'models/theme';
 
 export default {
-    map: theme => new ThemeModel({
-        id: theme.Id,
-        name: theme.Name,
-        settings: JSON.parse(theme.Settings),
-        templateId: theme.TemplateId,
-        createdBy: theme.CreatedBy,
-        createdOn: new Date(theme.CreatedOn),
-        modifiedOn: new Date(theme.ModifiedOn)
-    })
+    map(theme){
+        //fix for nulls in json array (see Simple course templateSettings.js)
+        var themeSettings = JSON.parse(theme.Settings);
+        themeSettings && themeSettings.fonts && removeNullsInArray(themeSettings.fonts);
+        themeSettings && themeSettings.branding && themeSettings.branding.colors && removeNullsInArray(themeSettings.branding.colors);
+        //end fix
+        return new ThemeModel({
+            id: theme.Id,
+            name: theme.Name,
+            settings: themeSettings,
+            templateId: theme.TemplateId,
+            createdBy: theme.CreatedBy,
+            createdOn: new Date(theme.CreatedOn),
+            modifiedOn: new Date(theme.ModifiedOn)
+        });
+    }
 };
+
+function removeNullsInArray(array){
+    if (array && array.length) {
+        for (var i = 0; i < array.length; i++) {
+            if (array[i] === null) {
+                delete array[i];
+            }
+        }
+    }
+}
