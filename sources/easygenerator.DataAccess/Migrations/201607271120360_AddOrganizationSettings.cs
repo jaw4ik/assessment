eb786405e926470baa4f884a181a13893a309e46
Yulia Savchuk <yulia.savchuk3@gmail.com>
2016-08-11 10:00:12 +0300

    Fix initial db creation (#641)

--- sources/easygenerator.DataAccess/Migrations/201607271120360_AddOrganizationSettings.cs
+++ sources/easygenerator.DataAccess/Migrations/201607271120360_AddOrganizationSettings.cs
@@ -1,35 +1,34 @@
 namespace easygenerator.DataAccess.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddOrganizationSettings : DbMigration
     {
         public override void Up()
         {
             CreateTable(
-                "dbo.OrganizationSettingsCollection",
+                "dbo.OrganizationSettings",
                 c => new
-                    {
-                        Id = c.Guid(nullable: false),
-                        AccessType = c.Int(),
-                        ExpirationDate = c.DateTime(),
-                    })
+                {
+                    Id = c.Guid(nullable: false),
+                    AccessType = c.Int(),
+                    ExpirationDate = c.DateTime(),
+                })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Organizations", t => t.Id, cascadeDelete: true)
                 .Index(t => t.Id);
-            
+
             AddColumn("dbo.UserSettings", "PersonalAccessType", c => c.Int());
             AddColumn("dbo.UserSettings", "PersonalExpirationDate", c => c.DateTime());
         }
-        
+
         public override void Down()
         {
-            DropForeignKey("dbo.OrganizationSettingsCollection", "Id", "dbo.Organizations");
-            DropIndex("dbo.OrganizationSettingsCollection", new[] { "Id" });
+            DropForeignKey("dbo.OrganizationSettings", "Id", "dbo.Organizations");
+            DropIndex("dbo.OrganizationSettings", new[] { "Id" });
             DropColumn("dbo.UserSettings", "PersonalExpirationDate");
             DropColumn("dbo.UserSettings", "PersonalAccessType");
-            DropTable("dbo.OrganizationSettingsCollection");
+            DropTable("dbo.OrganizationSettings");
         }
     }
 }
