0455e085a30270d426ccdd7e33f27be12eee4023
Roman Petriv <petriv.roma@gmail.com>
2016-08-15 17:05:50 +0300

    update player deploy, remove sources endpoint from storage

--- tools/PlayerConfigTransform/Staging.transform.js
+++
@@ -1,6 +0,0 @@
-﻿module.exports = {
-    sourcesUrl: '//storage-staging.easygenerator.com/api/mediasources/',
-    vimeoFileQualities: ['hd', 'sd', 'mobile'],
-    playerUpdateInterval: 30000,
-	volumeKey: 'easyplayerVolume'
-};
