1c12357253405b035d2f316827270c05840fe18a
Roman Petriv <petriv.roma@gmail.com>
2016-09-02 14:54:23 +0300

    add cache control to player on get sources from vimeo

---
+++ sources/easygenerator.Player/middlewares/nocache.js
@@ -0,0 +1,6 @@
+﻿module.exports = function (req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+    next();
+}
