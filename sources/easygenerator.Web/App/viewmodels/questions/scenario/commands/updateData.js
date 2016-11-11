e4753295fc52284d1f85c9f8f1a9f93c58316728
jaw4ik <urec.savchuk@gmail.com>
2016-11-11 16:34:59 +0200

    Disable Branchtrack autofocus

--- sources/easygenerator.Web/App/viewmodels/questions/scenario/commands/updateData.js
+++ sources/easygenerator.Web/App/viewmodels/questions/scenario/commands/updateData.js
@@ -1,15 +1,28 @@
-﻿define(['http/apiHttpWrapper'], function (apiHttpWrapper) {
+﻿import apiHttpWrapper from 'http/apiHttpWrapper';
+import $ from 'jquery';
 
-    return {
-        execute: function (questionId, projectInfo) {
-            return apiHttpWrapper.post('api/question/scenario/updatedata', {
-                questionId: questionId,
-                projectId: projectInfo.token,
-                embedCode: projectInfo.embed_code,
-                embedUrl: projectInfo.embed_url,
-                projectArchiveUrl: projectInfo.zip_url
-            });
-        }
-    };
+const scenarioDisableFocusParameter = '?disable-focus';
 
-});
+export default {
+    execute(questionId, projectInfo) {
+        return apiHttpWrapper.post('api/question/scenario/updatedata', {
+            questionId: questionId,
+            projectId: projectInfo.token,
+            embedCode: processEmbedCode(projectInfo.embed_code),
+            embedUrl: processEmbedUrl(projectInfo.embed_url),
+            projectArchiveUrl: projectInfo.zip_url
+        });
+    }
+};
+
+function processEmbedUrl(embedUrl) {
+    return embedUrl + scenarioDisableFocusParameter;
+}
+
+function processEmbedCode(embedCode) {
+    let $embedCode = $(embedCode);
+
+    return $embedCode
+        .attr('src', $embedCode.attr('src') + scenarioDisableFocusParameter)
+        .get(0).outerHTML;
+}
