ed5cd528e7845ec071071c0d74fea9215e8b30d5
Roman Petriv <petriv.roma@gmail.com>
2016-12-23 17:28:37 +0200

    fix bundling and froala loading

--- sources/easygenerator.Web/App/bootstrapping/htmlEditorInitializationTask.js
+++ sources/easygenerator.Web/App/bootstrapping/htmlEditorInitializationTask.js
@@ -1,5 +1,14 @@
-﻿import _ from 'underscore';
+﻿import $ from 'jquery';
 import froala from 'components/htmlEditor/editorWrapper';
+
+// plugins
+import paragraphFormatPlugin from 'froala-editor/js/plugins/paragraph_format.min';
+import linkPlugin from 'froala-editor/js/plugins/link.min';
+import listsPlugin from 'froala-editor/js/plugins/lists.min';
+import urlPlugin from 'froala-editor/js/plugins/url.min';
+import alignPlugin from 'froala-editor/js/plugins/align.min';
+
+// commands
 import markCommand from 'components/htmlEditor/commands/markCommand';
 import alignCommand from 'components/htmlEditor/commands/alignCommand';
 import insertLinkCommand from 'components/htmlEditor/commands/insertLinkCommand';
@@ -10,12 +19,14 @@ export default {
         froala.setLicense(window.froalaLicenseKey);
         froala.addBlockTag('cite');
 
-        let plugins = ['paragraph_format', 'link', 'lists', 'url', 'align'],
+        let plugins = [paragraphFormatPlugin, linkPlugin, listsPlugin, urlPlugin, alignPlugin],
             commands = [markCommand, alignCommand, insertLinkCommand, paragraphFormatCommand];
 
-        Promise.all(_.map(plugins, froala.addPlugin))
-            .then(() => {
-                _.each(commands, command => command.register());
-            });
+        for(let plugin of plugins) {
+            froala.initFroalaModule(plugin);
+        }
+        for(let command of commands) {
+            command.register();
+        }
     }
 };
